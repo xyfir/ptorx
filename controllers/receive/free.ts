3677bb8951800a8492bdba23c97d5f69ccf3b682
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 10:17:02 -0700

    Add todos

--- controllers/receive/free.ts
+++ controllers/receive/free.ts
@@ -1,1 +1,12 @@
-﻿
+﻿export = function (req, res) {
+
+    /*
+        - Free users only receive subject filtering
+            - MailGun has already matched subject
+        - Pull keywords out of email
+        - Generate an ad from Xyfir Ads with keywords
+        - Insert into email
+        - Send email off to user
+    */
+
+};
--- controllers/receive/paid.ts
+++ controllers/receive/paid.ts
@@ -1,1 +1,11 @@
-﻿
+﻿export = function (req, res) {
+
+    /*
+        - Grab filters for email + modifiers + redirect email + save_mail
+        - Loop through filters
+        - If email passes through filters, send to redirect if available
+            - Run content through any available modifiers
+        - If email did not pass and save_mail === true, delete it from MailGun's storage
+    */
+
+};
