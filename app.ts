16a73a77e153177c1c8e27801648fb08fabebc20
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 09:12:40 -0700

    Install / configure body-parser, express-session/mysql-session, stripe

--- app.ts
+++ app.ts
@@ -1,13 +1,39 @@
 /// <reference path="typings/main.d.ts" />
 
 import * as express from "express";
+import * as session from "express-session";
+import * as parser from "body-parser";
 
-
+let sstore = require("express-mysql-session");
 let app = express();
 
-let config = require("./config").environment;
+let config = require("./config");
+app.listen(config.environment.port, () => {
+    console.log("SERVER RUNNING ON", config.environment.port);
+});
 
-app.listen(config.port, () => { console.log("SERVER RUNNING ON ", config.port); });
+/* Sessions */
+let sessionStore = new sstore({
+    host: config.database.host,
+    port: config.database.port,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database,
+    useConnectionPooling: true
+});
+app.use(session({
+    secret: config.secrets.session,
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        httpOnly: false
+    }
+}));
+
+/* Body Parser */
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
 // Express middleware / controllers
 app.use("/", express.static(__dirname + "/public"));
