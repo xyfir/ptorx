9dc1d8b99d3a3d24110864ea9ef7e353c11324ce
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 12:27:46 -0700

    Add controller for DELETE api/modifiers/:mod

--- controllers/modifiers/delete.ts
+++ controllers/modifiers/delete.ts
@@ -1,1 +1,20 @@
-﻿
+﻿import db = require("../../lib/db");
+
+/*
+    DELETE api/modifiers/:mod
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Deletes a modifier
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        DELETE FROM modifiers WHERE modifier_id = ? AND user_id = ?
+    `;
+    db(cn => cn.query(sql, [req.params.mod, req.session.uid], (err, rows) => {
+        cn.release();
+        res.json({ error: !!err || !rows.length });
+    }));
+
+};
