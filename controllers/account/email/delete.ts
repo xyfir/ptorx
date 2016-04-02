730e0108ca49273c657fcda81359098aa53ed742
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 10:16:23 -0700

    Add controller for DELETE api/account/email/:email

--- controllers/account/email/delete.ts
+++ controllers/account/email/delete.ts
@@ -1,1 +1,24 @@
-﻿
+﻿import db = require("../../../lib/db");
+
+/*
+    DELETE api/account/email/:email
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Deletes a MAIN email from user's account
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        DELETE FROM main_emails WHERE id = ? AND user_id = ?
+    `;
+    let vars = [
+        req.params.email, req.session.uid
+    ];
+
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+        res.json({ error: !!err || !result.affectedRows });
+    }));
+
+};
