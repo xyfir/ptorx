b4f3b30d53967e4cdffaa90663d9df04e9471643
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 09:57:55 -0700

    Add /emails/:email/messages and /modifiers routes/controllers

--- controllers/index.ts
+++ controllers/index.ts
@@ -14,6 +14,11 @@ router.route("/emails/:email")
     .update(require("./emails/update"))
     .delete(require("./emails/delete"));
 router.post("/emails/:email/send", require("./emails/send"));
+router.get("/emails/:email/messages", require("./emails/messages/list"));
+router.route("/emails/:email/messages/:id")
+    .get(require("./emails/messages/get"))
+    .post(require("./emails/messages/reply"))
+    .delete(require("./emails/messages/delete"));
 
 // Filter controllers
 router.get("/filters", require("./filters/list"));
@@ -23,12 +28,20 @@ router.route("/filters/:filter")
     .update(require("./filters/update"))
     .delete(require("./filters/delete"));
 
+// Modifier controllers
+router.get("/modifiers", require("./modifiers/list"));
+router.route("/modifiers/:mod")
+    .get(require("./modifiers/get"))
+    .post(require("./modifiers/create"))
+    .update(require("./modifiers/update"))
+    .delete(require("./modifiers/delete"));
+
 // Account controllers
 router.get("/account", require("./account/info"));
 router.update("/account/subscription", require("./account/purchase"));
 router.route("/account/email/:id")
-    .post(require("./account/add-email"))
-    .delete(require("./account/delete-email"));
+    .post(require("./account/email/add"))
+    .delete(require("./account/email/delete"));
 router.post("/account/login", require("./account/login"));
 
 export = router;
