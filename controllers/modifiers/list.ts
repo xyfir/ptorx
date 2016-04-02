6054a2beb30b7c909ad128a6cce9c7bdac36089a
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 12:02:23 -0700

    Add controller for GET api/modifiers

--- controllers/modifiers/list.ts
+++ controllers/modifiers/list.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import db = require("../../lib/db");
+
+/*
+    GET api/modifiers
+    RETURN
+        { modifiers: [
+            id: number, name: string, description: string, type: number
+        ]}
+    DESCRIPTION
+        Returns basic information for all modifiers linked to account
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        SELECT modifier_id as id, name, description, type
+        FROM modifiers WHERE user_id = ? 
+    `;
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+        res.json({ modifiers: (err || !rows.length ? [] : rows) });
+    }));
+
+};
