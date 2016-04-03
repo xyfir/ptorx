eb99d03d969af516d7a316e8f281bd23002f0cd3
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-03 08:51:55 -0700

    Add controller for GET api/emails

--- controllers/emails/list.ts
+++ controllers/emails/list.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import db = require("../../lib/db");
+
+/*
+    GET api/emails
+    RETURN
+        { emails: [
+            id: number, name: string, description: string, address: string
+        ]}
+    DESCRIPTION
+        Returns basic information for all REDIRECT emails linked to account
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        SELECT emails_id as id, name, description, address
+        FROM redirect_emails WHERE user_id = ?
+    `;
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+        res.json({ emails: (err || !rows.length ? [] : rows) });
+    }));
+
+};
