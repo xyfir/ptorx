998f723fb5f2f0dcec4e8c2ff58d962d0d131383
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 19:22:49 -0700

    Add module for validating filter information

---
+++ lib/filter/is-valid.ts
@@ -0,0 +1,14 @@
+﻿export = function (filter): boolean {
+
+    if (!filter.type || !(filter.name || "").match(/^\w{1,40}$/) || (filter || "").description.length > 150 || !filter.find)
+        return false;
+
+    else if ((filter.find || "").length > 150)
+        return false;
+
+    else if (filter.type == 6 && filter.find.indexOf(":::") === -1)
+        return false;
+
+    else return true;
+
+};
