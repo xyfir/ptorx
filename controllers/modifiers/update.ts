82aa44cd1f4eb51d5364b8e9d3b1f12ab1b1342a
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 18:36:02 -0700

    Add controller for UPDATE api/modifiers/:mod

--- controllers/modifiers/update.ts
+++ controllers/modifiers/update.ts
@@ -1,1 +1,48 @@
-﻿
+﻿import buildData = require("../../lib/modifier/build-data");
+import isValid = require("../../lib/modifier/is-valid");
+import db = require("../../lib/db");
+
+/*
+    UPDATE api/modifiers/:mod
+    REQUIRED
+        type: number, name: string, description: string
+    OPTIONAL
+        ENCRYPT
+            algorithm: string, key: string
+        REPLACE
+            value: string, with: string, regex: boolean
+        TAG
+            prepend: string, value: string
+        SUBJECT
+            subject: string
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Update a modifier's data
+*/
+export = function (req, res) {
+
+    if (!isValid(req.body)) {
+        res.json({ error: true });
+        return;
+    }
+
+    let sql: string = `
+        UPDATE modifiers SET name = ?, description = ?, type = ?, data = ?
+        WHERE modifier_id = ? AND user_id = ?
+    `;
+    let vars = [
+        req.body.name, req.body.description, req.body.type, buildData(req.body),
+        req.params.mod, req.session.uid
+    ];
+
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+
+        if (err || !result.affectedRows)
+            res.json({ error: true });
+        else
+            res.json({ error: false });
+    }));
+
+};
