1e380606d18c6b916548aac657c9a600211db899
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 18:46:38 -0700

    Add controller for DELETE api/filter/:filter

--- controllers/filters/delete.ts
+++ controllers/filters/delete.ts
@@ -1,1 +1,20 @@
-﻿
+﻿import db = require("../../lib/db");
+
+/*
+    DELETE api/filters/:filter
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Deletes a filter
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        DELETE FROM filters WHERE filter_id = ? AND user_id = ?
+    `;
+    db(cn => cn.query(sql, [req.params.filter, req.session.uid], (err, rows) => {
+        cn.release();
+        res.json({ error: !!err || !rows.length });
+    }));
+
+};
