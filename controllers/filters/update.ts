ad6d18cdb323c581869ac6c98c2f811b9cc5529e
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 19:31:47 -0700

    Add controller for UPDATE api/filters/:filter

--- controllers/filters/update.ts
+++ controllers/filters/update.ts
@@ -1,1 +1,42 @@
-﻿
+﻿import isValid = require("../../lib/filter/is-valid");
+import db = require("../../lib/db");
+
+/*
+    UPDATE api/filters/:filter
+    REQUIRED
+        type: number, name: string, description: string, find: string
+    OPTIONAL
+        acceptOnMatch: boolean, useRegex: boolean
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Update a filter's data
+*/
+export = function (req, res) {
+
+    if (!isValid(req.body)) {
+        res.json({ error: true });
+        return;
+    }
+
+    let sql: string = `
+        UPDATE filters SET name = ?, description = ?, type = ?, find = ?,
+        accept_on_match = ?, use_regex = ?
+        WHERE filter_id = ? AND user_id = ?
+    `;
+    let vars = [
+        req.body.name, req.body.description, req.body.type, req.body.find,
+        req.body.acceptOnMatch, req.body.useRegex,
+        req.params.filter, req.session.uid
+    ];
+
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+
+        if (err || !result.affectedRows)
+            res.json({ error: true });
+        else
+            res.json({ error: false });
+    }));
+
+};
