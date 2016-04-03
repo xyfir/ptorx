0e882c4d5e2176287bc9ab27108b6a3154531c96
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 17:08:22 -0700

    Change create routes for modifiers and filters

--- controllers/index.ts
+++ controllers/index.ts
@@ -22,24 +22,24 @@ router.route("/emails/:email/messages/:id")
 
 // Filter controllers
 router.get("/filters", require("./filters/list"));
+router.post("/filters", require("./filters/create"));
 router.route("/filters/:filter")
     .get(require("./filters/get"))
-    .post(require("./filters/create"))
     .update(require("./filters/update"))
     .delete(require("./filters/delete"));
 
 // Modifier controllers
 router.get("/modifiers", require("./modifiers/list"));
+router.post("/modifiers", require("./modifiers/create"));
 router.route("/modifiers/:mod")
     .get(require("./modifiers/get"))
-    .post(require("./modifiers/create"))
     .update(require("./modifiers/update"))
     .delete(require("./modifiers/delete"));
 
 // Account controllers
 router.get("/account", require("./account/info"));
 router.update("/account/subscription", require("./account/purchase"));
-router.route("/account/email/:id")
+router.route("/account/email/:email")
     .post(require("./account/email/add"))
     .delete(require("./account/email/delete"));
 router.post("/account/login", require("./account/login"));
