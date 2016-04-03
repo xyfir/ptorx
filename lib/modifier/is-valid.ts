54b95c6066e34b236e53afd023eff0beb9aee9b7
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 17:05:47 -0700

    Add module for determining if provided modifier data is valid

---
+++ lib/modifier/is-valid.ts
@@ -0,0 +1,21 @@
+﻿export = function (mod): boolean {
+
+    if (!mod.type || !(mod.name || "").match(/^\w{1,40}$/) || (mod || "").description.length > 150)
+        return false;
+
+    switch (mod.type) {
+        case 1:
+            return typeof mod.algorithm == "string" && typeof mod.key == "string";
+        case 2:
+            return true;
+        case 3:
+            return mod.value !== undefined && mod.with !== undefined && [0, 1].indexOf(mod.regex) > -1;
+        case 4:
+            return mod.subject !== undefined;
+        case 5:
+            return [0, 1].indexOf(mod.prepend) > -1 && mod.value !== undefined;
+        default:
+            return false;
+    }
+    
+};
