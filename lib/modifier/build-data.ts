907089faf45c17dfe53158fdbdfaa18675edd8ea
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 17:06:25 -0700

    Add module for building ptorx.modifiers.data value/object

---
+++ lib/modifier/build-data.ts
@@ -0,0 +1,16 @@
+﻿export = function (mod): string {
+    
+    switch (mod.type) {
+        case 1:
+            return JSON.stringify({ algorithm: mod.algorithm, key: mod.key });
+        case 2:
+            return "";
+        case 3:
+            return JSON.stringify({ value: mod.value, with: mod.with, regex: !!mod.regex });
+        case 4:
+            return mod.subject + '';
+        case 5:
+            return JSON.stringify({ prepend: mod.prepend, value: mod.value });
+    }
+
+};
