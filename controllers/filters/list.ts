1f13fb03144ba7c29a9a6100d1aa1b36ca03047e
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-02 18:39:01 -0700

    Add controller for GET api/filters

--- controllers/filters/list.ts
+++ controllers/filters/list.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import db = require("../../lib/db");
+
+/*
+    GET api/filters
+    RETURN
+        { filters: [
+            id: number, name: string, description: string, type: number
+        ]}
+    DESCRIPTION
+        Returns basic information for all filters linked to account
+*/
+export = function (req, res) {
+
+    let sql: string = `
+        SELECT filter_id as id, name, description, type
+        FROM filters WHERE user_id = ? 
+    `;
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+        res.json({ modifiers: (err || !rows.length ? [] : rows) });
+    }));
+
+};
