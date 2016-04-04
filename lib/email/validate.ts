946bca384cf3591f89a7cde8f7541c1116ae7f28
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-04 10:34:14 -0700

    Add module for initiial validation of email info when creating/updating a redirect email

---
+++ lib/email/validate.ts
@@ -0,0 +1,27 @@
+﻿export = function (email, subscription: number): string {
+
+    if (!(email.name || '').match(/^[\w\s-]{1,40}$/))
+        return "Invalid name";
+    if ((email.description || '').length > 150)
+        return "Description character limit hit";
+
+    if (Date.now() > subscription) {
+        if ((email.address || '').length > 0)
+            return "Free members cannot customize email address";
+        if ((email.filters || '').indexOf(',') > -1)
+            return "Free members can only have a subject filter";
+        if ((email.modifiers || '').length > 0)
+            return "Free members cannot use modifiers";
+        if (!email.to || email.noToAddress)
+            return "Free member emails must redirect to a main address";
+        if (email.saveMail)
+            return "Free members cannot save emails";
+    }
+    else {
+        if (!(email.address || '').match(/^[A-Za-z0-9-]{1,40}@ptorx.com$/))
+            return "Invalid email address characters or length";
+    }
+
+    return "ok";
+
+};
