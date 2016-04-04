aa1254b7cdb687008f33bfca623dc77e9b27e23d
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-04 10:36:17 -0700

    Add module for validating that the user has access to the modifiers provided and that there will be no conflicts

---
+++ lib/email/validate-modifiers.ts
@@ -0,0 +1,34 @@
+﻿export = function (modifiers: number[], req, cn, fn: Function) {
+
+    // User has no modifiers
+    if (modifiers[0] + '' === '') {
+        fn(false);
+        return;
+    }
+
+    // Validate that all filters exist and user has access to them
+    let sql: string = `
+        SELECT type FROM modifiers WHERE modifier_id IN (?) AND user_id = ?
+    `;
+    cn.query(sql, [modifiers.join(", "), req.session.uid], (err, rows) => {
+        if (rows.length !== modifiers.length) {
+            fn(true, "One or more modifiers provided do not exist");
+        }
+        else {
+            // Ensure no more than one encryption modifier is present
+            let encrypt = false;
+            for (let row in rows) {
+                if (row.type == 1 && encrypt) {
+                    fn(true, "Cannot use more than one encryption modifier");
+                    return;
+                }
+                else if (row.type == 1) {
+                    encrypt = true;
+                }
+            }
+
+            fn(false);
+        }
+    });
+
+};
