2734157cbba7bee3f162bde9fe6deb567ce906af
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-04 10:35:32 -0700

    Add module for validating that the user has access to the filters provided and that there will be no conflicts

---
+++ lib/email/validate-filters.ts
@@ -0,0 +1,46 @@
+﻿export = function (filters: number[], req, cn, fn: Function) {
+
+    // User has no filters
+    if (filters[0] + '' === '') {
+        fn(false);
+        return;
+    }
+
+    // Validate that all filters exist and user has access to them
+    let sql: string = `
+        SELECT type, accept_on_match, use_regex FROM filters
+        WHERE filter_id IN (?) AND user_id = ?
+    `;
+    cn.query(sql, [filters.join(", "), req.session.uid], (err, rows) => {
+        if (rows.length !== filters.length) {
+            fn(true, "One or more filters provided do not exist");
+        }
+        else {
+            let types = [];
+
+            // Ensure free members are only using allowed filters/options
+            for (let row in rows) {
+                if (types.indexOf(row.type) > -1 && row.type != 6) {
+                    fn(true, "Cannot have multiple non-header filters");
+                    return;
+                }
+
+                if (Date.now() > req.session.subscription) {
+                    if (row.type != 6) {
+                        fn(true, "Free members can only use subject filters"); return;
+                    }
+                    else if (row.accept_on_match != 1) {
+                        fn(true, "Free members cannot use 'reject on match' filters"); return;
+                    }
+                    else if (row.use_regex == 1) {
+                        fn(true, "Free members cannot use Regular Expressions in filters"); return;
+                    }
+                }
+
+                types.push(row.type);
+            }
+            fn(false);
+        }
+    });
+
+};
