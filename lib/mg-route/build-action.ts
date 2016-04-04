05f782a71a3d5ff014aa60a9adc8820f68972e0b
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-04-04 10:37:27 -0700

    Add module for building the action parameter for MailGun's 'routes' API

---
+++ lib/mg-route/build-action.ts
@@ -0,0 +1,14 @@
+﻿export = function (id: number, subscription: number, save: boolean): string {
+
+    let action: string = "https://ptorx.com/api/receive/"
+        + (Date.now() > subscription ? "free" : "paid")
+        + "/" + id;
+
+    if (save)
+        action = `store(notify="${action}")`;
+    else
+        action = `forward("${action}")`
+
+    return action;
+
+};
