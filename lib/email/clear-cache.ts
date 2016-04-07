ff15bf9d4b140cb5bdb00093fe75095039226768
Mr. Xyfir <mr@xyfir.com>
2016-04-06 17:45:11 -0700

    Add module for clearing an email from Redis cache

---
+++ lib/email/clear-cache.ts
@@ -0,0 +1,18 @@
+﻿import redis = require("redis");
+
+if (global["__redis"] === undefined) {
+    let config = require("../../config").database;
+    global["__redis"] = require("sol-redis-pool")(
+        config.redis, config.redisPool
+    );
+}
+
+export = function (email: number) {
+
+    global["__redis"].acquire((err, redis: redis.RedisClient) => {
+        redis.del(email, (err, reply) => {
+            global["__redis"].release(redis);
+        });
+    });
+
+};
