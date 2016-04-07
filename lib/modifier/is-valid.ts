fef819e6fedc99ca209625cf6e1b4f15db63bcae
Mr. Xyfir <mr@xyfir.com>
2016-04-07 16:59:33 -0700

    Clean up / fix / improve validation

--- lib/modifier/is-valid.ts
+++ lib/modifier/is-valid.ts
@@ -1,19 +1,25 @@
 ﻿export = function (mod): boolean {
-
-    if (!mod.type || !(mod.name || "").match(/^\w{1,40}$/) || (mod || "").description.length > 150)
+    
+    if (!mod.type)
         return false;
 
-    switch (mod.type) {
+    if (!(mod.name || "").match(/^[\w\d -]{1,40}$/))
+        return false;
+
+    if ((mod.description || "").length > 150)
+        return false;
+
+    switch (+mod.type) {
         case 1:
             return mod.key !== undefined && mod.key.substr(0, 1) != '{';
         case 2:
             return true;
         case 3:
-            return mod.value !== undefined && mod.with !== undefined && [0, 1].indexOf(mod.regex) > -1;
+            return mod.value !== undefined && mod.with !== undefined && mod.regex;
         case 4:
             return mod.subject !== undefined && mod.subject.substr(0, 1) != '{';
         case 5:
-            return [0, 1].indexOf(mod.prepend) > -1 && mod.value !== undefined;
+            return mod.prepend !== undefined && mod.value !== undefined;
         default:
             return false;
     }
