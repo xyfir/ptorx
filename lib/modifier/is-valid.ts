2d882ac2947bd00e326a61397f18ed171c59d733
Mr. Xyfir <mr@xyfir.com>
2016-04-06 19:45:11 -0700

    Subject overwrite / encryption key strings cannot start with '{'

--- lib/modifier/is-valid.ts
+++ lib/modifier/is-valid.ts
@@ -5,13 +5,13 @@
 
     switch (mod.type) {
         case 1:
-            return mod.key !== undefined;
+            return mod.key !== undefined && mod.key.substr(0, 1) != '{';
         case 2:
             return true;
         case 3:
             return mod.value !== undefined && mod.with !== undefined && [0, 1].indexOf(mod.regex) > -1;
         case 4:
-            return mod.subject !== undefined;
+            return mod.subject !== undefined && mod.subject.substr(0, 1) != '{';
         case 5:
             return [0, 1].indexOf(mod.prepend) > -1 && mod.value !== undefined;
         default:
