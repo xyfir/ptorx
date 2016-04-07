ee327c89e0c59b41d532a2cf11ab164e381bf62a
Mr. Xyfir <mr@xyfir.com>
2016-04-06 18:52:49 -0700

    Improve code. Utilize clearCache()

--- controllers/filters/update.ts
+++ controllers/filters/update.ts
@@ -1,4 +1,5 @@
 ﻿import escapeRegExp = require("escape-string-regexp");
+import clearCache = require("../../lib/email/clear-cache");
 import isValid = require("../../lib/filter/is-valid");
 import db = require("../../lib/db");
 
@@ -24,36 +25,56 @@ export = function (req, res) {
         req.body.find = escapeRegExp(req.body.find);
 
     let sql: string = `
-        UPDATE filters SET name = ?, description = ?, type = ?, find = ?,
-        accept_on_match = ?, use_regex = ?
+        SELECT type, accept_on_match as acceptOnMatch FROM filters
         WHERE filter_id = ? AND user_id = ?
     `;
-    let vars = [
-        req.body.name, req.body.description, req.body.type, req.body.find,
-        !!(+req.body.acceptOnMatch), !!(+req.body.useRegex),
-        req.params.filter, req.session.uid
-    ];
 
-    db(cn => cn.query(sql, vars, (err, result) => {
-        if (err || !result.affectedRows) {
+    db(cn => cn.query(sql, [req.params.filter, req.session.uid], (err, rows) => {
+        if (err || !rows.length) {
             cn.release();
             res.json({ error: true });
+            return;
         }
-        // Determine if MailGun routes need to be updated
-        else if ([1, 2, 3, 6].indexOf(req.body.type) && req.body.acceptOnMatch) {
+
+        sql = `
+            UPDATE filters SET name = ?, description = ?, type = ?, find = ?,
+            accept_on_match = ?, use_regex = ?
+            WHERE filter_id = ?
+        `;
+        let vars = [
+            req.body.name, req.body.description, req.body.type, req.body.find,
+            !!(+req.body.acceptOnMatch), !!(+req.body.useRegex),
+            req.params.filter
+        ];
+
+        cn.query(sql, vars, (err, result) => {
+            if (err || !result.affectedRows) {
+                cn.release();
+                res.json({ error: true });
+                return;
+            }
+
             sql = `SELECT email_id as id FROM linked_filters WHERE filter_id = ?`;
             cn.query(sql, [req.params.filter], (err, rows) => {
                 cn.release();
 
-                if (err || !rows.length)
+                if (!rows.length) {
                     res.json({ error: false });
-                else
-                    res.json({ error: false, update: rows.map(email => { return email.id; }) });
+                }
+                else {
+                    let update: number[] = rows.map(email => { return email.id; });
+
+                    // Clear from Redis cache
+                    update.forEach(email => clearCache(email));
+
+                    if ( // Determine if MailGun routes need to be updated
+                        ([1, 2, 3, 6].indexOf(rows[0].type) > -1 && !!(+rows[0].acceptOnMatch))
+                        ||
+                        ([1, 2, 3, 6].indexOf(req.body.type) > -1 && req.body.acceptOnMatch)
+                    ) res.json({ error: false, update });
+                    else res.json({ error: false });
+                }
             });
-        }
-        else {
-            cn.release();
-            res.json({ error: false })
         }
     }));
 
