8f625fbb5cc5be1fd507d753595c46959c4e6ec0
Mr. Xyfir <mr@xyfir.com>
2016-04-07 15:37:18 -0700

    Change global.__mysql to global['__mysql'] to IDE errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,14 @@
 ﻿import * as mysql from "mysql";
 
-// Set global.__mysql equal to a client pool
-if (global.__mysql === undefined) {
+// Set global["__mysql"] equal to a client pool
+if (global["__mysql"] === undefined) {
     let config = require("../config").database.mysql;
-    global.__mysql = mysql.createPool(config);
+    global["__mysql"] = mysql.createPool(config);
 }
 
 export = (callback: Function) => {
 
-    global.__mysql.getConnection((err, cn: mysql.IConnection) => {
+    global["__mysql"].getConnection((err, cn: mysql.IConnection) => {
         callback(cn);
     });
 
