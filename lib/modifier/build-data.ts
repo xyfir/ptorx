ff5377b4793e8af8725ddfbe206cb564ce2d5ca5
Mr. Xyfir <mr@xyfir.com>
2016-04-07 17:00:08 -0700

    Fix type conversions

--- lib/modifier/build-data.ts
+++ lib/modifier/build-data.ts
@@ -2,7 +2,7 @@
 
 export = function (mod): string {
     
-    switch (mod.type) {
+    switch (+mod.type) {
         case 1:
             return mod.key + '';
         case 2:
@@ -11,12 +11,12 @@ export = function (mod): string {
             return JSON.stringify({
                 value: (mod.regex ? mod.value : escapeRegExp(mod.value)),
                 with: (mod.regex ? mod.with : escapeRegExp(mod.with)),
-                regex: !!mod.regex
+                regex: !!(+mod.regex)
             });
         case 4:
             return mod.subject + '';
         case 5:
-            return JSON.stringify({ prepend: mod.prepend, value: mod.value });
+            return JSON.stringify({ prepend: !!(+mod.prepend), value: mod.value });
     }
 
 };
