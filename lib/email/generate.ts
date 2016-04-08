24c47abf4df6545a931cce05fa7767e7c8795df2
Mr. Xyfir <mr@xyfir.com>
2016-04-08 10:45:04 -0700

    Change .__words -> ['__words'] to avoid IDE errors. Prepopulate __words to avoid calling RandWord during development

--- lib/email/generate.ts
+++ lib/email/generate.ts
@@ -1,7 +1,7 @@
 ﻿import * as request from "request";
 
-if (global.__words === undefined) {
-    global.__words = [];
+if (global["__words"] === undefined) {
+    global["__words"] = ["hello", "world", "email"]; // ** empty
 }
 
 export = function (cn, fn: Function) {
@@ -9,28 +9,26 @@ export = function (cn, fn: Function) {
     const generate = (email: string) => {
         email += String(Date.now()).substr(-2);
 
-        let sql: string = `
-            SELECT email_id FROM redirect_emails WHERE address = ?
-        `;
+        let sql: string = `SELECT email_id FROM redirect_emails WHERE address = ?`;
         cn.query(sql, [email + "@ptorx.com"], (err, rows) => {
             if (!!rows.length)
                 generate(email);
             else
-                fn(email);
+                fn(email + "@ptorx.com");
         });
     };
 
     // Load 500 words from Xyfir's RandWord API
-    if (global.__words.length === 0) {
+    if (global["__words"].length === 0) {
         let url: string = require("../../config").addresses.randword;
 
         request.get(url + "?count=500", (err, response, body) => {
-            global.__words = JSON.parse(body).words;
-            generate(global.__words.pop());
+            global["__words"] = JSON.parse(body).words;
+            generate(global["__words"].pop());
         });
     }
     else {
-        generate(global.__words.pop());
+        generate(global["__words"].pop());
     }
 
 };
