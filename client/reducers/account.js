fa98a58deb0f591ca8533207bbc9e16ab713682d
Mr. Xyfir <mr@xyfir.com>
2016-04-09 13:54:13 -0700

    Add 'purchase subscription', 'add email', 'delete email' reducers

---
+++ client/reducers/account.js
@@ -0,0 +1,24 @@
+import { PURCHASE_SUBSCRIPTION } from "../actions/types/account/subscription";
+import { ADD_EMAIL, DELETE_EMAIL } from "../actions/types/account/email";
+
+export default function(state, action) {
+    switch (action.type) {
+        case PURCHASE_SUBSCRIPTION:
+            return Object.assign({}, state, {
+                subscription: action.subscription
+            });
+
+        case ADD_EMAIL:
+            return Object.assign({}, state, {
+                emails: state.emails.concat([{ id: action.id, email: action.email }])
+            });
+
+        case DELETE_EMAIL:
+            return Object.assign({}, state, {
+                emails: state.emails.filter(email => { return email.id != action.id })
+            });
+            
+        default:
+            return state;
+    }
+}
