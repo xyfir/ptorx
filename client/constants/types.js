e9b6709929f0ddeb6eefb971b6712721e83b7492
Mr. Xyfir <mr@xyfir.com>
2016-04-09 16:15:28 -0700

    Add filter/modifier types

---
+++ client/constants/types.js
@@ -0,0 +1,16 @@
+export const filterTypes = {
+    '1': "Subject",
+    '2': "Address",
+    '3': "Domain",
+    '4': "Text Content",
+    '5': "HTML Content",
+    '6': "Header"
+};
+
+export const modifierTypes = {
+    '1': "Encrypt",
+    '2': "Text Only",
+    '3': "Find and Replace",
+    '4': "Subject Overwrite",
+    '5': "Subject Tag"
+};
