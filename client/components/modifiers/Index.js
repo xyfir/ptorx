546dbfa3001c1c78a8e47717cb87d82b14333ad6
Mr. Xyfir <mr@xyfir.com>
2016-04-09 16:09:47 -0700

    Add Index components for emails/filters/modifiers

---
+++ client/components/modifiers/Index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// Components
+import Create from "./Create";
+import Edit from "./Edit";
+import List from "./List";
+
+export default class Modifiers extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        switch (this.props.data.view.split('/')[1]) {
+            case "CREATE":
+                return <Create data={this.props.data} dispatch={this.props.dispatch} />;
+            case "EDIT":
+                return <Edit data={this.props.data} dispatch={this.props.dispatch} />;
+            case "LIST":
+                return <List data={this.props.data} dispatch={this.props.dispatch} />;
+        }
+    }
+
+}
