65c23e05acfaadc22590c6a9e37f88724b12f0d5
Mr. Xyfir <mr@xyfir.com>
2016-04-09 12:34:47 -0700

    Add 'add email', 'delete email' actions

---
+++ client/actions/creators/account/email.js
@@ -0,0 +1,13 @@
+import { ADD_EMAIL, DELETE_EMAIL } from "../../types/account/email";
+
+export function addEmail(id, email) {
+    return {
+        type: ADD_EMAIL, id, email
+    };
+}
+
+export function deleteEmail(id) {
+    return {
+        type: DELETE_EMAIL, id
+    };
+}
---
+++ client/actions/types/account/email.js
@@ -0,0 +1,2 @@
+export const DELETE_EMAIL = "ACCOUNT/DELETE_EMAIL";
+export const ADD_EMAIL = "ACCOUNT/ADD_EMAIL";
