2ca7fda3c2cf6e52561763517b66507dfee14394
Mr. Xyfir <mr@xyfir.com>
2016-04-10 11:03:53 -0700

    Add component for listing redirect emails

---
+++ client/components/emails/List.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+// Action creators
+import {
+    loadEmails, deleteEmail
+} from "../../actions/creators/emails";
+
+// Constants
+import { URL } from "../../constants/config";
+
+export default class ModifierList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        if (props.data.emails.length == 0) {
+            ajax({
+                url: URL + "api/emails", success: (res) => {
+                    this.props.dispatch(loadEmails(res.emails));
+                }
+            });
+        }
+    }
+
+    onDeleteEmail(id) {
+        swal({
+            title: "Are you sure?",
+            text: "You will no longer receive emails sent to this address.",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: false
+        }, () => {
+            ajax({
+                url: URL + "api/modifiers/" + id,
+                method: "DELETE", success: (res) => {
+                    if (res.error) {
+                        swal("Error", "Could not delete email", "error");
+                    }
+                    else {
+                        this.props.dispatch(deleteEmail(id));
+                    }
+                }
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="emails">
+                <a href="#emails/create" className="btn btn-primary">Create an Email</a>
+                <div className="list">{
+                    this.props.data.emails.map(email => {
+                        return (
+                            <div className="modifier">
+                                <span className="name"><a href={`#emails/edit/${email.id}`}>
+                                    {email.name}
+                                </a></span>
+                                <span
+                                    className="icon-trash"
+                                    title="Delete Email"
+                                    onClick={this.onDeleteEmail.bind(this, email.id) }
+                                />
+                                <span className="address">{mod.address}</span>
+                                <hr />
+                                <span className="description">{email.description}</span>
+                            </div>
+                        );
+                    })
+                }</div>
+            </div>
+        );
+    }
+
+}
