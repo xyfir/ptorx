d27c97a9eb65d135816ee824e05d161c2f79c025
Mr. Xyfir <mr@xyfir.com>
2016-04-09 17:18:30 -0700

    Add modifiers reducers

---
+++ client/reducers/modifiers.js
@@ -0,0 +1,31 @@
+import {
+    LOAD_MODIFIERS, ADD_MODIFIER, DELETE_MODIFIER, EDIT_MODIFIER
+} from "../actions/types/modifiers";
+
+export default function(state, action) {
+    switch (action.type) {
+        case LOAD_MODIFIERS:
+            return action.modifiers;
+
+        case ADD_MODIFIER:
+            return state.concat([action.data]);
+
+        case EDIT_MODIFIER:
+            return (() => {
+                let temp = Object.assign({}, state);
+                
+                temp.forEach((mod, i) => {
+                    if (action.data.id == mod.id)
+                        temp[i] = action.data;
+                });
+
+                return temp;
+            }).call();
+
+        case DELETE_MODIFIER:
+            return state.filter(mod => { return mod.id != action.id });
+            
+        default:
+            return state;
+    }
+}
