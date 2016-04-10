6720d0d23024f875da98b91bb8a14585852ba94e
Mr. Xyfir <mr@xyfir.com>
2016-04-09 17:17:27 -0700

    Add change view and modifiers reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,4 +1,5 @@
 // Reducers
+import modifiers from "./modifiers";
 import account from "./account";
 
 import { INITIALIZE_STATE, CHANGE_VIEW } from "../actions/types/";
@@ -9,8 +10,11 @@ export default function (state, action) {
         return action.state;
     else if (state == undefined)
         return {};
+    else if (action.type == CHANGE_VIEW)
+        return Object.assign({}, state, { view: action.view });
 
     return {
+        modifiers: modifiers(state.modifiers, action),
         account: account(state.account, action)
     };
 
