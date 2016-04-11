7d33ee3d987583d45a24e619e703a7a62d447ee8
Mr. Xyfir <mr@xyfir.com>
2016-04-11 12:10:46 -0700

    Add filters reducers

---
+++ client/reducers/filters.js
@@ -0,0 +1,31 @@
+import {
+    LOAD_FILTERS, ADD_FILTER, DELETE_FILTER, EDIT_FILTER
+} from "../actions/types/filters";
+
+export default function(state, action) {
+    switch (action.type) {
+        case LOAD_FILTERS:
+            return action.filters;
+
+        case ADD_FILTER:
+            return state.concat([action.data]);
+
+        case EDIT_FILTER:
+            return (() => {
+                let temp = Object.assign({}, state);
+                
+                temp.forEach((f, i) => {
+                    if (action.data.id == f.id)
+                        temp[i] = action.data;
+                });
+
+                return temp;
+            }).call();
+
+        case DELETE_FILTER:
+            return state.filter(f => { return f.id != action.id });
+            
+        default:
+            return state;
+    }
+}
