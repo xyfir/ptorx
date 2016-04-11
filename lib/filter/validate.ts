23829f06fbe09afb35dc03efd97c737f8c230b9c
Mr. Xyfir <mr@xyfir.com>
2016-04-10 19:01:26 -0700

    Add string error messages

---
+++ lib/filter/validate.ts
@@ -0,0 +1,20 @@
+﻿export = function (filter): string {
+    
+    if (!filter.type)
+        return "Invalid type";
+
+    else if (!(filter.name || "").match(/^[\w\d -]{1,40}$/))
+        return "Invalid name characters or length";
+
+    else if (!filter.find || (filter.find + '').length > 250)
+        return "Missing find query or invalid length. Limit 250 characters";
+
+    else if ((filter.description || '').length > 150)
+        return "Missing description or invalid length. Limit 150 characters";
+
+    else if (filter.type == 6 && filter.find.indexOf(":::") === -1)
+        return "Bad header filter";
+
+    else return "ok";
+
+};
