411978f8d677a1edb3550750fed72f2c459ebf97
Mr. Xyfir <mr@xyfir.com>
2016-04-11 12:08:06 -0700

    Add filter actions

---
+++ client/actions/creators/filters.js
@@ -0,0 +1,27 @@
+import {
+    LOAD_FILTERS, ADD_FILTER, DELETE_FILTER, EDIT_FILTER
+} from "../types/filters";
+
+export function loadFilters(filters) {
+    return {
+        type: LOAD_FILTERS, filters
+    };
+}
+
+export function deleteFilter(id) {
+    return {
+        type: DELETE_FILTER, id
+    };
+}
+
+export function addFilter(data) {
+    return {
+        type: ADD_FILTER, data
+    };
+}
+
+export function editFilter(data) {
+    return {
+        type: EDIT_FILTER, data
+    };
+}
---
+++ client/actions/types/filters.js
@@ -0,0 +1,4 @@
+export const DELETE_FILTER = "FILTERS/DELETE";
+export const LOAD_FILTERS = "FILTERS/LOAD";
+export const EDIT_FILTER = "FILTERS/EDIT";
+export const ADD_FILTER = "FILTERS/ADD";
