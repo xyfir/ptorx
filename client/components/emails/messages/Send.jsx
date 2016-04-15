c2d3e75fdd13d48fb15376f4fcf88caede81d8eb
Mr. Xyfir <mr@xyfir.com>
2016-04-15 15:17:40 -0700

    Add component for sending emails from a Ptorx address

---
+++ client/components/emails/messages/Send.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default class SendMessage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = { id: location.hash.split('/')[2] };
+        
+        this.onSend = this.onSend.bind(this);
+    }
+    
+    onSend() {
+        ajax({
+            url: `${URL}api/emails/${this.state.id}/messages/`,
+            method: "POST", data: {
+                to: this.refs.to.value, subject: this.refs.subject.value,
+                content: this.refs.content.value
+            }, success: (res) => {
+                if (res.error) {
+                    swal("Error", res.message, "error");
+                }
+                else {
+                    swal("Success", `Message sent to ${this.refs.to.value}`, "success");
+                    location.hash = `emails/messages/${this.state.id}/list`;
+                }
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div className="message-send">
+                <nav className="nav-bar-sub">
+                    <a href={`#emails/messages/${this.state.id}/list`}>Messages</a>
+                    <a href={`#emails/list`}>Emails</a>
+                </nav>
+                
+                <hr />
+                
+                <input type="text" ref="to" placeholder="To" />
+                
+                <input type="text" ref="subject" placeholder="Subject" />
+                
+                <textarea ref="content"></textarea>
+                
+                <button onClick={this.onSend} className="btn-primary">
+                    Send Message
+                </button>
+            </div>
+        );
+    }
+    
+}
