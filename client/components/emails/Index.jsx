03e8c42e846ad64ea341d190dd3a87b758c61903
Mr. Xyfir <mr@xyfir.com>
2016-04-15 11:28:01 -0700

    Add MESSAGES case that outputs Messages component

--- client/components/emails/Index.jsx
+++ client/components/emails/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 // Components
+import Messages from "./messages/";
 import Create from "./Create";
 import Edit from "./Edit";
 import List from "./List";
@@ -13,6 +14,8 @@ export default class Emails extends React.Component {
 
     render() {
         switch (this.props.data.view.split('/')[1]) {
+            case "MESSAGES":
+                return <Messages data={this.props.data} dispatch={this.props.dispatch} />;
             case "CREATE":
                 return <Create data={this.props.data} dispatch={this.props.dispatch} />;
             case "EDIT":
