1b6b3f1efcf3cb6f7443fd71d081a43bb07fc47e
MrXyfir <mr@xyfir.com>
2016-11-15 12:44:54 -0800

    Clean up code

--- client/components/emails/Create.jsx
+++ client/components/emails/Create.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 // Components
-import LinkModifier from "../../components/modifiers/Link";
-import LinkFilter from "../../components/filters/Link";
+import LinkModifier from "components/modifiers/Link";
+import LinkFilter from "components/filters/Link";
 
 // Action creators
-import { loadModifiers } from "../../actions/creators/modifiers";
-import { loadFilters } from "../../actions/creators/filters";
-import { addEmail } from "../../actions/creators/emails";
+import { loadModifiers } from "actions/creators/modifiers";
+import { loadFilters } from "actions/creators/filters";
+import { addEmail } from "actions/creators/emails";
 
 // Constants
-import { URL } from "../../constants/config";
-import { filterTypes, modifierTypes } from "../../constants/types";
+import { URL } from "constants/config";
+import { filterTypes, modifierTypes } from "constants/types";
 
 // Modules
-import request from "../../lib/request";
+import request from "lib/request";
 
 export default class CreateEmail extends React.Component {
 
@@ -102,10 +102,11 @@ export default class CreateEmail extends React.Component {
 
     onCreate() {
         let data = {
-            name: this.refs.name.value, description: this.refs.description.value, to: 0,
-            address: "", filters: this.state.filters.map(f => { return f.id; }),
+            name: this.refs.name.value, description: this.refs.description.value,
+            to: 0, address: "", saveMail: +false, noSpamFilter: +false,
             modifiers: this.state.modifiers.map(m => { return m.id; }),
-            saveMail: +false, noSpamFilter: +false, noToAddress: +false
+            filters: this.state.filters.map(f => { return f.id; }),
+            noToAddress: +false
         };
 
         const isPremium = this.props.data.account.subscription > Date.now();
@@ -149,11 +150,15 @@ export default class CreateEmail extends React.Component {
         return (
             <div className="email-create">
                 <label>Name</label>
-                <span className="input-description">Give your email a name to find it easier.</span>
+                <span className="input-description">
+                    Give your email a name to find it easier.
+                </span>
                 <input type="text" ref="name" />
                 
                 <label>Description</label>
-                <span className="input-description">Describe your email to find it easier.</span>
+                <span className="input-description">
+                    Describe your email to find it easier.
+                </span>
                 <input type="text" ref="description" />
                 
                 { // Ptorx Address
@@ -161,8 +166,14 @@ export default class CreateEmail extends React.Component {
                     ? (
                         <div>
                             <label>Address</label>
-                            <span className="input-description">Customize your Ptorx address or leave it blank for a randomly generated address.</span>
-                            <input type="text" ref="address" placeholder="user@ptorx.com" />
+                            <span className="input-description">
+                                Customize your Ptorx address or leave it blank for a randomly generated address.
+                            </span>
+                            <input
+                                type="text"
+                                ref="address"
+                                placeholder="user@ptorx.com"
+                            />
                         </div>
                     ) : (
                         <div />
@@ -170,7 +181,9 @@ export default class CreateEmail extends React.Component {
                 }
                 
                 <label>Redirect To</label>
-                <span className="input-description">This is your real email that messages sent to your Ptorx address will be redirected to.</span>
+                <span className="input-description">
+                    This is your real email that messages sent to your Ptorx address will be redirected to.
+                </span>
                 <select ref="to">{
                     this.props.data.account.emails.map(email => {
                         return <option value={email.id}>{email.address}</option>;
@@ -179,91 +192,127 @@ export default class CreateEmail extends React.Component {
                 
                 <hr />
                 
-                { // Advanced Settings (filters, modifiers, ...)
-                    this.state.showAdvanced
-                    ? (
-                        <div className="advanced-settings">
-                            <a onClick={this.onToggleShowAdvanced}>Hide Advanced Settings</a>
-                            
-                            <label>Spam Filter</label>
-                            <span className="input-description">By default we block any messages that are marked as spam. Disable this only if you believe legitimate messages are being blocked by the spam filter.</span>
-                            <input type="checkbox" ref="spamFilter" defaultChecked={true} />Enable
-                            
-                            { // Save Mail, No To Address
-                                isPremium
-                                ? (
-                                    <div>
-                                        <label>Save Mail</label>
-                                        <span className="input-description">Any emails that are sent to this address will be temporarily stored for 3 days. You can then access the messages by viewing the <em>Messages</em> section when viewing this email's info. <strong>Note:</strong> This is required if you want to reply to emails.</span>
-                                        <input type="checkbox" ref="saveMail" />Enable
-                                        
-                                        <label>No 'To' Address</label>
-                                        <span className="input-description">Enabling this will allow you to avoid having emails sent to your Ptorx address redirected to your real email. This will act like the <em>Save Mail</em> feature just without the emails being redirected.</span>
-                                        <input type="checkbox" ref="noToAddress" />Enable
+                {this.state.showAdvanced ? (
+                    <div className="advanced-settings">
+                        <a onClick={this.onToggleShowAdvanced}>
+                            Hide Advanced Settings
+                        </a>
+                        
+                        <label>Spam Filter</label>
+                        <span className="input-description">
+                            By default we block any messages that are marked as spam. Disable this only if you believe legitimate messages are being blocked by the spam filter.
+                        </span>
+                        <input
+                            type="checkbox"
+                            ref="spamFilter"
+                            defaultChecked={true}
+                        />Enable
+                        
+                        { // Save Mail, No To Address
+                            isPremium ? (
+                                <div>
+                                    <label>Save Mail</label>
+                                    <span className="input-description">
+                                        Any emails that are sent to this address will be temporarily stored for 3 days. You can then access the messages by viewing the <em>Messages</em> section when viewing this email's info. <strong>Note:</strong> This is required if you want to reply to emails.
+                                    </span>
+                                    <input type="checkbox" ref="saveMail" />Enable
+                                    
+                                    <label>No 'To' Address</label>
+                                    <span className="input-description">
+                                        Enabling this will allow you to avoid having emails sent to your Ptorx address redirected to your real email. This will act like the <em>Save Mail</em> feature just without the emails being redirected.
+                                    </span>
+                                    <input type="checkbox" ref="noToAddress" />Enable
+                                </div>
+                            ) : (
+                                <div />
+                            )
+                        }
+
+                        <hr />
+
+                        <h3>Filters</h3>
+                        <p>Create or select filters for your new email to use.</p>
+                        <div className="linked-filters">{
+                            this.state.filters.map(filter => {
+                                return (
+                                    <div className="filter">
+                                        <span className="type">{
+                                            filterTypes[filter.type]
+                                        }</span>
+                                        <span className="name">{
+                                            filter.name
+                                        }</span>
+                                        <span
+                                            className="icon-trash"
+                                            title="Remove Filter"
+                                            onClick={
+                                                this.onRemoveFilter
+                                                    .bind(this, filter.id)
+                                            }
+                                        />
+                                        <span className="description">{
+                                            filter.description
+                                        }</span>
                                     </div>
-                                ) : (
-                                    <div />
-                                )
-                            }
+                                );
+                            })
+                        }</div>
 
-                            <hr />
+                        <LinkFilter
+                            data={this.props.data}
+                            onAdd={this.onAddFilter}
+                        />
 
-                            <h3>Filters</h3>
-                            <p>Create or select filters for your new email to use.</p>
-                            <div className="linked-filters">{
-                                this.state.filters.map(filter => {
-                                    return (
-                                        <div className="filter">
-                                            <span className="type">{filterTypes[filter.type]}</span>
-                                            <span className="name">{filter.name}</span>
-                                            <span
-                                                className="icon-trash"
-                                                title="Remove Filter"
-                                                onClick={this.onRemoveFilter.bind(this, filter.id) }
-                                            />
-                                            <span className="description">{filter.description}</span>
-                                        </div>
-                                    );
-                                })
-                            }</div>
+                        <hr />
 
-                            <LinkFilter data={this.props.data} onAdd={this.onAddFilter} />
+                        <h3>Modifiers</h3>
+                        <p>
+                            Create or select modifiers for your new email to use.
+                            <br />
+                            <strong>Note:</strong> The order in which the modifiers are listed are the order in which they are applied to emails.
+                        </p>
+                        <div className="linked-modifiers">{
+                            this.state.modifiers.map(mod => {
+                                return (
+                                    <div className="modifier">
+                                        <span className="type">{
+                                            modifierTypes[mod.type]
+                                        }</span>
+                                        <span className="name">{
+                                            mod.name
+                                        }</span>
+                                        <span
+                                            className="icon-trash"
+                                            title="Remove Modifier"
+                                            onClick={
+                                                this.onRemoveModifier
+                                                    .bind(this, mod.id)
+                                            }
+                                        />
+                                        <span className="description">{
+                                            mod.description
+                                        }</span>
+                                    </div>
+                                );
+                            })
+                        }</div>
 
-                            <hr />
-
-                            <h3>Modifiers</h3>
-                            <p>
-                                Create or select modifiers for your new email to use.
-                                <br />
-                                <strong>Note:</strong> The order in which the modifiers are listed are the order in which they are applied to emails.
-                            </p>
-                            <div className="linked-modifiers">{
-                                this.state.modifiers.map(mod => {
-                                    return (
-                                        <div className="modifier">
-                                            <span className="type">{modifierTypes[mod.type]}</span>
-                                            <span className="name">{mod.name}</span>
-                                            <span
-                                                className="icon-trash"
-                                                title="Remove Modifier"
-                                                onClick={this.onRemoveModifier.bind(this, mod.id) }
-                                            />
-                                            <span className="description">{mod.description}</span>
-                                        </div>
-                                    );
-                                })
-                            }</div>
-
-                            <LinkModifier data={this.props.data} onAdd={this.onAddModifier} />
-                        </div>
-                    ) : (
-                        <a onClick={this.onToggleShowAdvanced}>Show Advanced Settings</a>
-                    )
-                }
+                        <LinkModifier
+                            data={this.props.data}
+                            onAdd={this.onAddModifier}
+                        />
+                    </div>
+                ) : (
+                    <a onClick={this.onToggleShowAdvanced}>
+                        Show Advanced Settings
+                    </a>
+                )}
                 
                 <hr />
                 
-                <button className="btn-primary" onClick={this.onCreate}>Create Email</button>
+                <button className="btn-primary" onClick={this.onCreate}>
+                    Create Email
+                </button>
             </div>
         );
     }
