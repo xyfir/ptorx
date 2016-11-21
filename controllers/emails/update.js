0770f019e5e7aae32817db2072786b86a6fcd4f7
MrXyfir <mr@xyfir.com>
2016-11-20 17:01:04 -0800

    Support ordering of linked modifiers. Clean up code

--- controllers/emails/update.js
+++ controllers/emails/update.js
@@ -14,7 +14,8 @@ let mailgun = require("mailgun-js")({
 /*
     PUT api/emails/:email
     REQUIRED
-        name: string, description: string, filters: string, modifiers: string, to: number
+        name: string, description: string, filters: string,
+        modifiers: string, to: number
     OPTIONAL
         saveMail: boolean, noSpamFilter: boolean, noToAddress: boolean
     RETURN
@@ -31,17 +32,18 @@ module.exports = function(req, res) {
         return;
     }
 
-    let sql = "";
+    let sql = "", vars = [];
 
     db(cn => {
         /* Extended Validation */
         const step1 = () => {
             sql = `
                 SELECT mg_route_id as mgRouteId, address, (
-                    SELECT COUNT(email_id) FROM main_emails WHERE email_id = ? AND user_id = ?
-                ) as toEmail FROM redirect_emails WHERE email_id = ? AND user_id = ?
-            `;
-            let vars = [
+                    SELECT COUNT(email_id) FROM main_emails
+                    WHERE email_id = ? AND user_id = ?
+                ) as toEmail FROM redirect_emails
+                WHERE email_id = ? AND user_id = ?
+            `, vars = [
                 req.body.to, req.session.uid,
                 req.params.email, req.session.uid
             ];
@@ -50,12 +52,16 @@ module.exports = function(req, res) {
                 // toEmail doesn't exist and user didn't provide noToAddress
                 if (!rows[0].toEmail && !(+req.body.noToAddress)) {
                     cn.release();
-                    res.json({ error: true, message: "Could not find main email" });
+                    res.json({
+                        error: true, message: "Could not find main email"
+                    });
                 }
                 // Redirect email doesn't exist
                 else if (rows[0].address == null) {
                     cn.release();
-                    res.json({ error: true, message: "Could not find redirect email" });
+                    res.json({
+                        error: true, message: "Could not find redirect email"
+                    });
                 }
                 // Validate filters and modifiers
                 else {
@@ -75,7 +81,9 @@ module.exports = function(req, res) {
                                 }
                                 else {
                                     step2({
-                                        address: rows[0].address, routeId: rows[0].mgRouteId, filters, modifiers
+                                        address: rows[0].address,
+                                        routeId: rows[0].mgRouteId,
+                                        filters, modifiers
                                     });
                                 }
                             });
@@ -91,8 +99,7 @@ module.exports = function(req, res) {
             sql = `
                 UPDATE redirect_emails SET to_email = ?, name = ?, description = ?,
                 save_mail = ?, spam_filter = ? WHERE email_id = ?
-            `;
-            let vars = [
+            `, vars = [
                 req.body.to, req.body.name, req.body.description,
                 !!(+req.body.saveMail), !(+req.body.noSpamFilter), req.params.email
             ];
@@ -100,23 +107,27 @@ module.exports = function(req, res) {
             cn.query(sql, vars, (err, result) => {
                 if (err || !result.affectedRows) {
                     cn.release();
-                    res.json({ error: true, message: "An unknown error occured" });
-                    return;
+                    res.json({
+                        error: true, message: "An unknown error occured"
+                    }); return;
                 }
 
                 // Build MailGun route expression(s)
                 buildExpression(data.address, data.filters, cn, (expression) => {
                     // Update MailGun route
                     mailgun.routes(data.routeId).update({
-                        priority: (!(+req.body.noSpamFilter) ? 2 : 0), description: "",
-                        expression, action: buildAction(
+                        priority: (!(+req.body.noSpamFilter) ? 2 : 0),
+                        description: "", expression,
+                        action: buildAction(
                             req.params.email, req.session.subscription,
                             data.to_email == 0 || data.save_mail
                         )
                     }, (err, body) => {
                         if (err) {
                             cn.release();
-                            res.json({ error: true, message: "An unknown error occured" });
+                            res.json({
+                                error: true, message: "An unknown error occured"
+                            });
                         }
                         else {
                             step3(data.filters, data.modifiers);
@@ -128,21 +139,40 @@ module.exports = function(req, res) {
 
         /* Create entries in linked_modifiers|filters */
         const step3 = (filters, modifiers) => {
-            // Filters/modifiers already linked will not be inserted twice due to unique constraint
+            // Filters already linked will not be inserted twice due to unique constraint
             sql = "INSERT INTO linked_filters (filter_id, email_id) VALUES ";
-            filters.forEach(filter => sql += `('${+filter}', '${+req.params.email}'), `);
+            filters.forEach(filter => {
+                sql += `('${+filter}', '${+req.params.email}'), `;
+            });
 
             cn.query(sql.substr(0, sql.length - 2), (err, result) => {
-                sql = "INSERT INTO linked_modifiers (modifier_id, email_id) VALUES ";
-                modifiers.forEach(modifier => sql += `('${+modifier}', '${+req.params.email}'), `);
+                // Delete all modifiers since the order may have changed
+                sql = `
+                    DELETE FROM linked_modifiers WHERE email_id = ?
+                `, vars = [
+                    req.params.email
+                ];
 
-                cn.query(sql.substr(0, sql.length - 2), (err, result) => {
-                    // Filters/Modifiers that have been removed will be deleted via NOT IN (...)
-                    sql = "DELETE FROM linked_filters WHERE email_id = ? AND filter_id NOT IN (?)";
+                cn.query(sql, vars, (err, result) => {
+                    // Insert modifiers
+                    sql = `
+                        INSERT INTO linked_modifiers
+                        (modifier_id, email_id, order_number) VALUES 
+                    `;
+                    modifiers.forEach((modifier, i) => {
+                        sql += `('${+modifier}', '${+req.params.email}', '${i}'), `
+                    });
 
-                    cn.query(sql, [filters.join(", "), req.params.email], (err, result) => {
-                        sql = "DELETE FROM linked_modifiers WHERE email_id = ? AND modifier_id NOT IN (?)";
-                        cn.query(sql, [modifiers.join(", "), req.params.email], (err, result) => {
+                    cn.query(sql.substr(0, sql.length - 2), (err, result) => {
+                        // Filters that have been removed will be deleted via NOT IN (...)
+                        sql = `
+                            DELETE FROM linked_filters
+                            WHERE email_id = ? AND filter_id NOT IN (?)
+                        `, vars = [
+                            req.params.email, filters
+                        ];
+
+                        cn.query(sql, vars, (err, result) => {
                             cn.release();
                             clearCache(req.params.email);
                             res.json({ error: false });
