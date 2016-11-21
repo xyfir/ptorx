74db819226b80a6ba7dd4d2f2d01d796dc2fce15
MrXyfir <mr@xyfir.com>
2016-11-21 10:07:47 -0800

    Update to support more arguments. Improve code

--- client/lib/request.js
+++ client/lib/request.js
@@ -1,39 +1,57 @@
-export default function(options) {
+export default function() {
+
+    let options = {
+        url: "", fn: () => {}, method: "GET", data: {}, dataType: "json"
+    };
+
+    // request("url", (res) => {})
+    if (typeof arguments[0] == "string") {
+        options.url = arguments[0];
+        options.fn = arguments[1];
+    }
+    // request({ url, success|fn, ?method, ?data, ?dataType })
+    else if (arguments.length == 1) {
+        Object.assign(options, arguments[0]);
+
+        // Old version used success() for callback
+        if (options.success) options.fn = options.success;
+    }
+    // request({ url, ?method, ?data, ?dataType }, (res) => {})
+    else {
+        Object.assign(options, arguments[0]);
+        options.fn = arguments[1];
+    }
     
     let request = new XMLHttpRequest();
     
     // Set method and URL
-    request.open(options.method || "GET", options.url, true);
+    request.open(options.method, options.url, true);
     
     // Handle response
     request.onload = function() {
         if (this.status >= 200 && this.status < 400) {
             switch (options.dataType) {
                 case "text":
-                    options.success(this.responseText, this.status); break;
+                    options.fn(this.responseText, this.status); break;
                 default:
-                    options.success(JSON.parse(this.responseText), this.status);
+                    options.fn(JSON.parse(this.responseText), this.status);
             }
-        }
-        else if (options.error) {
-            options.error(this.responseText, this.status);
         }
     };
     
     // Send request + data
-    if (options.method == undefined || options.method == "GET") {
+    if (options.method == "GET") {
         request.send();
     }
     else {
-        let query = "";
+        const query =
+            Object.keys(options.data).map(key => {
+                return key + "=" + encodeURIComponent(options.data[key]);
+            }).join("&");
         
-        for (let key in options.data) {
-            query += encodeURIComponent(key) + "=" + encodeURIComponent(options.data[key]) + "&";
-        }
-        
-        query = query.substr(0, query.length - 1);
-        
-        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setRequestHeader(
+            "Content-Type", "application/x-www-form-urlencoded"
+        );
         request.send(query);
     }
     
