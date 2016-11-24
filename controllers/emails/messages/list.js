8091bb828cced1057af6d17ca8a43b99bca6279f
MrXyfir <mr@xyfir.com>
2016-11-24 11:35:14 -0800

    Support returning accepted or rejected messages

--- controllers/emails/messages/list.js
+++ controllers/emails/messages/list.js
@@ -2,26 +2,35 @@
 
 /*
     GET api/emails/:email/messages
+    OPTIONAL
+        rejected: boolean
     RETURN
         { messages: [{ id: string, received: number, subject: string }] }
     DESCRIPTION
-        Return basic data on any stored messages for email
+        Return basic data on any stored messages for :email
+        Returns accepted or rejected messages based on req.query.rejected
 */
 module.exports = function(req, res) {
 
     let sql = `
         DELETE FROM messages WHERE received + 255600 < UNIX_TIMESTAMP()
-    `;
+    `, vars = [];
 
     db(cn => cn.query(sql, (err, result) => {
         sql = `
-            SELECT message_id as id, received, subject FROM messages
-            WHERE email_id IN (
-                SELECT email_id FROM redirect_emails WHERE email_id = ? AND user_id = ?
-            )
-        `;
+            SELECT
+                m.message_id as id, m.received, m.subject
+            FROM
+                messages AS m, redirect_emails AS re
+            WHERE
+                m.email_id = re.email_id AND m.rejected = ?
+                AND re.email_id = ? AND re.user_id = ?
+        `, vars = [
+            !!+req.query.rejected,
+            req.params.email, req.session.uid
+        ];
 
-        cn.query(sql, [req.params.email, req.session.uid], (err, rows) => {
+        cn.query(sql, vars, (err, rows) => {
             cn.release();
 
             res.json({ messages: (err || !rows.length) ? [] : rows });
