db2caf3e56f14a0c24e6d2f2b45862f126aa8325
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-12-17 19:46:15 -0800

    Add 'send subscription expiration emails' cron job

---
+++ cron/send-subscription-expiration-emails.js
@@ -0,0 +1,62 @@
+const sendEmail = require("lib/email/send");
+const db = require("db");
+
+const config = require("config");
+const mailgun = require("mailgun-js")({
+    apiKey: config.keys.mailgun, domain: "ptorx.com"
+});
+
+module.exports = (fn) => db(cn => {
+
+    let sql = `
+        SELECT
+            UNIX_TIMESTAMP(DATE_ADD(CURDATE(), INTERVAL 1 DAY)) * 1000
+                AS startrange,
+            UNIX_TIMESTAMP(DATE_ADD(CURDATE(), INTERVAL 3 DAY)) * 1000
+                AS endrange
+    `;
+
+    cn.query(sql, (err, rows) => {
+        if (err) {
+            cn.release();
+            fn(true);
+            return;
+        }
+
+        sql = `
+            SELECT email, subscription, trial
+            FROM users
+            WHERE
+                subscription >= ${rows[0].startrange}
+                AND subscription <= ${rows[0].endrange}
+        `;
+        
+        cn.query(sql)
+            .on("error", () => {
+                cn.release();
+                fn(true);
+            })
+            .on("result", (row) => {
+                cn.pause();
+
+                sendEmail(
+                    row.email,
+                    "Your subscription is expiring soon",
+                    `
+                        Your <a href="https://ptorx.com">Ptorx</a> subscription is expiring soon! Increase your subscription in your <a href="https://ptorx.com/panel/#account/purchase-subscription">user panel</a> before it's too late.
+                        <br /><br />
+                        If your subscription expires, any proxy emails you have created will be deleted and you will no longer receive mail from them. Additionally, these deleted emails will <em>not</em> be able to be created again should you decide to purchase a new subscription after expiration.
+                        <br /><br />
+                        Your subscription will expire after <strong>${(new Date(row.subscription)).toLocaleString()}</strong> (server time).
+                        <br />
+                        Current server time is <strong>${(new Date()).toLocaleString()}</strong> (at time of sending this email).
+                        <br /><br />
+                        Thank you for being a Ptorx subscriber! We hope you'll stick around a bit longer. Have any questions? Send a reply to this email or use our <a href="https://xyfir.com/#/contact">contact form</a>.
+                    `,
+                    () => cn.resume()
+                );
+            })
+            .on("end", () => cn.release());
+    });
+
+});
