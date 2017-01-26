33232ad5e4e8ad0d286ca7f104bd1babc5e5cd41
MrXyfir <mr@xyfir.com>
2017-01-25 17:10:47 -0800

    Use 'deviceready' listener

--- www/app.js
+++ www/app.js
@@ -1,1 +1,5 @@
-window.location.href = "https://ptorx.com/panel/#?phonegap=1";
+function onDeviceReady() {
+    window.location.href = "https://ptorx.com/panel/#?phonegap=1";
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
