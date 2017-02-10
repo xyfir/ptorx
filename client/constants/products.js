e8d5a5c23b25b5f86c4755ca2a65a8eb8d171ab9
MrXyfir <mr@xyfir.com>
2017-02-10 13:36:24 -0800

    Convert to object with android and ios properties

--- client/constants/products.js
+++ client/constants/products.js
@@ -1,5 +1,15 @@
-export default [
-    "com.xyfir.ptorx.premium.30days",
-    "com.xyfir.ptorx.premium.182days",
-    "com.xyfir.ptorx.premium.365days"
-];
+export default {
+  
+  android: [
+    'com.xyfir.ptorx.premium.30days',
+    'com.xyfir.ptorx.premium.182days',
+    'com.xyfir.ptorx.premium.365days'
+  ],
+
+  ios: [
+    'com.xyfir.ptorx.premium.30',
+    'com.xyfir.ptorx.premium.182',
+    'com.xyfir.ptorx.premium.365'
+  ]
+
+};
