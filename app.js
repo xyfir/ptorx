c044d0b787e40326f2a81ca3be436db82a21995d
MrXyfir <mr@xyfir.com>
2017-05-03 15:24:49 -0700

    Add moment. Change code style

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-require("app-module-path").addPath(__dirname);
+require('app-module-path').addPath(__dirname);
 
-const express =  require("express");
-const session = require("express-session");
-const parser = require("body-parser");
+const SessionStore = require('express-mysql-session');
+const express = require('express');
+const session = require('express-session');
+const parser = require('body-parser');
+const moment = require('moment');
+const app = express();
 
-let sstore = require("express-mysql-session");
-let app = express();
+const config = require('./config');
 
-let config = require("./config");
-app.listen(config.environment.port, () => {
-    console.log("~~Server running on port", config.environment.port);
-});
+app.listen(config.environment.port, () =>
+  console.log('~~Server running on port', config.environment.port)
+);
 
 /* Sessions */
-let sessionStore = new sstore({
-    host: config.database.mysql.host,
-    port: config.database.mysql.port,
-    user: config.database.mysql.user,
-    password: config.database.mysql.password,
-    database: config.database.mysql.database,
-    useConnectionPooling: true
-});
-app.use(session({
+app.use(
+  session({
     secret: config.keys.session,
-    store: sessionStore,
+    store: new SessionStore({
+      host: config.database.mysql.host,
+      port: config.database.mysql.port,
+      user: config.database.mysql.user,
+      password: config.database.mysql.password,
+      database: config.database.mysql.database,
+      useConnectionPooling: true
+    }),
     saveUninitialized: true,
     resave: true,
     cookie: {
-        httpOnly: false
+      httpOnly: false
     }
-}));
+  })
+);
 
 /* Body Parser */
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 // Express middleware / controllers
-app.use("/static", express.static(__dirname + "/static"));
-app.get("/panel/*", (req, res) => {
-    res.sendFile(__dirname + "/views/Panel.html");
-});
-app.use("/api", require("./controllers/"));
-app.get("/*", (req, res) => {
-    if (config.environment.type == "dev") {
-        req.session.uid = 1,
-        req.session.subscription = Date.now() + (1000 * 60 * 60);
-    }
-    res.sendFile(__dirname + "/views/Home.html");
+app.use('/static', express.static(__dirname + '/static'));
+app.get('/panel/*', (req, res) =>
+  res.sendFile(__dirname + '/views/Panel.html')
+);
+app.use('/api', require('./controllers/'));
+app.get('/*', (req, res) => {
+  if (config.environment.type == 'dev') {
+    req.session.uid = 1,
+    req.session.subscription = moment().add(7, 'days').unix() * 1000;
+  }
+
+  res.sendFile(__dirname + '/views/Home.html');
 });
 
-if (config.environment.runCronJobs)
-    require("./cron/start")();
+if (config.environment.runCronJobs) require('./cron/start')();
