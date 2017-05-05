713b6df219af5a618b8c6bba9cb926278e5ce25a
MrXyfir <mr@xyfir.com>
2017-05-05 13:38:29 -0700

    Improvements, changes
    - Change code style
    - Support `ignore` param
    - Add JSDOC documentation
    - Match email adresses

--- client/lib/find-matching.js
+++ client/lib/find-matching.js
@@ -1,16 +1,32 @@
-// Finds matching modifiers or filters
-// Type 0 matches all types
-export default function (items, search) {
-    
-    search.query = search.query.toLowerCase();
-    
-    return items.filter(item => {
-        if (item.type != search.type && search.type != 0 )
-            return false;
-        else if (item.name.toLowerCase().indexOf(search.query) > -1)
-            return true;
-        else if (item.description.toLowerCase().indexOf(search.query) > -1)
-            return true;
-    });
-    
+/**
+ * Finds matching modifiers or filters.
+ * @param {object[]} items - An array of objects that must contain `name` and 
+ * `description` string properties.
+ * @param {object} search - An object containing `type` and `query` properties.
+ * @param {object[]} [ignore] - Same as items. Items in `items` that match 
+ * `search` but are present in `ignore` are not returned. 
+ * @returns {object[]}
+ */
+export default function (items, search, ignore = []) {
+  
+  search.query = search.query.toLowerCase();
+  
+  return items.filter(item => {
+    // Is ignored
+    if (!!ignore.find(i => i.id == item.id))
+      return false;
+    // Type doesn't match and user is requesting specific type
+    else if (item.type != search.type && search.type != 0 )
+      return false;
+    // Name matches search
+    else if (item.name.toLowerCase().indexOf(search.query) > -1)
+      return true;
+    // Description matches search
+    else if (item.description.toLowerCase().indexOf(search.query) > -1)
+      return true;
+    // Email address property exists and matches search
+    else if (item.address && item.address.indexOf(search.query) > -1)
+      return true;
+  });
+  
 }
