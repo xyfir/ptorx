2fc0bd9ca9027180a235e512ba0fbbfc18659a54
MrXyfir <mr@xyfir.com>
2017-05-12 13:14:09 -0700

    Change code style

--- client/reducers/emails.js
+++ client/reducers/emails.js
@@ -1,31 +1,35 @@
 import {
-    LOAD_EMAILS, ADD_EMAIL, DELETE_EMAIL, EDIT_EMAIL
-} from "../actions/types/emails";
+  LOAD_EMAILS, ADD_EMAIL, DELETE_EMAIL, EDIT_EMAIL
+} from 'actions/types/emails';
 
 export default function(state, action) {
-    switch (action.type) {
-        case LOAD_EMAILS:
-            return action.emails;
 
-        case ADD_EMAIL:
-            return state.concat([action.data]);
+  console.log('reduce state', state, 'action', action);
+  
+  switch (action.type) {
+    case LOAD_EMAILS:
+      return action.emails;
 
-        case EDIT_EMAIL:
-            return (() => {
-                let temp = state.slice(0);
-                
-                temp.forEach((email, i) => {
-                    if (action.data.id == email.id)
-                        temp[i] = action.data;
-                });
+    case ADD_EMAIL:
+      return state.concat([action.data]);
 
-                return temp;
-            }).call();
+    case EDIT_EMAIL:
+      return (() => {
+        const temp = state.slice(0);
+        
+        temp.forEach((email, i) => {
+          if (action.data.id == email.id)
+            temp[i] = action.data;
+        });
 
-        case DELETE_EMAIL:
-            return state.filter(email => { return email.id != action.id });
-            
-        default:
-            return state;
-    }
+        return temp;
+      })();
+
+    case DELETE_EMAIL:
+      return state.filter(email => email.id != action.id);
+      
+    default:
+      return state;
+  }
+
 }
