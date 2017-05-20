0ea29b4962ae44ebad58e127934db5c63afd5f00
MrXyfir <mr@xyfir.com>
2017-05-20 16:46:23 -0700

    Use config.addresses.ptorx

--- controllers/account/logout.js
+++ controllers/account/logout.js
@@ -1,8 +1,10 @@
+const config = require('config');
+
 /*
   GET api/account/logout
 */
 module.exports = function(req, res) {
 
-  req.session.destroy(err => res.redirect('../../../'));
+  req.session.destroy(err => res.redirect(config.addresses.ptorx));
 
 };
