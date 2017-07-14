4961cd3d9ac512a2d5dce25fc6dc535ccae410b1
MrXyfir <mr@xyfir.com>
2017-07-14 15:46:13 -0700

    Improvements, changes
    - Update code style
    - Use superagent
    - Use lib/mysql
    - req.body.token -> stripeToken

--- controllers/account/stripe-purchase.js
+++ controllers/account/stripe-purchase.js
@@ -1,131 +1,109 @@
-const request = require("request");
-const stripe = require("stripe");
-const db = require("lib/db");
+const request = require('superagent');
+const stripe = require('stripe');
+const mysql = require('lib/mysql');
 
-const config = require("config");
+const subscriptions = require('subscriptions');
+const config = require('config');
 
 function setSubscription(subscription, days) {
-    return Date.now() > subscription
-        ? (Date.now() + ((60 * 60 * 24 * days) * 1000))
-        : (subscription + ((60 * 60 * 24 * days) * 1000));
-}
-
-// Give user who referred buyer one week subscription for every month bought
-function rewardReferrer(cn, ref, days) {
-    let sql = `
-        SELECT subscription FROM users WHERE user_id = ?
-    `, vars = [ref];
-
-    cn.query(sql, vars, (err, rows) => {
-        if (err || !rows.length) {
-            cn.release();
-        }
-        else {
-            sql = `
-                UPDATE users SET subscription = ? WHERE user_id = ?
-            `, vars = [
-                setSubscription(rows[0].subscription, (days / 30) * 7 ), ref
-            ];
-
-            cn.query(sql, vars, (err, result) => cn.release());
-        }
-    });
-}
-
-// Notify XACC of purchase
-function rewardAffiliate(aff, amount) {
-    request.post({
-        url: config.address.xacc + "api/affiliate/purchase", form: {
-            service: 13, serviceKey: config.keys.xacc,
-            promoCode: aff, amount
-        }
-    }, (err, response, body) => 1);
+  return Date.now() > subscription
+    ? (Date.now() + ((60 * 60 * 24 * days) * 1000))
+    : (subscription + ((60 * 60 * 24 * days) * 1000));
 }
 
 /*
-    POST api/account/stripe-purchase
-    REQUIRED
-        stripeToken: string, subscription: number
-    RETURN
-        { error: boolean, message?: string }
-    DESCRIPTION
-        Add months to user's subscription after charging card via Stripe
+  POST api/account/stripe-purchase
+  REQUIRED
+    token: string, subscription: number
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Add months to user's subscription after charging card via Stripe
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
 
     let sql = `
-        SELECT subscription, referral FROM users WHERE user_id = ?
-    `, vars = [
-        req.session.uid
+      SELECT subscription, referral FROM users WHERE user_id = ?
+    `,
+    vars = [
+      req.session.uid
+    ],
+    rows = await db.query(sql, vars);
+
+    if (!rows.length) throw 'Invalid user';
+
+    let {amount} = subscriptions[+req.body.subscription];
+
+    // Add days to current subscription expiration (or now())
+    const {days} = subscriptions[+req.body.subscription];
+    const subscription = setSubscription(rows[0].subscription, days);
+    
+    const ref = JSON.parse(rows[0].referral);
+
+    // Discount 10% off of first purchase
+    if ((ref.referral || ref.affiliate) && !ref.hasMadePurchase) {
+      ref.hasMadePurchase = true;
+      amount -= amount * 0.10;
+    }
+
+    const charge = await stripe(config.keys.stripe).charges.create({
+      amount, currency: 'usd', source: req.body.token,
+      description: 'Ptorx - Premium Subscription'
+    });
+
+    // Update user's account
+    sql = `
+      UPDATE users SET subscription = ?, referral = ?, trial = 0
+      WHERE user_id = ?
+    `,
+    vars = [
+      subscription, JSON.stringify(ref),
+      req.session.uid
     ];
+    const result = await db.query(sql, vars);
 
-    db(cn => cn.query(sql, vars, (err, rows) => {
-        if (err || !rows.length) {
-            cn.release();
-            res.json({ error: true, message: "An unknown error occured" });
-            return;
-        }
+    if (!result.affectedRows) throw 'Could not update subscription';
+      
+    if (ref.referral) {
+      sql = `
+        SELECT subscription FROM users WHERE user_id = ?
+      `,
+      vars = [ref.referral],
+      rows = await db.query(sql, vars);
 
-        let amount = [0, 300, 1500, 2400][req.body.subscription];
+      if (rows.length) {
+        sql = `
+          UPDATE users SET subscription = ? WHERE user_id = ?
+        `,
+        vars = [
+          setSubscription(rows[0].subscription, (days / 30) * 7 ), ref.referral
+        ];
+        await db.query(sql, vars);
+      }
+    }
+    else if (ref.affiliate) {
+      request
+        .post(config.address.xacc + 'api/affiliate/purchase')
+        .send({
+          service: 13, serviceKey: config.keys.xacc,
+          promoCode: ref.affiliate, amount
+        })
+        .end(() => 1);
+    }
 
-        if (!amount) {
-            res.json({ error: true, message: "Invalid subscription length" });
-            return;
-        }
-
-        // Add days to current subscription expiration (or now())
-        const days = [0, 30, 180, 365][req.body.subscription];
-        const subscription = setSubscription(rows[0].subscription, days);
-        
-        const referral = JSON.parse(rows[0].referral);
-
-        // Discount 10% off of first purchase
-        if ((referral.referral || referral.affiliate) && !referral.hasMadePurchase) {
-            referral.hasMadePurchase = true;
-            amount -= amount * 0.10;
-        }
-        
-        stripe(config.keys.stripe).charges.create({
-            amount, currency: "usd", source: req.body.stripeToken,
-            description: "Ptorx - Premium Subscription"
-        }, (err, charge) => {
-            if (err) {
-                res.json({
-                    error: true,
-                    message: "Error processing your card. Please try again."
-                }); return;
-            }
-
-            // Update user's account
-            sql = `
-                UPDATE users SET subscription = ?, referral = ?, trial = 0
-                WHERE user_id = ?
-            `, vars = [
-                subscription, JSON.stringify(referral),
-                req.session.uid
-            ];
-
-            cn.query(sql, vars, (err, result) => {
-                if (err || !result.affectedRows) {
-                    cn.release();
-                    res.json({ error: true, message: "An unknown error occured" });
-                    return;
-                }
-                else if (referral.referral) {
-                    rewardReferrer(cn, referral.referral, days);
-                }
-                else if (referral.affiliate) {
-                    cn.release();
-                    rewardAffiliate(referral.affiliate, amount);
-                }
-                else {
-                    cn.release();
-                }
-                
-                req.session.subscription = subscription;
-                res.json({ error: false });
-            });
-        });
-    }));
+    db.release();
+    
+    req.session.subscription = subscription;
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
 
 };
