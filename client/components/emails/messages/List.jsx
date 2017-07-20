4103cc9789f7cb691731e198eba10a62246162d7
MrXyfir <mr@xyfir.com>
2017-07-19 20:17:12 -0700

    Improvements, changes
    - Update code style
    - Use superagent
    - Use react-md

--- client/components/emails/messages/List.jsx
+++ client/components/emails/messages/List.jsx
@@ -1,104 +1,121 @@
-import React from "react";
+import request from 'superagent';
+import React from 'react';
+
+// react-md
+import ListItem from 'react-md/lib/Lists/ListItem';
+import Dialog from 'react-md/lib/Dialogs';
+import List from 'react-md/lib/Lists/List';
 
 // Action creators
-import {
-    loadMessages, deleteMessage
-} from "actions/creators/messages";
+import { loadMessages, deleteMessage } from 'actions/creators/messages';
 
 // Modules
-import parseQuery from "lib/parse-hash-query";
-import request from "lib/request";
+import parseQuery from 'lib/parse-hash-query';
 
 export default class MessageList extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            id: location.hash.split('/')[2], rejected: !!parseQuery().rejected,
-            loading: true
-        };
+    this.state = {
+      emailId: location.hash.split('/')[2], loading: true,
+      rejected: !!parseQuery().rejected
+    };
 
-        const url = `../api/emails/${this.state.id}/messages`
-            + (this.state.rejected ? "?rejected=1" : "");
+    request
+      .get(`../api/emails/${this.state.emailId}/messages`)
+      .query({ rejected: this.state.rejected ? 1 : undefined })
+      .end((err, res) => {
+        this.props.dispatch(loadMessages(res.body.messages));
+        this.setState({ loading: false });
+      });
+  }
 
-        request(url, (res) => {
-            this.props.dispatch(loadMessages(res.messages));
-            this.setState({ loading: false });
-        });
-    }
+  onDelete(id) {
+    swal({
+      title: 'Are you sure?',
+      text: 'This action cannot be undone',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#DD6B55',
+      confirmButtonText: 'Yes, delete it!'
+    }, () => request
+      .delete(`../api/emails/${this.state.emailId}/messages/${id}`)
+      .end((err, res) => {
+        if (err || res.body.error)
+          swal('Error', 'Could not delete message', 'error');
+        else
+          this.props.dispatch(deleteMessage(id));
+      })
+    );
+  }
 
-    onDeleteMessage(id) {
-        swal({
-            title: "Are you sure?",
-            text: "This action cannot be undone",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!"
-        }, () => {
-            request({
-                url: `../api/emails/${this.state.id}/messages/${id}`,
-                method: "DELETE",
-            }, (res) => {
-                if (res.error)
-                    swal("Error", "Could not delete message", "error");
-                else
-                    this.props.dispatch(deleteMessage(id));
-            });
-        });
-    }
+  render() {
+    if (this.state.loading) return <div />;
 
-    render() {
-        if (this.state.loading) return <div />;
+    const { selected, emailId } = this.state;
+    const { messages } = this.props.data;
+    
+    return (
+      <div className='messages'>
+        <nav className='navbar-sub'>
+          <a href={`#emails/messages/${emailId}/send`}>Send Message</a>
+          <a href={`#emails/edit/${emailId}`}>Edit Email</a>
+          <a onClick={() => {
+            if (this.state.rejected)
+              location.hash = location.hash.split('?')[0];
+            else
+              location.hash += '?rejected=1';
+            location.reload();
+          }}>
+            {this.state.rejected ? '' : 'Rejected '}Messages
+          </a>
+        </nav>
         
-        return (
-            <div className="messages">
-                <nav className="navbar-sub">
-                    <a href={`#emails/messages/${this.state.id}/send`}>
-                        Send Message
-                    </a>
-                    <a href={`#emails/edit/${this.state.id}`}>
-                        Edit Email
-                    </a>
-                    <a onClick={() => {
-                        if (this.state.rejected)
-                            location.hash = location.hash.split('?')[0];
-                        else
-                            location.hash += "?rejected=1";
-                        location.reload();
-                    }}>
-                        {this.state.rejected ? "" : "Rejected "}Messages
-                    </a>
-                </nav>
-                
-                <div className="list">{
-                    this.props.data.messages.length
-                    ? this.props.data.messages.map(msg => {
-                        return (
-                            <div className="message">
-                                <span className="subject">
-                                    <a href={
-                                        "#emails/messages/" + this.state.id
-                                        + "/view/" + msg.id
-                                    }>{msg.subject}</a>
-                                </span>
-                                <span className="received">{
-                                    (new Date(msg.received * 1000))
-                                        .toLocaleString()
-                                }</span>
-                                <div className="controls">
-                                    <a
-                                        className="icon-trash"
-                                        onClick={() => this.onDeleteMessage(msg.id)}
-                                    >Delete</a>
-                                </div>
-                            </div>
-                        );
-                    }) : <h3>This inbox is empty</h3>
-                }</div>
-            </div>
-        );
-    }
+        <List className='messages md-paper md-paper--1 section'>{
+          messages.length ?
+            messages.map(msg =>
+              <ListItem
+                key={msg.id}
+                onClick={() => this.setState({ selected: msg.id })}
+                primaryText={msg.subject}
+                secondaryText={
+                  (new Date(msg.received * 1000)).toLocaleString()
+                }
+              />
+            ) :
+            <h3>This inbox is empty.</h3>
+        }</List>
+
+        <Dialog
+          id='selected-message'
+          title={
+            selected && messages.find(m => m.id == selected).subject
+          }
+          onHide={() => this.setState({ selected: 0 })}
+          visible={!!selected}
+        >
+          <List>
+            <ListItem
+              primaryText='View'
+              onClick={() => location.hash =
+                `#emails/messages/${emailId}/view/${selected}`
+              }
+            />
+            <ListItem
+              primaryText='Reply'
+              onClick={() => location.hash =
+                `#emails/messages/${emailId}/view/${selected}?reply=1`
+              }
+            />
+            <ListItem
+              primaryText='Delete'
+              onClick={() => this.onDelete()}
+            />
+          </List>
+        </Dialog>
+      </div>
+    );
+  }
 
 }
