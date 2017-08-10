8e0f02fa4049d9b6799582258c97c94f90c02d75
MrXyfir <mr@xyfir.com>
2017-08-09 18:07:19 -0700

    Improvements, changes
    - Use lib/mysql
    - Update code style
    - Return `directForward`

--- controllers/emails/get.js
+++ controllers/emails/get.js
@@ -1,81 +1,91 @@
-const db = require("lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    GET api/emails/:email
-    RETURN
-        { 
-            error: boolean, toEmail: string, saveMail: boolean,
-            spamFilter: boolean, id: number, name: string, description: string,
-            address: string, filters: [{
-                id: number, name: string, description: string, type: number
-            }], modifiers: [{
-                id: number, name: string, description: string, type: number
-            }]
-        }
-    DESCRIPTION
-        Returns data for a specific REDIRECT email
+  GET api/emails/:email
+  RETURN
+    { 
+      error: boolean, toEmail: string, saveMail: boolean, name: string,
+      spamFilter: boolean, id: number, description: string,
+      address: string, directForward: boolean,
+      filters: [{
+        id: number, name: string, description: string, type: number
+      }],
+      modifiers: [{
+        id: number, name: string, description: string, type: number
+      }]
+    }
+  DESCRIPTION
+    Returns data for a specific proxy email
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
 
     // Ensure user owns email and grab toEmail/saveMail
     let sql = `
-        SELECT
-            email_id as id, name, description, address, save_mail as saveMail,
-            spam_filter as spamFilter, (
-                SELECT address FROM main_emails WHERE email_id IN (
-                    SELECT to_email FROM redirect_emails
-                    WHERE email_id = ? AND user_id = ?
-                )
-            ) as toEmail
-        FROM redirect_emails WHERE email_id = ?
-    `, vars = [
-        req.params.email, req.session.uid,
-        req.params.email
-    ];
+      SELECT
+        email_id AS id, name, description, address, save_mail AS saveMail,
+        spam_filter AS spamFilter, direct_forward AS directForward,
+        (SELECT address FROM main_emails WHERE email_id IN (
+          SELECT to_email FROM redirect_emails
+          WHERE email_id = ? AND user_id = ?
+        )) AS toEmail
+      FROM redirect_emails WHERE email_id = ?
+    `,
+    vars = [
+      req.params.email, req.session.uid,
+      req.params.email
+    ],
+    rows = await db.query(sql, vars);
 
-    db(cn => cn.query(sql, vars, (err, rows) => {
-        if (err || !rows.length || rows[0].toEmail == null) {
-            cn.release();
-            res.json({ error: true });
-        }
-        else {
-            let response = rows[0];
-            
-            response.error = false;
-            response.saveMail = !!(+response.saveMail);
-            response.spamFilter = !!(+response.spamFilter);
+    if (!rows.length || rows[0].toEmail == null)
+      throw 'Could not find email';
 
-            // Grab basic info for all filters linked to email
-            sql = `
-                SELECT filter_id as id, name, description, type
-                FROM filters WHERE filter_id IN (
-                    SELECT filter_id FROM linked_filters WHERE email_id = ?
-                )
-            `;
-            cn.query(sql, [req.params.email], (err, rows) => {
-                response.filters = (err || !rows.length ? [] : rows);
+    const response = rows[0];
+    
+    response.error = false,
+    response.saveMail = !!response.saveMail,
+    response.spamFilter = !!response.spamFilter,
+    response.directForward = !!response.directForward;
 
-                // Grab basic info for all modifiers linked to email
-                sql = `
-                    SELECT
-                        modifiers.modifier_id as id, modifiers.name,
-                        modifiers.description, modifiers.type
-                    FROM
-                        modifiers, linked_modifiers
-                    WHERE
-                        modifiers.modifier_id = linked_modifiers.modifier_id
-                        AND linked_modifiers.email_id = ?
-                    ORDER BY linked_modifiers.order_number
-                `;
-                cn.query(sql, [req.params.email], (err, rows) => {
-                    cn.release();
+    // Grab basic info for all filters linked to email
+    sql = `
+      SELECT filter_id AS id, name, description, type
+      FROM filters WHERE filter_id IN (
+        SELECT filter_id FROM linked_filters WHERE email_id = ?
+      )
+    `,
+    vars = [
+      req.params.email
+    ],
+    rows = await db.query(sql, vars);
 
-                    response.modifiers = (err || !rows.length ? [] : rows);
-                    res.json(response);
-                });
-            });
-        }
+    response.filters = rows;
 
-    }));
+    // Grab basic info for all modifiers linked to email
+    sql = `
+      SELECT
+        modifiers.modifier_id AS id, modifiers.name,
+        modifiers.description, modifiers.type
+      FROM
+        modifiers, linked_modifiers
+      WHERE
+        modifiers.modifier_id = linked_modifiers.modifier_id
+        AND linked_modifiers.email_id = ?
+      ORDER BY linked_modifiers.order_number
+    `,
+    rows = await db.query(sql, vars);
+    db.release();
+
+    response.modifiers = rows;
+    res.json(response);
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
 
 };
