9915fb7a6283dfe13eaa03ea907979dbd7eab070
MrXyfir <mr@xyfir.com>
2017-08-31 20:42:13 -0700

    Add 'POST api/domains/:domain/users'

---
+++ controllers/domains/users/add.js
@@ -0,0 +1,41 @@
+const request = require('superagent');
+const mysql = require('lib/mysql');
+
+/*
+  POST api/domains/:domain/users
+  REQUIRED
+    key: string, label: string
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Completes process of adding another Ptorx user to a domain
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+    const result = await db.query(`
+      UPDATE domain_users SET
+        label = ?, added = NOW(), authorized = 1
+      WHERE
+        domain_id = ? AND request_key = ? AND
+        (SELECT user_id FROM domains WHERE id = ?) = ?
+    `, [
+      req.body.label,
+      req.params.domain, req.body.key,
+      req.params.domain, req.session.uid
+    ]);
+    db.release();
+
+    if (!result.affectedRows) throw 'Could not add user to domain';
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
+
+};
