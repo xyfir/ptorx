f0f7983843c36caf42113e31b172546800f08043
MrXyfir <mr@xyfir.com>
2017-08-31 20:43:12 -0700

    Add /domains

--- controllers/index.js
+++ controllers/index.js
@@ -10,10 +10,10 @@ const upload = multer({
   }
 });
 
-// MailGun Inbound Controllers
+/* MAILGUN */
 router.post('/receive/:email', upload.any(), require('./receive'));
 
-// (Redirect) email controllers
+/* PROXY EMAILS */
 router.get('/emails', require('./emails/list'));
 router.post('/emails', require('./emails/create'));
 router.route('/emails/:email')
@@ -27,7 +27,7 @@ router.route('/emails/:email/messages/:message')
   .post(require('./emails/messages/reply'))
   .delete(require('./emails/messages/delete'));
 
-// Filter controllers
+/* FILTERS */
 router.get('/filters', require('./filters/list'));
 router.post('/filters', require('./filters/create'));
 router.route('/filters/:filter')
@@ -35,7 +35,7 @@ router.route('/filters/:filter')
   .put(require('./filters/update'))
   .delete(require('./filters/delete'));
 
-// Modifier controllers
+/* MODIFIERS */
 router.get('/modifiers', require('./modifiers/list'));
 router.post('/modifiers', require('./modifiers/create'));
 router.route('/modifiers/:mod')
@@ -43,7 +43,7 @@ router.route('/modifiers/:mod')
   .put(require('./modifiers/update'))
   .delete(require('./modifiers/delete'));
 
-// Account controllers
+/* ACCOUNT */
 router.get('/account', require('./account/info'));
 router.post('/account/stripe-purchase', require('./account/stripe-purchase'));
 router.post('/account/native-purchase', require('./account/native-purchase'));
@@ -52,5 +52,16 @@ router.route('/account/email/:email')
   .delete(require('./account/email/delete'));
 router.post('/account/login', require('./account/login'));
 router.get('/account/logout', require('./account/logout'));
+
+/* DOMAINS */
+router.route('/domains')
+  .get(require('./domains/list'))
+  .post(require('./domains/add'));
+router.route('/domains/:domain')
+  .get(require('./domains/get'))
+  .delete(require('./domains/remove'));
+router.put('/domains/:domain/verify', require('./domains/verify'));
+router.post('/domains/:domain/users', require('./domains/users/add'));
+router.delete('/domains/:domain/users/:user', require('./domains/users/remove'));
 
 module.exports = router;
