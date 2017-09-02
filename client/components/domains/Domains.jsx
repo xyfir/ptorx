5360db2f2dd867e5971f2e7a6f3449404a415d71
MrXyfir <mr@xyfir.com>
2017-09-02 16:27:46 -0700

    Add router for #domains

---
+++ client/components/domains/Domains.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import View from 'components/domains/View';
+import List from 'components/domains/List';
+import Add from 'components/domains/Add';
+
+export default props => {
+  switch (props.data.view.split('/')[1]) {
+    case 'VIEW': return <View {...props} />
+    case 'LIST': return <List {...props} />
+    case 'ADD':  return <Add {...props} />
+  }
+};
