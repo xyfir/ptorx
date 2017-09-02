ddeed31eb1d3e015db044ad3e8e87f16f38e1f08
MrXyfir <mr@xyfir.com>
2017-09-02 16:23:45 -0700

    Add state.domains. Update code

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,28 +1,28 @@
 // Reducers
-import modifiers from "./modifiers";
-import messages from "./messages";
-import filters from "./filters";
-import account from "./account";
-import emails from "./emails";
+import modifiers from 'reducers/modifiers';
+import messages from 'reducers/messages';
+import filters from 'reducers/filters';
+import account from 'reducers/account';
+import domains from 'reducers/domains';
+import emails from 'reducers/emails';
 
-import { INITIALIZE_STATE, CHANGE_VIEW } from "actions/types/index";
+import { INITIALIZE_STATE, CHANGE_VIEW } from 'actions/types/index';
 
-export default function (state, action) {
+export default function(state, action) {
 
-    if (action.type == INITIALIZE_STATE)
-        return action.state;
-    else if (state == undefined)
-        return {};
-    else if (action.type == CHANGE_VIEW)
-        return Object.assign({}, state, { view: action.view });
+  if (action.type == INITIALIZE_STATE)
+    return action.state;
+  else if (state == undefined)
+    return {};
 
-    return {
-        modifiers: modifiers(state.modifiers, action),
-        messages: messages(state.messages, action),
-        filters: filters(state.filters, action),
-        account: account(state.account, action),
-        emails: emails(state.emails, action),
-        view: state.view
-    };
+  return {
+    modifiers: modifiers(state.modifiers, action),
+    messages: messages(state.messages, action),
+    filters: filters(state.filters, action),
+    account: account(state.account, action),
+    domains: domains(state.domains, action),
+    emails: emails(state.emails, action),
+    view: action.type == CHANGE_VIEW ? action.view : state.view
+  };
 
 }
