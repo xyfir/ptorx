793a16a467b4fdb1b57d6cefcea440a22659ee44
MrXyfir <mr@xyfir.com>
2017-09-03 15:56:48 -0700

    Improvements, changes
    - Support multiple domains
    - Update code style
    - Use lib/mysql

--- controllers/emails/list.js
+++ controllers/emails/list.js
@@ -1,23 +1,41 @@
-const db = require("lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    GET api/emails
-    RETURN
-        { emails: [
-            id: number, name: string, description: string, address: string
-        ]}
-    DESCRIPTION
-        Returns basic information for all REDIRECT emails linked to account
+  GET api/emails
+  RETURN
+    {
+      error: boolean, message?: string,
+      emails: [{
+        id: number, name: string, description: string, address: string
+      }]
+    }
+  DESCRIPTION
+    Returns basic information for all REDIRECT emails linked to account
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    let sql = `
-        SELECT email_id as id, name, description, address
-        FROM redirect_emails WHERE user_id = ?
-    `;
-    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
-        cn.release();
-        res.json({ emails: (err || !rows.length ? [] : rows) });
-    }));
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+    const emails = await db.query(`
+      SELECT
+        re.email_id AS id, re.name, re.description,
+        CONCAT(re.address, '@', d.domain) AS address
+      FROM
+        redirect_emails AS re, domains AS d
+      WHERE
+        re.user_id = ? AND d.id = re.domain_id
+    `, [
+      req.session.uid
+    ]);
+    db.release();
+
+    res.json({ error: false, emails });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err, emails: [] });
+  }
 
 };
