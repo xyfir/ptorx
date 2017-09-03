b77981588d051e78acc206f055765b3a6d3415c6
MrXyfir <mr@xyfir.com>
2017-09-03 16:33:19 -0700

    Improvements, changes
    - Support multiple domains
    - Update code style
    - Use lib/mysql

--- controllers/emails/messages/delete.js
+++ controllers/emails/messages/delete.js
@@ -1,56 +1,51 @@
-const db = require("lib/db");
-
-let config = require("config");
-let mailgun = require("mailgun-js")({
-    apiKey: config.keys.mailgun, domain: "ptorx.com"
-});
+const MailGun = require('mailgun-js');
+const config = require('config');
+const mysql = require('lib/mysql');
 
 /*
-    DELETE api/emails/:email/messages/:message
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Delete message from Ptorx and MailGun
+  DELETE api/emails/:email/messages/:message
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Delete message from Ptorx and MailGun
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    // Make sure message exists and user has access
-    let sql = `
-        SELECT message_key as mkey FROM messages
-        WHERE message_key = ? AND email_id IN (
-            SELECT email_id FROM redirect_emails
-            WHERE email_id = ? AND user_id = ?
-        )
-    `, vars = [
-        req.params.message,
-        req.params.email, req.session.uid
-    ];
+  const db = new mysql;
 
-    db(cn => cn.query(sql, vars, (err, rows) => {
-        if (err || !rows.length) {
-            res.json({ error: true });
-        }
-        else {
-            // Delete message ref from Ptorx
-            sql = `
-                DELETE FROM messages WHERE message_key = ? AND email_id = ?
-            `, vars = [
-                req.params.message, req.params.email
-            ];
+  try {
+    await db.getConnection();
+    const [message] = await db.query(`
+      SELECT
+        m.message_key AS 'key', d.domain
+      FROM
+        messages AS m, domains AS d, redirect_emails AS re
+      WHERE
+        m.message_key = ? AND re.email_id = ? AND re.user_id = ? AND
+        m.email_id = re.email_id AND d.id = re.domain_id
+    `, [
+      req.params.message, req.params.email, req.session.uid
+    ]);
 
-            cn.query(sql, vars, (err, result) => {
-                cn.release();
+    if (!message) throw 'Could not find message';
 
-                if (err || !result.affectedRows) {
-                    res.json({ error: true });
-                }
-                else {
-                    // Delete from MailGun
-                    mailgun.messages(rows[0].mkey).delete(() => {});
-                    res.json({ error: false });
-                }
-            });
-        }
-    }));
+    await db.query(
+      'DELETE FROM messages WHERE message_key = ? AND email_id = ?',
+      [req.params.message, req.params.email]
+    );
+    db.release();
+
+    const mailgun = MailGun({
+      apiKey: config.keys.mailgun, domain: message.domain
+    });
+
+    await mailgun.messages(message.key).delete();
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err.toString() });
+  }
 
 };
