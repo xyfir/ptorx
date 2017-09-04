e8632c7828d29d8dac4ac4083072285ba6352292
MrXyfir <mr@xyfir.com>
2017-09-04 11:50:40 -0700

    Add 'DELETE api/domains/:domain'

---
+++ controllers/domains/remove.js
@@ -0,0 +1,42 @@
+const request = require('superagent');
+const config = require('config');
+const mysql = require('lib/mysql');
+
+/*
+  DELETE api/domains/:domain
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Remove a domain from Ptorx
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+    const [domain] = await db.query(
+      'SELECT domain FROM domains WHERE id = ? AND user_id = ?',
+      [req.params.domain, req.session.uid]
+    );
+
+    if (!domain) throw 'Could not find domain';
+
+    const mgRes = await request.delete(
+      `${config.addresses.mailgun}domains/${domain.domain}`
+    );
+
+    await db.query(
+      'DELETE FROM domains WHERE id = ?',
+      [req.params.domain]
+    );
+    db.release();
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
+
+};
