acb15cb0b9d3cc8dd200aede23c12c3b8f33d077
MrXyfir <mr@xyfir.com>
2017-09-06 09:28:29 -0700

    Improvements, changes
    - Update code style
    - Use lib/mysql
    - Replace message_key with id

--- controllers/emails/messages/list.js
+++ controllers/emails/messages/list.js
@@ -1,34 +1,46 @@
-const db = require("lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    GET api/emails/:email/messages
-    OPTIONAL
-        rejected: boolean
-    RETURN
-        { messages: [{ id: string, received: number, subject: string }] }
-    DESCRIPTION
-        Return basic data on any stored messages for :email
-        Returns accepted or rejected messages based on req.query.rejected
+  GET api/emails/:email/messages
+  OPTIONAL
+    rejected: boolean
+  RETURN
+    {
+      error: boolean, message?: string,
+      messages: [{
+        id: string, received: number, subject: string
+      }]
+    }
+  DESCRIPTION
+    Return basic data on any stored messages for :email
+    Returns accepted or rejected messages based on req.query.rejected
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    const sql = `
-        SELECT
-            m.message_key as id, m.received, m.subject
-        FROM
-            messages AS m, redirect_emails AS re
-        WHERE
-            m.email_id = re.email_id AND m.rejected = ?
-            AND m.received + 255600 > UNIX_TIMESTAMP()
-            AND re.email_id = ? AND re.user_id = ?
-    `, vars = [
-        !!+req.query.rejected,
-        req.params.email, req.session.uid
-    ];
+  const db = new mysql;
 
-    db(cn => cn.query(sql, vars, (err, rows) => {
-        cn.release();
-        res.json({ messages: (err || !rows.length) ? [] : rows });
-    }));
+  try {
+    await db.getConnection();
+    const messages = await db.query(`
+      SELECT
+        m.id, m.received, m.subject
+      FROM
+        messages AS m, redirect_emails AS re
+      WHERE
+        m.email_id = re.email_id AND m.rejected = ? AND
+        m.received + 255600 > UNIX_TIMESTAMP() AND
+        re.email_id = ? AND re.user_id = ?
+    `, [
+      !!+req.query.rejected,
+      req.params.email, req.session.uid
+    ]);
+    db.release();
+
+    res.json({ error: false, messages });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err, messages: [] });
+  }
 
 };
