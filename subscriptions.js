80a438d863a68c8d01870269e9b0a183bc1530c7
MrXyfir <mr@xyfir.com>
2017-09-07 13:51:41 -0700

    Remove subscriptions[]

--- subscriptions.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { name: '1 Month / $3 USD', amount: 300, days: 30 },
-  { name: '6 Months / $15 USD', amount: 1500, days: 182 },
-  { name: '12 Months / $24 USD', amount: 2400, days: 365 }
-];
