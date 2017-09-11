5a11791c777ac146ea787e92976441ef0f0dde54
MrXyfir <mr@xyfir.com>
2017-09-11 12:30:52 -0700

    Add LandingPage and 'anonymous-emails' section

---
+++ client/components/info/LandingPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// Constants
+import sections from 'constants/info-sections';
+
+// react-md
+import Button from 'react-md/lib/Buttons/Button';
+
+// Components
+import HowItWorks from 'components/info/HowItWorks';
+
+export default ({section}) => (
+  <div className={'landing-page ' + section}>
+    <header>
+      <h2>{sections[section].title}</h2>
+
+      <p>{sections[section].description}</p>
+
+      <HowItWorks />
+    </header>
+
+    <section className='free-trial'>
+      <Button
+        raised secondary
+        className='start-trial'
+        onClick={() => location.href =
+          'https://accounts.xyfir.com/app/#/register/13'
+        }
+        label='Try Free'
+      />
+
+      <p>All that's needed to start your 14 day free trial is a valid email to create a Xyfir Account. No payment info required.</p>
+    </section>
+
+    <section className='more-info'>
+      <p>If you decide to purchase a subscription and experience Ptorx in its entirety, we offer a risk-free 30 day money-back guarantee on all purchases.</p>
+
+      <p>Ptorx has lots of features, {sections[section].featuresMessage}. Check our <a href='features'>features list</a> to learn about all we can do!</p>
+
+      <p>Confused? Have questions? Browse through our <a href='docs'>Help Docs</a> or <a href='https://xyfir.com/#/contact'>send us your question</a> and we'll get back to you as soon as we can.</p>
+    </section>
+  </div>
+);
