ac0ff0ee93b8b44eb2e3f5c905ffc311b675d065
MrXyfir <mr@xyfir.com>
2017-09-23 09:18:06 -0700

    Add Admin.jsx

---
+++ client/components/Admin.jsx
@@ -0,0 +1,8 @@
+import AdminPanel from 'admyn/client/components/App';
+import { render } from 'react-dom';
+import React from 'react';
+
+render(
+  <AdminPanel title='Ptorx Admin' api='/admyn-yCPnUcXcU10QRkwv/' />,
+  document.getElementById('content')
+);
