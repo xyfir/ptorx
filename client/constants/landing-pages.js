3f22cd9ff226e041d48817d0874a3243ace37afa
MrXyfir <mr@xyfir.com>
2017-10-16 17:17:50 -0700

    Rename to landing-pages.js

--- client/constants/landing-pages.js
+++ client/constants/landing-pages.js
@@ -1,7 +1,7 @@
 export default {
   'anonymous-emails': {
     title: 'Anonymous Proxy Emails',
-    description: `Protect your privacy by keeping your real email addresses hidden behind Ptorx's proxy email addresses. Use a different proxy email anywhere you'd use your normal email address and any incoming mail to your proxy email will be redirected to your real one. You can also send or reply to mail from your proxy emails. Companies, hackers, spammers, and anyone else who receives your proxy address will be unable to find your real address. Whenever you're done with a proxy email, simply delete it, and it will no longer be linked to you or your real email address.`,
+    description: `Protect your privacy by keeping your real email addresses hidden behind Ptorx's proxy email addresses. Use a unique proxy email anywhere you'd use your normal email address and any incoming mail to your proxy email will be redirected to your real one. You can also send or reply to mail from your proxy emails. Companies, hackers, spammers, and anyone else who receives your proxy address will be unable to find your real address. Whenever you're done with a proxy email, simply delete it, and it will no longer be linked to you or your real email address.`,
     featuresMessage: 'protecting your privacy'
   },
   'safe-and-secure-emails': {
