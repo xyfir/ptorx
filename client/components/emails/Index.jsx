d13f305de7cf33531eae60ad020fb91be6b30132
MrXyfir <mr@xyfir.com>
2017-10-17 13:31:29 -0700

    Update code style. Pass all props through

--- client/components/emails/Index.jsx
+++ client/components/emails/Index.jsx
@@ -1,28 +1,24 @@
-import React from "react";
+import React from 'react';
 
 // Components
-import Messages from "./messages/Index";
-import Create from "./Create";
-import Edit from "./Edit";
-import List from "./List";
+import Messages from 'components/emails/messages/Index';
+import Create from 'components/emails/Create';
+import Edit from 'components/emails/Edit';
+import List from 'components/emails/List';
 
 export default class Emails extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        switch (this.props.data.view.split('/')[1]) {
-            case "MESSAGES":
-                return <Messages data={this.props.data} dispatch={this.props.dispatch} />;
-            case "CREATE":
-                return <Create data={this.props.data} dispatch={this.props.dispatch} />;
-            case "EDIT":
-                return <Edit data={this.props.data} dispatch={this.props.dispatch} />;
-            case "LIST":
-                return <List data={this.props.data} dispatch={this.props.dispatch} />;
-        }
+  render() {
+    switch (this.props.App.state.view.split('/')[1]) {
+      case 'MESSAGES': return <Messages {...this.props} />
+      case 'CREATE': return <Create {...this.props} />
+      case 'EDIT': return <Edit {...this.props} />
+      case 'LIST': return <List {...this.props} />
     }
+  }
 
 }
