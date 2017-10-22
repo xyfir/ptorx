ebab41e7da3137ce19da5873de70c15285588d44
MrXyfir <mr@xyfir.com>
2017-10-22 11:05:04 -0700

    Use webpack to build JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 const gutil = require('gulp-util');
 const gulp = require('gulp');
 
-const config = require('./config');
-
-const prod = config.environment.type == 'prod';
-process.env.NODE_ENV = prod ? 'production' : config.environment.type;
-
 function buildCSS(file) {
   const sass = require('gulp-sass');
   
@@ -17,40 +12,6 @@ function buildCSS(file) {
 
 gulp.task('css:main', () => buildCSS('styles'));
 gulp.task('css:admin', () => buildCSS('admin'));
-
-function buildJS(file) {
-  const browserify = require('browserify');
-  const streamify = require('gulp-streamify');
-  const babelify = require('babelify');
-  const uglify = require('gulp-uglify');
-  const source = require('vinyl-source-stream');
-
-  const extensions = ['.jsx', '.js'];
-  
-  return browserify(
-    `./client/components/${file}.jsx`, {
-      debug: true, extensions, paths: ['./client']
-    }
-  )
-  .transform(
-    babelify.configure({
-      extensions, presets: ['env', 'react']
-    })
-  )
-  .bundle()
-  .pipe(source(`${file}.js`))
-  .pipe(prod
-    ? streamify(
-        uglify({ mangle: false, compress: { unused: false } })
-      ).on('error', gutil.log)
-    : gutil.noop()
-  )
-  .pipe(gulp.dest('./static/js/'));
-}
-
-gulp.task('js:app', () => buildJS('App'));
-gulp.task('js:info', () => buildJS('Info'));
-gulp.task('js:admin', () => buildJS('Admin'));
 
 gulp.task('favicons', () => {
   const favicons = require('gulp-favicons');
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const config = require('./config');
+const path = require('path');
+
+const plugins = [];
+const isProd = config.environment.type == 'prod';
+
+if (isProd) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: { unused: false }
+    })
+  );
+}
+
+module.exports = {
+  
+  entry: {
+    Admin: './client/components/Admin.jsx',
+    //Info: './client/components/Info.jsx',
+    //App: './client/components/App.jsx'
+  },
+  
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'static/js')
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'client'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'client/components'),
+        path.resolve(__dirname, 'client/constants'),
+        path.resolve(__dirname, 'client/lib')
+      ],
+      exclude: /node_modules/,
+      options: {
+        presets: ['env', 'react']
+      }
+    }]
+  },
+
+  plugins
+
+};
