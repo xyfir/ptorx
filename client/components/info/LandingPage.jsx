5a044562910c6dea3dd1b3e8175ab86b2e3f2a88
MrXyfir <mr@xyfir.com>
2017-12-06 12:29:21 -0800

    Integrate `<PwnCheck>`. Set document title

--- client/components/info/LandingPage.jsx
+++ client/components/info/LandingPage.jsx
@@ -8,35 +8,51 @@ import Button from 'react-md/lib/Buttons/Button';
 
 // Components
 import HowItWorks from 'components/info/HowItWorks';
+import PwnCheck from 'components/info/PwnCheck';
 
-export default ({page}) => (
-  <div className={'landing-page ' + page}>
-    <header>
-      <h2>{pages[page].title}</h2>
+export default class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <p>{pages[page].description}</p>
+    document.title = pages[props.page].title + ' - Ptorx';
+  }
 
-      <HowItWorks />
-    </header>
+  render() {
+    const {page, pwnCheck} = this.props;
 
-    <section className='free-trial'>
-      <Button
-        raised secondary
-        className='start-trial'
-        onClick={() => location.href =
-          'https://accounts.xyfir.com/app/#/register/13'
-        }
-      >Try Free</Button>
+    return (
+      <div className={'landing-page ' + page}>
+        <header>
+          <h2>{pages[page].title}</h2>
 
-      <p>All that's needed to start your 14 day free trial is a valid email to create a Xyfir Account. Have your first proxy email ready in the next 3 minutes.</p>
-    </section>
+          <p>{pages[page].description}</p>
 
-    <section className='more-info'>
-      <p>If you decide to purchase a subscription and experience Ptorx in its entirety, we offer a <strong>risk-free, 30 day money-back guarantee</strong> on all purchases.</p>
+          <HowItWorks />
+        </header>
 
-      <p><strong>Ptorx has lots of features</strong>, and is used for much more than just {pages[page].featuresMessage}. Check our <strong><a href='features'>features list</a></strong> to learn about all we can do!</p>
+        <section className='free-trial'>
+          <Button
+            raised secondary
+            className='start-trial'
+            onClick={() => location.href =
+              'https://accounts.xyfir.com/app/#/register/13'
+            }
+          >Try Free</Button>
 
-      <p><strong>Confused? Have questions?</strong> Browse through our <strong><a href='docs'>Help Docs</a></strong> or <strong><a href='https://xyfir.com/#/contact'>send us your question</a></strong> and we'll get back to you as soon as we can.</p>
-    </section>
-  </div>
-);
+          <p>All that's needed to start your 14 day free trial is a valid email to create a Xyfir Account. Have your first proxy email ready in the next 3 minutes.</p>
+        </section>
+
+        {pwnCheck ? <PwnCheck /> : null}
+
+        <section className='more-info'>
+          <p>If you decide to purchase a subscription and experience Ptorx in its entirety, we offer a <strong>risk-free, 30 day money-back guarantee</strong> on all purchases.</p>
+
+          <p><strong>Ptorx has lots of features</strong>, and is used for much more than just {pages[page].featuresMessage}. Check our <strong><a href='features'>features list</a></strong> to learn about all we can do!</p>
+
+          <p><strong>Confused? Have questions?</strong> Browse through our <strong><a href='docs'>Help Docs</a></strong> or <strong><a href='https://xyfir.com/#/contact'>send us your question</a></strong> and we'll get back to you as soon as we can.</p>
+        </section>
+      </div>
+    );
+  }
+
+}
