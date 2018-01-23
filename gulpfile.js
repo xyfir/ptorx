2e42171a85786ce6d55be7a5d83a1136b8e8a965
MrXyfir <mr@xyfir.com>
2018-01-23 12:17:40 -0800

    css:main -> css:app

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const gulp = require('gulp');
 
 function buildCSS(file) {
   const sass = require('gulp-sass');
-  
+
   return gulp.src(`./client/styles/${file}.scss`)
     .pipe(sass({ outputStyle: 'compressed' })
     .on('error', sass.logError))
     .pipe(gulp.dest('./static/css'))
 }
 
-gulp.task('css:main', () => buildCSS('styles'));
+gulp.task('css:app', () => buildCSS('styles'));
 gulp.task('css:admin', () => buildCSS('admin'));
 
 gulp.task('favicons', () => {
