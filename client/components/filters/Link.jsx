c89345de76f7094fe5374a1629c763c0c66821e6
MrXyfir <mr@xyfir.com>
2018-01-24 13:45:55 -0800

    Remove ability to create modifier/filter

--- client/components/filters/Link.jsx
+++ client/components/filters/Link.jsx
@@ -1,9 +1,9 @@
+import { List, ListItem } from 'react-md';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 // Components
 import Search from 'components/misc/Search';
-import Create from './Create';
 
 // Constants
 import { filterTypes } from 'constants/types';
@@ -11,64 +11,40 @@ import { filterTypes } from 'constants/types';
 // Modules
 import findMatches from 'lib/find-matching';
 
-// react-md
-import ListItem from 'react-md/lib/Lists/ListItem';
-import List from 'react-md/lib/Lists/List';
-
 export default class LinkFilter extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      view: 'search', search: { query: '', type: 0 }
+      search: { query: '', type: 0 }
     };
   }
 
   render() {
     return (
       <div className='link-filter'>
-        {this.state.view == 'search' ? (
-          <a onClick={() => this.setState({ view: 'create' })}>
-            Create New Filter
-          </a>
-        ) : (
-          <a onClick={() => this.setState({ view: 'search' })}>
-            Find Existing Filter
-          </a>
-        )}
-        
-        {this.state.view == 'search' ? (
-          <div>
-            <Search
-              onSearch={v => this.setState({ search: v })}
-              type='filter'
-            />
+        <Search
+          onSearch={v => this.setState({ search: v })}
+          type='filter'
+        />
 
-            <List
-              className='filters-list section md-paper md-paper--1'
-            >{
-              findMatches(
-                this.props.data.filters, this.state.search, this.props.ignore
-              ).map(f =>
-                <ListItem
-                  threeLines
-                  key={f.id}
-                  onClick={() => this.props.onAdd(f.id)}
-                  className='filter'
-                  primaryText={f.name}
-                  secondaryText={filterTypes[f.type] + '\n' + f.description}
-                />
-              )
-            }</List>
-          </div>
-        ) : (
-          <Create
-            data={this.props.data}
-            dispatch={this.props.dispatch}
-            onCreate={this.onAdd}
-          />
-        )}
+        <List
+          className='filters-list section md-paper md-paper--1'
+        >{
+          findMatches(
+            this.props.data.filters, this.state.search, this.props.ignore
+          ).map(f =>
+            <ListItem
+              threeLines
+              key={f.id}
+              onClick={() => this.props.onAdd(f.id)}
+              className='filter'
+              primaryText={f.name}
+              secondaryText={filterTypes[f.type] + '\n' + f.description}
+            />
+          )
+        }</List>
       </div>
     );
   }
--- client/components/modifiers/Link.jsx
+++ client/components/modifiers/Link.jsx
@@ -1,9 +1,9 @@
+import { ListItem, List } from 'react-md';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 // Components
 import Search from 'components/misc/Search';
-import Create from './Create';
 
 // Constants
 import { modifierTypes } from 'constants/types';
@@ -11,64 +11,40 @@ import { modifierTypes } from 'constants/types';
 // Modules
 import findMatches from 'lib/find-matching';
 
-// react-md
-import ListItem from 'react-md/lib/Lists/ListItem';
-import List from 'react-md/lib/Lists/List';
-
 export default class LinkModifier extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      view: 'search', search: { query: '', type: 0 }
+      search: { query: '', type: 0 }
     };
   }
 
   render() {
     return (
       <div className='link-modifier'>
-        {this.state.view == 'search' ? (
-          <a onClick={() => this.setState({ view: 'create' })}>
-            Create New Modifier
-          </a>
-        ) : (
-          <a onClick={() => this.setState({ view: 'search' })}>
-            Find Existing Modifier
-          </a>
-        )}
-        
-        {this.state.view == 'search' ? (
-          <div>
-            <Search
-              onSearch={v => this.setState({ search: v })}
-              type='modifier'
-            />
+        <Search
+          onSearch={v => this.setState({ search: v })}
+          type='modifier'
+        />
 
-            <List
-              className='modifiers-list section md-paper md-paper--1'
-            >{
-              findMatches(
-                this.props.data.modifiers, this.state.search, this.props.ignore
-              ).map(m =>
-                <ListItem
-                  threeLines
-                  key={m.id}
-                  onClick={() => this.props.onAdd(m.id)}
-                  className='modifier'
-                  primaryText={m.name}
-                  secondaryText={modifierTypes[m.type] + '\n' + m.description}
-                />
-              )
-            }</List>
-          </div>
-        ) : (
-          <Create
-            data={this.props.data}
-            dispatch={this.props.dispatch}
-            onCreate={this.onAdd}
-          />
-        )}
+        <List
+          className='modifiers-list section md-paper md-paper--1'
+        >{
+          findMatches(
+            this.props.data.modifiers, this.state.search, this.props.ignore
+          ).map(m =>
+            <ListItem
+              threeLines
+              key={m.id}
+              onClick={() => this.props.onAdd(m.id)}
+              className='modifier'
+              primaryText={m.name}
+              secondaryText={modifierTypes[m.type] + '\n' + m.description}
+            />
+          )
+        }</List>
       </div>
     );
   }
