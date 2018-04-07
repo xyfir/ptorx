13cfa22922ebfa2653e1626e20039e89a5f0cfdc
MrXyfir <mr@xyfir.com>
2018-04-06 19:39:03 -0700

    Update to support xyPayments

--- client/components/account/Purchase.jsx
+++ client/components/account/Purchase.jsx
@@ -1,53 +1,28 @@
 import { TextField, Button, Paper } from 'react-md';
-import StripeCheckout from 'react-stripe-checkout';
 import request from 'superagent';
 import React from 'react';
 import swal from 'sweetalert';
-
-// Constants
-import { STRIPE_KEY_PUB } from 'constants/config';
 
 export default class Purchase extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = { subscription: 0 };
   }
 
-  onStripePurchase(token) {
+  /** @param {string} type - `'iap|normal|swiftdemand'` */
+  onPurchase(type) {
     request
-      .post('/api/account/purchase/stripe')
-      .send({ token: token.id })
+      .post('/api/account/purchase')
+      .send({ type })
       .end((err, res) => {
-          if (err || res.body.error) {
-            swal('Error', res.body.message, 'error');
-          }
-          else {
-            location.hash = '#/account';
-            location.reload();
-          }
-      });
-  }
-
-  onSwiftDemandPurchase() {
-    const swiftId = this.refs.swift.value;
-
-    if (!swiftId) return;
-
-    request
-      .post('/api/account/purchase/swiftdemand')
-      .send({ swiftId })
-      .end((err, res) => {
-        if (err || !res.body.redirect)
-          swal('Error', res.body.message, 'error');
-        else
-          location.href = res.body.redirect;
+        if (err || res.body.error)
+          return swal('Error', res.body.message, 'error');
+        location.href = res.body.url;
       });
   }
 
   render() {
-    const { referral, trial } = this.props.data.account;
+    const {referral, trial} = this.props.data.account;
     const discount =
       (referral.user || referral.promo) &&
       !referral.hasMadePurchase;
@@ -62,17 +37,9 @@ export default class Purchase extends React.Component {
           You can purchase a one-time, three month subscription using SwiftDemand. This offer will expire when your account's 14 day free trial ends, so get it soon!
         </p>
 
-        <TextField
-          id='text--swift'
-          ref='swift'
-          type='text'
-          label='Your Swift ID'
-          className='md-cell'
-        />
-
         <Button
           primary raised
-          onClick={() => this.onSwiftDemandPurchase()}
+          onClick={() => this.onPurchase('swiftdemand')}
         >Purchase</Button>
       </Paper>
     )
@@ -80,7 +47,7 @@ export default class Purchase extends React.Component {
       <Paper
         zDepth={1}
         component='section'
-        className='purchase-subscription stripe section flex'
+        className='purchase-subscription section flex'
       >
         <div className='info'>
           <p>
@@ -88,36 +55,21 @@ export default class Purchase extends React.Component {
           </p>
 
           <p>
-            A subscription with Ptorx costs $25 USD and gives you 365 days of access to all of Ptorx's features. You will not be automatically charged at the end of your subscription, but a week before expiration you will receive a notification to optionally purchase another year. If you currently already have a subscription, purchasing another one will add an additional 365 days to your current subscription.
+            A subscription with Ptorx costs $9.99 USD and gives you 365 days of access to all of Ptorx's features. You will not be automatically charged at the end of your subscription, but a week before expiration you will receive a notification to optionally purchase another year. If you currently already have a subscription, purchasing another one will add an additional 365 days to your current subscription.
           </p>
-          
+
           <p>
             Ptorx offers a 30 day money-back guarantee. If you're not happy with your purchase within the first 30 days, we'll offer a full refund, no questions asked.
           </p>
-          
+
           {discount ? (
-            <p>
-              You will receive 10% off of your first purchase. Your first subscription will only cost $22.50!
-            </p>
+            <p>You will receive 10% off of your first purchase.</p>
           ) : null}
         </div>
 
-        <StripeCheckout
-          bitcoin zipCode
-          name='Ptorx // Xyfir, LLC'
-          label='Purchase'
-          token={t => this.onStripePurchase(t)}
-          image='https://ptorx.com/static/icons/android-chrome-192x192.png'
-          amount={discount ? 2250 : 2500}
-          stripeKey={STRIPE_KEY_PUB}
-          description='365 Days'
-        />
-
         <Button
           raised primary
-          onClick={() =>
-            document.querySelector('.StripeCheckout').click()
-          }
+          onClick={() => this.onPurchase(window.cordova ? 'iap' : 'normal')}
         >Purchase</Button>
       </Paper>
     )
