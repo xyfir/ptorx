c896b8faaa3b163b296862a8dc848aae3bf3d872
MrXyfir <mr@xyfir.com>
2018-05-15 20:34:50 -0700

    Add lib/affiliates/get

--- controllers/affiliates/info.js
+++ controllers/affiliates/info.js
@@ -1,3 +1,4 @@
+const getAffiliate = require('lib/affiliates/get');
 const moment = require('moment');
 const MySQL = require('lib/mysql');
 
@@ -15,28 +16,12 @@ module.exports = async function(req, res) {
 
   try {
     await db.getConnection();
-    const [affiliate] = await db.query(
-      'SELECT * FROM affiliates WHERE user_id = ?',
-      [req.session.uid]
-    );
-    if (!affiliate) throw 'You are not an affiliate';
-
-    affiliate.timestamp = moment().format('YYYY-MM-DD HH:MM:SS');
-    const [{ unpaid_subscriptions }] = await db.query(
-      `
-        SELECT COUNT(user_id) AS unpaid_subscriptions
-        FROM affiliate_created_users
-        WHERE affiliate_id = ? AND created_at BETWEEN ? AND ?
-      `,
-      [req.session.uid, affiliate.last_payment, affiliate.timestamp]
+    const affiliate = await getAffiliate(
+      db,
+      +req.session.uid,
+      moment().format('YYYY-MM-DD HH:MM:SS')
     );
     db.release();
-
-    affiliate.owed = +(
-      unpaid_subscriptions *
-      (9.0 - affiliate.discount / 100)
-    ).toFixed(2);
-
     res.status(200).json(affiliate);
   } catch (err) {
     db.release();
