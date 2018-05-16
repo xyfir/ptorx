4bbe9f2e486d992d8fb3b0512469172bdafc44b1
MrXyfir <mr@xyfir.com>
2018-05-15 20:20:16 -0700

    Add `POST /api/affiliates/key`

---
+++ controllers/affiliates/generate-key.js
@@ -0,0 +1,28 @@
+const MySQL = require('lib/mysql');
+const uuid = require('uuid/v4');
+
+/*
+  POST /api/affiliates/key
+  RETURN
+    { api_key: string }
+*/
+module.exports = async function(req, res) {
+  const db = new MySQL();
+
+  try {
+    const api_key = uuid();
+
+    await db.getConnection();
+    const result = await db.query(
+      'UPDATE affiliates SET api_key = ? WHERE user_id = ?',
+      [api_key, req.session.uid]
+    );
+    if (!result.affectedRows) throw 'Could not generate key';
+    db.release();
+
+    res.status(200).json({ api_key });
+  } catch (err) {
+    db.release();
+    res.status(400).json({ message: err.toString() });
+  }
+};
