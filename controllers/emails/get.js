b7c7b9cf12de6f607f47640193e5ca7673d1e7fe
MrXyfir <mr@xyfir.com>
2018-07-05 13:20:54 -0700

    Add lib/email/get

--- controllers/emails/get.js
+++ controllers/emails/get.js
@@ -1,83 +1,24 @@
-const mysql = require('lib/mysql');
+const getProxyEmail = require('lib/email/get');
+const MySQL = require('lib/mysql');
 
 /*
-  GET api/emails/:email
+  GET /api/emails/:email
   RETURN
-    { 
-      error: boolean, toEmail: string, saveMail: boolean, name: string,
-      address: string, directForward: boolean, description: string,
-      spamFilter: boolean, id: number
-      filters: [{
-        id: number, name: string, description: string, type: number
-      }],
-      modifiers: [{
-        id: number, name: string, description: string, type: number
-      }]
-    }
-  DESCRIPTION
-    Returns full data for a single proxy email
+    { message?: string, ...ProxyEmail }
 */
 module.exports = async function(req, res) {
-  const db = new mysql();
+  const db = new MySQL();
 
   try {
     await db.getConnection();
-
-    let sql = `
-      SELECT
-        pxe.email_id AS id, pxe.name, pxe.description, pxe.save_mail AS saveMail,
-        CONCAT(pxe.address, '@', d.domain) AS address, pme.address AS toEmail,
-        pxe.spam_filter AS spamFilter, pxe.direct_forward AS directForward
-      FROM
-        proxy_emails AS pxe, primary_emails AS pme, domains AS d
-      WHERE
-        pxe.email_id = ? AND pxe.user_id = ? AND
-        pme.email_id = pxe.primary_email_id AND
-        d.id = pxe.domain_id
-    `,
-      vars = [req.params.email, req.session.uid],
-      rows = await db.query(sql, vars);
-
-    if (!rows.length || rows[0].toEmail == null) throw 'Could not find email';
-
-    const response = rows[0];
-
-    (response.error = false),
-      (response.saveMail = !!response.saveMail),
-      (response.spamFilter = !!response.spamFilter),
-      (response.directForward = !!response.directForward);
-
-    // Grab basic info for all filters linked to email
-    (sql = `
-      SELECT filter_id AS id, name, description, type
-      FROM filters WHERE filter_id IN (
-        SELECT filter_id FROM linked_filters WHERE email_id = ?
-      )
-    `),
-      (vars = [req.params.email]),
-      (rows = await db.query(sql, vars));
-
-    response.filters = rows;
-
-    // Grab basic info for all modifiers linked to email
-    (sql = `
-      SELECT
-        modifiers.modifier_id AS id, modifiers.name,
-        modifiers.description, modifiers.type
-      FROM
-        modifiers, linked_modifiers
-      WHERE
-        modifiers.modifier_id = linked_modifiers.modifier_id
-        AND linked_modifiers.email_id = ?
-      ORDER BY linked_modifiers.order_number
-    `),
-      (rows = await db.query(sql, vars));
+    const email = await getProxyEmail(db, {
+      user: +req.session.uid,
+      email: +req.params.email
+    });
     db.release();
-
-    response.modifiers = rows;
-    res.json(response);
+    res.status(200).json(email);
   } catch (err) {
     db.release();
-    res.json({ error: true, message: err });
+    res.status(400).json({ message: err });
   }
 };
