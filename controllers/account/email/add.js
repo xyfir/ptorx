fbbbaf82ac1153b5cee09fde6fe543d84a8c6ed8
MrXyfir <mr@xyfir.com>
2018-07-07 11:39:39 -0700

    Update code. Remove trial/subscriptions

--- controllers/account/email/add.js
+++ controllers/account/email/add.js
@@ -1,75 +1,41 @@
-const db = require('lib/db');
+const MySQL = require('lib/mysql');
 
 /*
-  POST api/account/email/:email
+  POST /api/account/email/:email
   RETURN
-    { error: boolean, id?: number, message?: string }
-  DESCRIPTION
-    Adds a MAIN email to user's account
+    { id?: number, message?: string }
 */
-module.exports = function(req, res) {
-  if (req.params.email.match(/.*ptorx.com$/)) {
-    res.json({
-      error: true,
-      message: 'Cannot use Ptorx addresses to receive mail from Ptorx'
+module.exports = async function(req, res) {
+  const db = new MySQL();
+
+  try {
+    await db.getConnection();
+    const [row] = await db.query(
+      `
+        SELECT (
+          SELECT COUNT(email_id) FROM primary_emails WHERE user_id = ?
+        ) AS emails, (
+          SELECT COUNT(email_id) FROM primary_emails WHERE user_id = ? AND address = ?
+        ) AS email_exists
+      `,
+      [req.session.uid, req.session.uid, req.params.email]
+    );
+
+    if (row.email_exists > 0)
+      throw 'This email is already linked to your account';
+    if (req.params.email.length < 6 || req.params.email.length > 320)
+      throw 'Invalid email length. 6-320 characters required';
+
+    const result = await db.query('INSERT INTO primary_emails SET ?', {
+      user_id: req.session.uid,
+      address: req.params.email
     });
-    return;
+    if (!result.affectedRows) throw 'Could not add email';
+
+    res.status(200).json({ id: result.insertId });
+  } catch (err) {
+    res.status(400).json({ message: err });
   }
 
-  let sql = `
-    SELECT (
-      SELECT COUNT(email_id) FROM primary_emails WHERE user_id = ?
-    ) AS emails, (
-      SELECT COUNT(email_id) FROM primary_emails WHERE user_id = ? AND address = ?
-    ) AS email_exists, (
-      SELECT trial FROM users WHERE user_id = ?
-    ) AS trial
-  `,
-    vars = [
-      req.session.uid,
-      req.session.uid,
-      req.params.email,
-      req.session.uid
-    ];
-
-  db(cn =>
-    cn.query(sql, vars, (err, rows) => {
-      if (err || !rows.length) {
-        cn.release();
-        res.json({ error: true, message: 'An unknown error occured' });
-      } else if (rows[0].emails > 0 && rows[0].trial) {
-        cn.release();
-        res.json({
-          error: true,
-          message: 'Trial users cannot have more than one main email'
-        });
-      } else if (rows[0].email_exists > 0) {
-        cn.release();
-        res.json({
-          error: true,
-          message: 'This email is already linked to your account'
-        });
-      } else if (req.params.email.length < 6 || req.params.email.length > 320) {
-        cn.release();
-        res.json({
-          error: true,
-          message: 'Invalid email length. 6-320 characters required'
-        });
-      } else {
-        const insert = {
-          user_id: req.session.uid,
-          address: req.params.email
-        };
-        sql = 'INSERT INTO primary_emails SET ?';
-
-        cn.query(sql, insert, (err, result) => {
-          cn.release();
-
-          if (err || !result.affectedRows)
-            res.json({ error: true, message: 'An error occured-' });
-          else res.json({ error: false, id: result.insertId });
-        });
-      }
-    })
-  );
+  db.release();
 };
