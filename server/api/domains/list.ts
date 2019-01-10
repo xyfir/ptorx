e75b4b7d3acb479128c48edb0bed86045fd167ef
MrXyfir <mr@xyfir.com>
2019-01-10 14:05:02 -0800

    Update

--- server/api/domains/list.ts
+++ server/api/domains/list.ts
@@ -1,29 +1,26 @@
+import { Request, Response } from 'express';
 import { MySQL } from 'lib/MySQL';
 
-export async function getDomains(req, res) {
+export async function getDomains(req: Request, res: Response): Promise<void> {
   const db = new MySQL();
-
   try {
-    const rows = await db.query(
+    const domains = await db.query(
       `
-      SELECT
-        id, domain, (user_id = ?) AS isCreator, global
-      FROM domains
-      WHERE
-        global = 1 OR id IN (
-          SELECT domain_id FROM domain_users
-          WHERE user_id = ? AND authorized = 1
-        )
-    `,
+        SELECT
+          id, domain, (user_id = ?) AS isCreator, global
+        FROM domains
+        WHERE
+          global = 1 OR id IN (
+            SELECT domain_id FROM domain_users
+            WHERE user_id = ? AND authorized = 1
+          )
+      `,
       [req.session.uid, req.session.uid]
     );
-    db.release();
-
-    if (!rows.length) throw 'No domains';
-
-    res.status(200).json({ domains: rows });
+    if (!domains.length) throw 'No domains';
+    res.status(200).json(domains);
   } catch (err) {
-    db.release();
     res.status(400).json({ error: err });
   }
+  db.release();
 }
