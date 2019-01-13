dd9c47230961563a9ecdf2d5ef118dc3b1812ea9
MrXyfir <mr@xyfir.com>
2019-01-13 13:41:12 -0800

    Add 'create modifier 2' test

--- server/__tests__/modifiers.ts
+++ server/__tests__/modifiers.ts
@@ -1,9 +1,34 @@
 import { deleteModifier } from 'lib/modifiers/delete';
-import { editModifier } from 'lib/modifiers/edit';
-import { getModifier } from 'lib/modifiers/get';
 import { addModifier } from 'lib/modifiers/add';
+import 'lib/tests/prepare';
+import { Ptorx } from 'typings/ptorx';
 
-it('addModifier()', async () => {
-  const modifier = await addModifier({ type: 2 }, 1);
-  expect(typeof modifier).toBe('object');
+// ** create for all types
+// ** list
+// ** delete
+
+test('create modifier 2', async () => {
+  const modifier = await addModifier({ type: 2 }, 1234);
+  expect(Object.keys(modifier).length).toBe(17);
+  expect(typeof modifier.modifierId).toBe('number');
+  expect(typeof modifier.created).toBe('number');
+  const _modifier: Ptorx.Modifier = {
+    modifierId: modifier.modifierId,
+    name: '',
+    type: 2,
+    subject: null,
+    replacement: null,
+    flags: null,
+    regex: null,
+    prepend: null,
+    target: null,
+    add: null,
+    to: null,
+    separator: null,
+    find: null,
+    tag: null,
+    template: null,
+    created: modifier.created
+  };
+  expect(modifier).toMatchObject(_modifier);
 });
