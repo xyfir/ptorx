979b5a70b9482135170231015f86186cbc5c1e4f
MrXyfir <mr@xyfir.com>
2019-01-13 14:26:25 -0800

    Add listModifiers()

--- server/api/modifiers/list.ts
+++ server/api/modifiers/list.ts
@@ -1,24 +1,14 @@
 import { Request, Response } from 'express';
-import { MySQL } from 'lib/MySQL';
+import { listModifiers } from 'lib/modifiers/list';
 
-export async function api_getModifiers(
+export async function api_listModifiers(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
   try {
-    const modifiers = await db.query(
-      `
-        SELECT
-          userId AS uid, modifierId AS id, name, type,
-          IF(userId = 0, 1, 0) AS global
-        FROM modifiers WHERE userId = ? OR userId = 0
-      `,
-      [req.session.uid]
-    );
+    const modifiers = await listModifiers(req.session.uid);
     res.status(200).json(modifiers);
   } catch (err) {
     res.status(400).json({ error: err });
   }
-  db.release();
 }
---
+++ server/lib/modifiers/list.ts
@@ -0,0 +1,29 @@
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function listModifiers(
+  userId: number
+): Promise<Ptorx.ModifierList> {
+  const db = new MySQL();
+  try {
+    const modifiers: Ptorx.ModifierList = await db.query(
+      `
+        SELECT
+          modifierId, userId, name, type, created,
+          IF(userId = 0, 1, 0) AS global
+        FROM modifiers
+        WHERE userId = ? OR userId = 0
+        ORDER BY created DESC
+      `,
+      [userId]
+    );
+    db.release();
+    return modifiers.map(m => {
+      m.global = !!m.global;
+      return m;
+    });
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
