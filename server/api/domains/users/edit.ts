989e36de9853c3d69bf7e0cf50e93aa617d25859
MrXyfir <mr@xyfir.com>
2019-01-14 13:44:12 -0800

    Update 'edit domain user' and add lib module

--- server/api/domains/users/edit.ts
+++ server/api/domains/users/edit.ts
@@ -1,38 +1,14 @@
 import { Request, Response } from 'express';
-import * as moment from 'moment';
-import { MySQL } from 'lib/MySQL';
+import { editDomainUser } from 'lib/domains/users/edit';
 
 export async function api_editDomainUser(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
-
   try {
-    const result = await db.query(
-      `
-        UPDATE domain_users SET
-          label = ?, created = ?, authorized = 1
-        WHERE
-          domainId = ? AND requestKey = ? AND
-          (SELECT userId FROM domains WHERE id = ?) = ?
-      `,
-      [
-        req.body.label,
-        moment().unix(),
-        req.params.domain,
-        req.body.key,
-        req.params.domain,
-        req.session.uid
-      ]
-    );
-    db.release();
-
-    if (!result.affectedRows) throw 'Could not add user to domain';
-
+    await editDomainUser(req.body, req.session.uid);
     res.status(200).json({});
   } catch (err) {
-    db.release();
     res.status(400).json({ error: err });
   }
 }
---
+++ server/lib/domains/users/edit.ts
@@ -0,0 +1,39 @@
+import { getDomainUser } from 'lib/domains/users/get';
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function editDomainUser(
+  domainUser: Ptorx.DomainUser,
+  /** User id of domain owner */
+  userId: number
+): Promise<Ptorx.DomainUser> {
+  const db = new MySQL();
+  try {
+    const result = await db.query(
+      `
+        UPDATE domain_users SET label = ?, authorized = ?
+        WHERE
+          domainId = ? AND requestKey = ? AND
+          (SELECT userId FROM domains WHERE id = ?) = ?
+      `,
+      [
+        domainUser.label,
+        domainUser.authorized,
+        domainUser.domainId,
+        domainUser.requestKey,
+        domainUser.domainId,
+        userId
+      ]
+    );
+    db.release();
+    if (!result.affectedRows) throw 'Could not edit domain user';
+    return await getDomainUser(
+      domainUser.domainId,
+      domainUser.requestKey,
+      userId
+    );
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
