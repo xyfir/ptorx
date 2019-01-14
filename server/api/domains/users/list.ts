45d5a56a843926f39d5cb06be4ad98d701c1fc3c
MrXyfir <mr@xyfir.com>
2019-01-14 13:32:04 -0800

    Add 'list domain users'

--- server/api/domains/users/list.ts
+++ server/api/domains/users/list.ts
@@ -1,1 +1,17 @@
-export async function api_listDomainUsers(req, res) {}
+import { Request, Response } from 'express';
+import { listDomainUsers } from 'lib/domains/users/list';
+
+export async function api_listDomainUsers(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
+    const domainUsers = await listDomainUsers(
+      +req.params.domain,
+      req.session.uid
+    );
+    res.status(200).json(domainUsers);
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+}
---
+++ server/lib/domains/users/list.ts
@@ -0,0 +1,29 @@
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function listDomainUsers(
+  domainId: Ptorx.Domain['id'],
+  userId: number
+): Promise<Ptorx.DomainUserList> {
+  const db = new MySQL();
+  try {
+    const domains: Ptorx.DomainUserList = await db.query(
+      `
+        SELECT label, requestKey, created, authorized
+        FROM domain_users WHERE domainId IN (
+          SELECT id FROM domains WHERE id = ? AND userId = ?
+        )
+        ORDER BY created DESC
+      `,
+      [domainId, userId]
+    );
+    db.release();
+    return domains.map(d => {
+      d.authorized = !!d.authorized;
+      return d;
+    });
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
