0b9d1f5df1fcde0c0fa08c6f4e1f333ebf2fc8af
MrXyfir <mr@xyfir.com>
2019-01-14 14:59:08 -0800

    Add second test user (id: 12345)

--- server/lib/tests/setup.ts
+++ server/lib/tests/setup.ts
@@ -2,7 +2,7 @@ import { MySQL } from 'lib/MySQL';
 
 export async function setupTests() {
   const db = new MySQL();
-  await db.query('DELETE FROM users WHERE userId = 1234');
-  await db.query('INSERT INTO users (userId) VALUES (1234)');
+  await db.query('DELETE FROM users WHERE userId = 1234 OR userId = 12345');
+  await db.query('INSERT INTO users (userId) VALUES (1234), (12345)');
   db.release();
 }
--- server/lib/tests/teardown.ts
+++ server/lib/tests/teardown.ts
@@ -2,6 +2,6 @@ import { MySQL } from 'lib/MySQL';
 
 export async function teardownTests() {
   const db = new MySQL();
-  await db.query('DELETE FROM users WHERE userId = 1234');
+  await db.query('DELETE FROM users WHERE userId = 1234 OR userId = 12345');
   db.release();
 }
