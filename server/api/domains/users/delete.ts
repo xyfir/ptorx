e5abc09d187453aa2b81e7d8e9f820f2d92580b8
MrXyfir <mr@xyfir.com>
2019-01-14 13:56:41 -0800

    Update 'delete doman user' and add lib module

--- server/api/domains/users/delete.ts
+++ server/api/domains/users/delete.ts
@@ -1,58 +1,14 @@
-import { api_deleteProxyEmail } from 'api/proxy-emails/delete';
 import { Request, Response } from 'express';
-import { MySQL } from 'lib/MySQL';
+import { deleteDomainUser } from 'lib/domains/users/delete';
 
 export async function api_deleteDomainUser(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
-
   try {
-    let result;
-
-    // Remove self from domain
-    if (req.params.user == req.session.uid) {
-      result = await db.query(
-        'DELETE FROM domain_users WHERE domainId = ? AND userId = ?',
-        [req.params.domain, req.params.user]
-      );
-    }
-    // Remove another user from domain
-    else {
-      result = await db.query(
-        `
-        DELETE FROM domain_users WHERE userId = ? AND domainId IN (
-          SELECT id FROM domains WHERE id = ? AND userId = ?
-        )
-      `,
-        [req.params.user, req.params.domain, req.session.uid]
-      );
-    }
-
-    if (!result.affectedRows) throw 'Could not remove from domain';
-
-    // Get all of user's proxy emails on this domain
-    const emails = await db.query(
-      `
-      SELECT proxyEmailId AS id FROM proxy_emails
-      WHERE userId = ? AND domainId = ?
-    `,
-      [req.params.user, req.params.domain]
-    );
-    db.release();
-
-    // Delete emails
-    for (let email of emails) {
-      await api_deleteProxyEmail(
-        { params: { email: email.id }, session: { uid: req.params.user } },
-        { json: () => 1 }
-      );
-    }
-
+    await deleteDomainUser(+req.params.domain, req.params.key, req.session.uid);
     res.status(200).json({});
   } catch (err) {
-    db.release();
     res.status(400).json({ error: err });
   }
 }
---
+++ server/lib/domains/users/delete.ts
@@ -0,0 +1,39 @@
+import { deleteProxyEmail } from 'lib/proxy-emails/delete';
+import { getDomainUser } from 'lib/domains/users/get';
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function deleteDomainUser(
+  domainId: Ptorx.Domain['id'],
+  /** UUID key of the user to remove */
+  domainUserKey: Ptorx.DomainUser['requestKey'],
+  /** User id of the _domain owner_ */
+  userId: number
+): Promise<void> {
+  const db = new MySQL();
+  try {
+    const domainUser = await getDomainUser(domainId, domainUserKey, userId);
+    if (domainUser.userId == userId) throw 'You cannot delete yourself';
+
+    await db.query(
+      `
+        DELETE FROM domain_users WHERE userId = ? AND domainId IN (
+          SELECT id FROM domains WHERE id = ? AND userId = ?
+        )
+      `,
+      [domainUser.userId, domainId, userId]
+    );
+
+    const proxyEmails: { proxyEmailId: number }[] = await db.query(
+      'SELECT proxyEmailId FROM proxy_emails WHERE userId = ? AND domainId = ?',
+      [domainUser.userId, domainId]
+    );
+    for (let proxyEmail of proxyEmails) {
+      await deleteProxyEmail(db, proxyEmail.proxyEmailId, domainUser.userId);
+    }
+    db.release();
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
