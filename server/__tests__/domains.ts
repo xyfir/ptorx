ec0efaff2104f7727d89de607abde0bf9e5d6623
MrXyfir <mr@xyfir.com>
2019-01-14 14:59:19 -0800

    Add domain user tests

--- server/__tests__/domains.ts
+++ server/__tests__/domains.ts
@@ -1,10 +1,12 @@
+import { listDomainUsers } from 'lib/domains/users/list';
+import { addDomainUser } from 'lib/domains/users/add';
 import { verifyDomain } from 'lib/domains/verify';
 import { deleteDomain } from 'lib/domains/delete';
 import { listDomains } from 'lib/domains/list';
 import { addDomain } from 'lib/domains/add';
+import { getDomain } from 'lib/domains/get';
 import { Ptorx } from 'typings/ptorx';
 import 'lib/tests/prepare';
-import { getDomain } from 'lib/domains/get';
 
 test('create domain 1', async () => {
   const domain = await addDomain({ domain: 'example.com' }, 1234);
@@ -42,6 +44,37 @@ test('verify domain', async () => {
   await verifyDomain(domain.id, 1234);
   const _domain = await getDomain(domain.id, 1234);
   expect(_domain.verified).toBeTrue();
+});
+
+test('add domain user', async () => {
+  const domainUser = await addDomainUser('example.com', 12345);
+  expect(Object.keys(domainUser).length).toBe(6);
+  expect(domainUser.created).toBeNumber();
+  expect(domainUser.requestKey).toBeString();
+  expect(domainUser.requestKey).toBeTruthy();
+  const _domainUser: Ptorx.DomainUser = {
+    ...domainUser,
+    userId: 12345,
+    label: '',
+    authorized: false
+  };
+  expect(domainUser).toMatchObject(_domainUser);
+});
+
+test('list domain users', async () => {
+  const domains = await listDomains(1234);
+  const domain = await domains.find(d => !d.global);
+  const domainUsers = await listDomainUsers(domain.id, 1234);
+  expect(domainUsers).toBeArrayOfSize(2);
+  const keys: Array<keyof Ptorx.DomainUserList[0]> = [
+    'authorized',
+    'created',
+    'label',
+    'requestKey'
+  ];
+  expect(domainUsers[0]).toContainAllKeys(keys);
+  expect(domainUsers.find(u => u.authorized)).not.toBeUndefined();
+  expect(domainUsers.find(u => !u.authorized)).not.toBeUndefined();
 });
 
 test('delete domain', async () => {
