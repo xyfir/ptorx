813b1ff2d7f2f8b6000ab912f0fa2561d10f59b6
MrXyfir <mr@xyfir.com>
2019-01-13 19:27:16 -0800

    Update 'edit filter' and add lib module

--- server/api/filters/edit.ts
+++ server/api/filters/edit.ts
@@ -1,69 +1,14 @@
 import { Request, Response } from 'express';
-import { requireCredits } from 'lib/users/require-credits';
-import { validateFilter } from 'lib/filters/validate';
-import { MySQL } from 'lib/MySQL';
+import { editFilter } from 'lib/filters/edit';
 
 export async function api_editFilter(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
-
   try {
-    const valid = validateFilter(req.body);
-    if (valid != 'ok') throw valid;
-
-    await requireCredits(db, +req.session.uid);
-
-    const [filter] = await db.query(
-      `
-        SELECT type, acceptOnMatch AS acceptOnMatch FROM filters
-        WHERE filterId = ? AND userId = ?
-      `,
-      [req.params.filter, req.session.uid]
-    );
-    if (!filter) throw 'Could not find filter';
-
-    const result = await db.query(
-      `
-        UPDATE filters SET name = ?, type = ?, find = ?,
-        acceptOnMatch = ?, regex = ?
-        WHERE filterId = ?
-      `,
-      [
-        req.body.name,
-        req.body.type,
-        req.body.find,
-        !!+req.body.acceptOnMatch,
-        !!+req.body.regex,
-        req.params.filter
-      ]
-    );
-    if (!result.affectedRows) throw 'Could not update filter';
-
-    const rows = await db.query(
-      `SELECT proxyEmailId AS id FROM links WHERE filterId = ?`,
-      [req.params.filter]
-    );
-
-    /** @type {number[]} */
-    let update = [];
-
-    if (rows.length) {
-      const emails = rows.map(email => email.id);
-
-      if (
-        // Determine if Mailgun routes need to be updated
-        ([1, 2, 3, 6].indexOf(filter.type) > -1 && !!+filter.acceptOnMatch) ||
-        ([1, 2, 3, 6].indexOf(+req.body.type) > -1 && req.body.acceptOnMatch)
-      )
-        update = emails;
-    }
-
-    res.status(200).json({ update });
+    const filter = await editFilter(req.body, req.session.uid);
+    res.status(200).json(filter);
   } catch (err) {
     res.status(400).json({ error: err });
   }
-
-  db.release();
 }
---
+++ server/lib/filters/edit.ts
@@ -0,0 +1,43 @@
+import { requireCredits } from 'lib/users/require-credits';
+import { getFilter } from 'lib/filters/get';
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function editFilter(
+  filter: Ptorx.Filter,
+  userId: number
+): Promise<Ptorx.Filter> {
+  const db = new MySQL();
+  try {
+    if (!filter.type) throw 'Invalid type';
+    else if (typeof filter.name != 'string') throw 'Missing name';
+    else if (typeof filter.find != 'string') throw 'Missing "find"';
+    else if (filter.type == 6 && filter.find.indexOf(':::') === -1)
+      throw 'Bad header filter';
+
+    await requireCredits(userId);
+    const result = await db.query(
+      `
+        UPDATE filters SET
+          name = ?, type = ?, find = ?, acceptOnMatch = ?, regex = ?
+        WHERE filterId = ? AND userId = ?
+      `,
+      [
+        filter.name,
+        filter.type,
+        filter.find,
+        filter.acceptOnMatch,
+        filter.regex,
+        filter.filterId,
+        filter.userId
+      ]
+    );
+    if (!result.affectedRows) throw 'Could not update filter';
+
+    db.release();
+    return getFilter(filter.filterId, userId);
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
