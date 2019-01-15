97a97dc25f3f25192a68b8dce1916252936a7df8
MrXyfir <mr@xyfir.com>
2019-01-15 14:36:58 -0800

    Add editMessage()

---
+++ server/lib/messages/edit.ts
@@ -0,0 +1,27 @@
+import { getMessage } from 'lib/messages/get';
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
+
+export async function editMessage(
+  message: Ptorx.Message,
+  userId: number
+): Promise<Ptorx.Message> {
+  const db = new MySQL();
+  try {
+    const result = await db.query(
+      `
+        UPDATE messages m SET m.subject = ?, m.sender = ?, m.type = ?
+        WHERE m.id = ? AND m.proxyEmailId = (
+          SELECT pxe.proxyEmailId FROM proxy_emails
+          WHERE pxe.proxyEmailId = m.proxyEmailId pxe.userId = ?
+        )
+      `,
+      [message.subject, message.sender, message.type, message.id, userId]
+    );
+    if (!result.affectedRows) throw 'Could not edit message';
+    return await getMessage(message.id, userId);
+  } catch (err) {
+    db.release();
+    throw err;
+  }
+}
