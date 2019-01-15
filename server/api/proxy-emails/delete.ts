342d045715a7f7a6259d42951d746e8c9accf84e
MrXyfir <mr@xyfir.com>
2019-01-15 11:47:17 -0800

    Update deleteProxyEmail() and API controller

--- server/api/proxy-emails/delete.ts
+++ server/api/proxy-emails/delete.ts
@@ -1,19 +1,14 @@
-import { deleteProxyEmail } from 'lib/proxy-emails/delete';
 import { Request, Response } from 'express';
-import { MySQL } from 'lib/MySQL';
+import { deleteProxyEmail } from 'lib/proxy-emails/delete';
 
 export async function api_deleteProxyEmail(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
-
   try {
-    await deleteProxyEmail(db, +req.params.email, +req.session.uid);
-    db.release();
+    await deleteProxyEmail(+req.params.email, +req.session.uid);
     res.status(200).json({});
   } catch (err) {
-    db.release();
     res.status(400).json({ error: err });
   }
 }
--- server/lib/proxy-emails/delete.ts
+++ server/lib/proxy-emails/delete.ts
@@ -1,51 +1,29 @@
-import * as Mailgun from 'mailgun-js';
-import * as CONFIG from 'constants/config';
+import { Ptorx } from 'typings/ptorx';
+import { MySQL } from 'lib/MySQL';
 
-/**
- * @async
- * @param {object} db
- * @param {number} email
- * @param {number} user
- * @throws {string}
- */
-export async function deleteProxyEmail(db, email, user) {
-  const [row] = await db.query(
-    `
-      SELECT
-        pxe.mgRouteId AS mgRouteId, d.domain
-      FROM
-        proxy_emails AS pxe, domains AS d
-      WHERE
-        pxe.proxyEmailId = ? AND pxe.userId = ? AND d.id = pxe.domainId
-    `,
-    [email, user]
-  );
+export async function deleteProxyEmail(
+  proxyEmailId: Ptorx.ProxyEmail['proxyEmailId'],
+  userId: number
+): Promise<void> {
+  const db = new MySQL();
+  try {
+    // Remove any information that could be linked to the creator
+    // Keep in database so that a 'deleted' proxy email cannot be created again
+    const result = await db.query(
+      `
+        UPDATE proxy_emails SET userId = NULL, name = NULL
+        WHERE proxyEmailId = ? AND userId
+      `,
+      [proxyEmailId, userId]
+    );
+    if (!result.affectedRows) throw 'Could not delete proxy email';
 
-  if (!row) throw 'Could not find email';
-
-  // Remove any information that could be linked to the creator
-  // Keep in database so that a 'deleted' proxy email cannot be created again
-  await db.query(
-    `
-      UPDATE proxy_emails SET
-        userId = NULL, primaryEmailId = NULL, name = NULL, mgRouteId = NULL
-      WHERE proxyEmailId = ?
-    `,
-    [email]
-  );
-
-  if (row.mgRouteId) {
-    const mailgun = Mailgun({
-      apiKey: CONFIG.MAILGUN_KEY,
-      domain: row.domain
-    });
-    // @ts-ignore
-    await mailgun.routes(row.mgRouteId).delete();
+    // Because proxy email is not actually getting deleted and thus won't
+    // trigger an ON DELETE CASCADE we must manually remove the links
+    await db.query('DELETE FROM links WHERE proxyEmailId = ?', [proxyEmailId]);
+    db.release();
+  } catch (err) {
+    db.release();
+    throw err;
   }
-
-  // Because proxy email is not actually getting deleted and thus won't
-  // trigger a foreign key ON DELETE CASCADE we must manually remove the
-  // linked filters and modifiers
-  await db.query('DELETE FROM links WHERE proxyEmailId = ?', [email]);
-  await db.query('DELETE FROM links WHERE proxyEmailId = ?', [email]);
 }
