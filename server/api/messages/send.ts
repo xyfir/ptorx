dc0e228cedd7c94151631dd8a64769ad8f2019ff
MrXyfir <mr@xyfir.com>
2019-01-15 18:49:27 -0800

    Update 'send message' and add lib module

--- server/api/messages/send.ts
+++ server/api/messages/send.ts
@@ -1,51 +1,14 @@
 import { Request, Response } from 'express';
-import { chargeUser } from 'lib/users/charge';
-import * as Mailgun from 'mailgun-js';
-import * as CONFIG from 'constants/config';
-import { MySQL } from 'lib/MySQL';
+import { sendMessage } from 'lib/messages/send';
 
 export async function api_sendMessage(
   req: Request,
   res: Response
 ): Promise<void> {
-  const db = new MySQL();
-
   try {
-    const [row] = await db.query(
-      `
-        SELECT
-          pxe.address, d.domain
-        FROM
-          domains AS d, proxy_emails AS pxe, users AS u, primary_emails AS pme
-        WHERE
-          pxe.proxyEmailId = ? AND u.userId = ? AND
-          pme.proxyEmailId = pxe.primaryEmailId AND
-          pxe.userId = u.userId AND
-          d.id = pxe.domainId
-      `,
-      [req.params.email, req.session.uid]
-    );
-
-    if (!row) throw 'Email does not exist';
-
-    const credits = await chargeUser(db, +req.session.uid, 1);
-    db.release();
-
-    const mailgun = Mailgun({
-      apiKey: CONFIG.MAILGUN_KEY,
-      domain: row.domain
-    });
-
-    await mailgun.messages().send({
-      to: req.body.to,
-      from: row.address + '@' + row.domain,
-      text: req.body.content,
-      subject: req.body.subject
-    });
-
-    res.status(200).json({ credits });
+    await sendMessage(req.body, req.session.uid);
+    res.status(200).json({});
   } catch (err) {
-    db.release();
     res.status(400).json({ error: err });
   }
 }
---
+++ server/lib/messages/send.ts
@@ -0,0 +1,23 @@
+import { getProxyEmail } from 'lib/proxy-emails/get';
+import { chargeUser } from 'lib/users/charge';
+import { Ptorx } from 'typings/ptorx';
+
+/**
+ * @todo Actually send email
+ */
+export async function sendMessage(
+  data: {
+    proxyEmailId: Ptorx.ProxyEmail['proxyEmailId'];
+    content: string;
+    subject: string;
+    to: string;
+  },
+  userId: number
+): Promise<void> {
+  try {
+    const proxyEmail = await getProxyEmail(data.proxyEmailId, userId);
+    await chargeUser(userId, 1);
+  } catch (err) {
+    throw err;
+  }
+}
