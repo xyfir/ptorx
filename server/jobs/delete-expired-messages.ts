54b48b239470823c98d8fd1c8246d4f84b624ca8
MrXyfir <mr@xyfir.com>
2019-01-16 13:33:41 -0800

    Update deleteExpiredMessages()

--- server/jobs/delete-expired-messages.ts
+++ server/jobs/delete-expired-messages.ts
@@ -2,24 +2,11 @@ import { MySQL } from 'lib/MySQL';
 
 export async function deleteExpiredMessages() {
   const db = new MySQL();
-
   try {
-    // Messages expire from Mailgun after 3 days
-    // These messages can no longer be accessed from the Ptorx app
-    await db.query(`
-      UPDATE messages
-      SET url = ''
-      WHERE created + 255600 < UNIX_TIMESTAMP() AND url != ''
-    `);
-
-    // Messages can no longer be replied to from outside of the Ptorx app
-    // after 30 days
+    // Delete after 30 days
     await db.query(`
       DELETE FROM messages WHERE created + 2592000 < UNIX_TIMESTAMP()
     `);
-
-    db.release();
-  } catch (err) {
-    db.release();
-  }
+  } catch (err) {}
+  db.release();
 }
