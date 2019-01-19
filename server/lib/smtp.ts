e28b35288641c3d46cb8fac9c0db3beeeb76ac3b
MrXyfir <mr@xyfir.com>
2019-01-19 11:35:43 -0800

    Implement modifiers

--- server/lib/smtp.ts
+++ server/lib/smtp.ts
@@ -194,6 +194,68 @@ const server = new SMTPServer({
         }
         // Modify mail
         else if (link.modifierId) {
+          const modifier = await getModifier(link.modifierId, recipient.userId);
+          switch (modifier.type) {
+            case 'text-only':
+              delete modified.html;
+              break;
+            case 'replace':
+              // Escape search if not regular expression
+              modifier.find = !modifier.regex
+                ? escapeRegExp(modifier.find)
+                : modifier.replacement;
+              // Escape '$' if not regular expression
+              modifier.replacement = !modifier.regex
+                ? modifier.replacement.replace(/\$/g, '$$')
+                : modifier.replacement;
+              // Replace text in body text and html
+              modified.text = (modified.text as string).replace(
+                new RegExp(modifier.find, modifier.flags),
+                modifier.replacement
+              );
+              modified.html =
+                modified.html &&
+                (modified.html as string).replace(
+                  new RegExp(modifier.find, modifier.flags),
+                  modifier.replacement
+                );
+              break;
+            case 'subject':
+              modified.subject = modifier.subject;
+              break;
+            case 'tag':
+              modified.subject = modifier.prepend
+                ? modifier.tag + modified.subject
+                : modified.subject + modifier.tag;
+              break;
+            case 'concat':
+              modified[modifier.to] = modifier.prepend
+                ? modified[modifier.add] +
+                  modifier.separator +
+                  modified[modifier.to]
+                : modified[modifier.to] +
+                  modifier.separator +
+                  modified[modifier.add];
+              break;
+            case 'builder':
+              modified[modifier.target] = modifier.template
+                .replace(
+                  /{{html}}/g,
+                  typeof modified.html == 'string' ? modified.html : ''
+                )
+                .replace(/{{text}}/g, modified.text as string)
+                .replace(/{{sender}}/g, session.ptorx.from.address)
+                .replace(/{{subject}}/g, modified.subject)
+                .replace(
+                  /{{header\('(.+)', '(.+)'\)}}/g,
+                  (m, p1: string, p2: string): string => {
+                    const header = original.headerLines.find(h => h.key == p1);
+                    return header
+                      ? header.line.substr(header.key.length + 2)
+                      : p2;
+                  }
+                );
+          }
         }
         // Forward mail
         else if (link.primaryEmailId) {
