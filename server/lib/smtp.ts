a84ebf28500fb9129a0329963811387f940e52a9
MrXyfir <mr@xyfir.com>
2019-01-20 15:15:25 -0800

    Support replying to forwarded mail through proxy email

--- server/lib/smtp.ts
+++ server/lib/smtp.ts
@@ -4,8 +4,10 @@ import * as escapeRegExp from 'escape-string-regexp';
 import { getProxyEmail } from 'lib/proxy-emails/get';
 import { simpleParser } from 'mailparser';
 import { getModifier } from 'lib/modifiers/get';
+import { getMessage } from 'lib/messages/get';
 import { addMessage } from 'lib/messages/add';
 import { SMTPServer } from 'smtp-server';
+import { getDomain } from 'lib/domains/get';
 import { getFilter } from 'lib/filters/get';
 import { Ptorx } from 'typings/ptorx';
 import { MySQL } from 'lib/MySQL';
@@ -13,6 +15,7 @@ import { MySQL } from 'lib/MySQL';
 interface AddressInfo {
   proxyEmailId?: Ptorx.ProxyEmail['id'];
   domainId?: number;
+  message?: Ptorx.Message;
   address: string;
   userId?: number;
 }
@@ -36,12 +39,26 @@ async function getAddressInfo(address: string): Promise<AddressInfo> {
   const [user, domain] = address.split('@');
   const db = new MySQL();
   try {
-    const [row]: {
-      proxyEmailId: Ptorx.ProxyEmail['id'];
-      domainId: Ptorx.ProxyEmail['domainId'];
-      userId: Ptorx.ProxyEmail['userId'];
-    }[] = await db.query(
-      `
+    // Reply-To address
+    if (user.endsWith('--reply')) {
+      try {
+        const [userId, messageId, messageKey] = user.split('--');
+        const message = await getMessage(+messageId, +userId);
+        if (message.key != messageKey) throw new Error('Message key mismatch');
+        return { address, message, userId: +userId };
+      } catch (err) {
+        // ** Send email response explaining problem
+        throw new Error('Bad message reply address');
+      }
+    }
+    // Check if proxy address
+    else {
+      const [row]: {
+        proxyEmailId: Ptorx.ProxyEmail['id'];
+        domainId: Ptorx.ProxyEmail['domainId'];
+        userId: Ptorx.ProxyEmail['userId'];
+      }[] = await db.query(
+        `
         SELECT pxe.id AS proxyEmailId, pxe.userId, d.id AS domainId
         FROM domains d
         LEFT JOIN proxy_emails pxe ON
@@ -50,15 +67,17 @@ async function getAddressInfo(address: string): Promise<AddressInfo> {
         WHERE
           d.domain = ? AND d.verified = ?
       `,
-      [user, domain, true]
-    );
+        [user, domain, true]
+      );
+      db.release();
 
-    // Not a verified Ptorx domain
-    if (!row) return { address };
-    // Not an active proxy email on verified Ptorx domain
-    if (!row.proxyEmailId) throw new Error('User does not exist on domain');
-    // Valid, active proxy email
-    return { ...row, address };
+      // Not a verified Ptorx domain
+      if (!row) return { address };
+      // Not an active proxy email on verified Ptorx domain
+      if (!row.proxyEmailId) throw new Error('User does not exist on domain');
+      // Valid, active proxy email
+      return { ...row, address };
+    }
   } catch (err) {
     db.release();
     throw err;
@@ -120,12 +139,56 @@ const server = new SMTPServer({
 
     for (let recipient of session.to) {
       // Ignore if not for Ptorx
-      if (!recipient.proxyEmailId) continue;
+      if (!recipient.proxyEmailId && !recipient.message) continue;
 
       const proxyEmail = await getProxyEmail(
         recipient.proxyEmailId,
         recipient.userId
       );
+
+      // Handle reply to a stored message
+      if (recipient.message) {
+        const proxyEmail = await getProxyEmail(
+          recipient.message.proxyEmailId,
+          recipient.userId
+        );
+        const domain = await getDomain(proxyEmail.domainId, proxyEmail.userId);
+
+        const fullAddress = `${proxyEmail.address}@${domain.domain}`;
+        const transporter = createTransport({ sendmail: true });
+        await transporter.sendMail({
+          subject: original.subject,
+          sender: fullAddress,
+          from: fullAddress,
+          html: typeof original.html == 'string' ? original.html : undefined,
+          text: original.text,
+          to: recipient.message.from
+        });
+        continue;
+      }
+
+      // Save message
+      let savedMessage: Ptorx.Message;
+      if (proxyEmail.saveMail) {
+        savedMessage = await addMessage(
+          {
+            attachments: original.attachments.map(a => ({
+              filename: a.filename,
+              contentType: a.contentType,
+              content: a.content,
+              size: a.content.byteLength
+            })),
+            from: original.from.text,
+            headers: original.headerLines.map(h => h.line),
+            html: typeof original.html == 'string' ? original.html : undefined,
+            proxyEmailId: proxyEmail.id,
+            subject: original.subject,
+            text: original.text,
+            to: original.to.text
+          },
+          proxyEmail.userId
+        );
+      }
 
       for (let link of proxyEmail.links) {
         // Filter mail
@@ -241,33 +304,16 @@ const server = new SMTPServer({
           const transporter = createTransport({ sendmail: true });
           await transporter.sendMail({
             ...modified,
+            replyTo: savedMessage
+              ? `${proxyEmail.userId}--${savedMessage.id}--${
+                  savedMessage.key
+                }--reply@${recipient.address.split('@')[1]}`
+              : undefined,
             sender: recipient.address,
             from: recipient.address,
             to: primaryEmail.address
           });
         }
-      }
-
-      // Save message
-      if (proxyEmail.saveMail) {
-        await addMessage(
-          {
-            attachments: original.attachments.map(a => ({
-              filename: a.filename,
-              contentType: a.contentType,
-              content: a.content,
-              size: a.content.byteLength
-            })),
-            from: original.from.text,
-            headers: original.headerLines.map(h => h.line),
-            html: typeof original.html == 'string' ? original.html : undefined,
-            proxyEmailId: proxyEmail.id,
-            subject: original.subject,
-            text: original.text,
-            to: original.to.text
-          },
-          proxyEmail.userId
-        );
       }
     }
 
