d108c0d0e3d1bbbe4f8e6db3739756c8e6726437
MrXyfir <mr@xyfir.com>
2019-01-25 14:18:46 -0800

    Add default config file

---
+++ server/constants/config.default.ts
@@ -0,0 +1,56 @@
+import { SMTPServerOptions } from 'smtp-server';
+import { readFileSync } from 'fs';
+
+export const PROD = false;
+
+export const CRON = false;
+
+export const MYSQL = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  charset: 'UTF8MB4_UNICODE_CI',
+  password: '',
+  database: 'ptorx',
+  dateStrings: true,
+  connectionLimit: 100,
+  supportBigNumbers: true,
+  waitForConnections: true
+};
+
+export const API_PORT = 2070;
+
+export const SMTP_PORT = 2071;
+
+export const PTORX_URL = 'http://localhost:2070';
+
+export const XYPAYMENTS_ID = 13;
+
+export const TEST_SMTP_PORT = 2072;
+
+export const XYPAYMENTS_URL = 'http://localhost:2062';
+
+export const XYPAYMENTS_KEY = '';
+
+export const SESSION_SECRET = '';
+
+export const XYACCOUNTS_KEY = '';
+
+export const XYACCOUNTS_URL = 'http://localhost:2000';
+
+export const ACCESS_TOKEN_KEY = '';
+
+export const PTORX_CALLBACK_URL = 'http://';
+
+export const XYPAYMENTS_PRODUCTS = {
+  1: 10,
+  2: 11,
+  3: 12
+};
+
+export const DIRECTORIES = {
+  MAIL_CACHE: '',
+  WEB: ''
+};
+
+export const SMTP_SERVER_OPTIONS: SMTPServerOptions = {};
