298bfff69aeaf05ff025888d0b4d243a3bc5ed0b
MrXyfir <mr@xyfir.com>
2019-01-27 11:27:54 -0800

    Resolve promise when server is closed

--- server/lib/tests/capture-mail.ts
+++ server/lib/tests/capture-mail.ts
@@ -2,27 +2,29 @@ import { SMTPServer, SMTPServerSession } from 'smtp-server';
 import { simpleParser, ParsedMail } from 'mailparser';
 import * as CONFIG from 'constants/config';
 
-export async function captureMail(
+export function captureMail(
   expected: number,
   fn: (message: ParsedMail, session: SMTPServerSession) => void
 ): Promise<void> {
-  let received = 0;
-  let server: SMTPServer;
-  try {
-    server = new SMTPServer({
-      authOptional: true,
-      async onData(stream, session, callback) {
-        const message = await simpleParser(stream);
-        fn(message, session);
-        callback();
-        if (++received >= expected) server.close(() => 1);
-      }
-    });
-    server.on('error', e => {
-      throw e;
-    });
-    server.listen(CONFIG.TEST_SMTP_PORT);
-  } catch (err) {
-    server && server.close(() => 1);
-  }
+  return new Promise(resolve => {
+    let received = 0;
+    let server: SMTPServer;
+    try {
+      server = new SMTPServer({
+        authOptional: true,
+        async onData(stream, session, callback) {
+          const message = await simpleParser(stream);
+          fn(message, session);
+          callback();
+          if (++received >= expected) server.close(resolve);
+        }
+      });
+      server.on('error', e => {
+        throw e;
+      });
+      server.listen(CONFIG.TEST_SMTP_PORT);
+    } catch (err) {
+      server ? server.close(resolve) : resolve();
+    }
+  });
 }
