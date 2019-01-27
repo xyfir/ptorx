9df5a43765971bf084a4b5915d851ab27d7c1366
MrXyfir <mr@xyfir.com>
2019-01-26 18:47:37 -0800

    Finish reply and send modules to actually send mail

--- server/lib/messages/reply.ts
+++ server/lib/messages/reply.ts
@@ -1,22 +1,24 @@
+import { sendMessage } from 'lib/messages/send';
 import { getMessage } from 'lib/messages/get';
-import { chargeUser } from 'lib/users/charge';
 import { Ptorx } from 'typings/ptorx';
-import { MySQL } from 'lib/MySQL';
 
-/**
- * @todo Actually reply to message
- */
 export async function replyToMessage(
   messageId: Ptorx.Message['id'],
   content: string,
   userId: number
 ): Promise<void> {
-  const db = new MySQL();
   try {
     const message = await getMessage(messageId, userId);
-    await chargeUser(userId, 1);
+    await sendMessage(
+      {
+        proxyEmailId: message.proxyEmailId,
+        subject: message.subject,
+        content,
+        to: message.from
+      },
+      userId
+    );
   } catch (err) {
-    db.release();
     throw err;
   }
 }
--- server/lib/messages/send.ts
+++ server/lib/messages/send.ts
@@ -1,10 +1,9 @@
 import { getProxyEmail } from 'lib/proxy-emails/get';
 import { chargeUser } from 'lib/users/charge';
+import { getDomain } from 'lib/domains/get';
+import { sendMail } from 'lib/mail/send';
 import { Ptorx } from 'typings/ptorx';
 
-/**
- * @todo Actually send email
- */
 export async function sendMessage(
   data: {
     proxyEmailId: Ptorx.ProxyEmail['id'];
@@ -16,6 +15,13 @@ export async function sendMessage(
 ): Promise<void> {
   try {
     const proxyEmail = await getProxyEmail(data.proxyEmailId, userId);
+    const domain = await getDomain(proxyEmail.domainId, userId);
+    await sendMail(domain.id, {
+      subject: data.subject,
+      from: `${proxyEmail.address}@${domain.domain}`,
+      text: data.content,
+      to: data.to
+    });
     await chargeUser(userId, 1);
   } catch (err) {
     throw err;
