45fda0137d573d1030edc0c695789b28932a4d49
MrXyfir <mr@xyfir.com>
2019-02-02 12:33:16 -0800

    Update config

--- server/constants/config.default.ts
+++ server/constants/config.default.ts
@@ -1,10 +1,10 @@
 import { SMTPServerOptions } from 'smtp-server';
 import { readFileSync } from 'fs';
 
+export const URL = 'http://';
+export const NAME = '';
 export const PROD = false;
-
 export const CRON = false;
-
 export const MYSQL = {
   host: 'localhost',
   port: 3306,
@@ -17,50 +17,23 @@ export const MYSQL = {
   supportBigNumbers: true,
   waitForConnections: true
 };
-
-export const NAME = '';
-
 export const DOMAIN = '';
-
-export const DOMAIN_ID = 4;
-
-export const PERSISTENT_PROXY_EMAIL = '';
-
-export const TEST_SMTP_PORT = 2072;
-
+export const JWT_KEY = '';
 export const API_PORT = 2070;
-
+export const DOMAIN_ID = 4;
 export const SMTP_PORT = 2071;
-
-export const URL = 'http://localhost:2070';
-
-export const XYPAYMENTS_ID = 13;
-
-export const XYPAYMENTS_URL = 'http://localhost:2062';
-
-export const XYPAYMENTS_KEY = '';
-
-export const SESSION_SECRET = '';
-
-export const XYACCOUNTS_KEY = '';
-
-export const XYACCOUNTS_URL = 'http://localhost:2000';
-
-export const ACCESS_TOKEN_KEY = '';
-
-export const CALLBACK_URL = 'http://9973719b.ngrok.io';
-
-export const XYPAYMENTS_PRODUCTS = {
-  1: 10,
-  2: 11,
-  3: 12
-};
-
 export const DIRECTORIES = {
   MAIL_CACHE: '',
   WEB: ''
 };
-
+export const WEBPACK_PORT = 2073;
+export const CALLBACK_URL = 'http://';
+export const XYPAYMENTS_ID = 13;
+export const TEST_SMTP_PORT = 2072;
+export const XYPAYMENTS_KEY = '';
+export const XYPAYMENTS_URL = 'http://';
+export const XYPAYMENTS_PRODUCTS = { 1: 10, 2: 11, 3: 12 };
+export const PERSISTENT_PROXY_EMAIL = '';
 export const SMTP_SERVER_OPTIONS: SMTPServerOptions = {
   banner: '',
   logger: true,
