491c5961c9cc2b8d24098796503e94b62f8186d2
MrXyfir <mr@xyfir.com>
2019-02-02 12:28:43 -0800

    Add test for getUser()

--- server/__tests__/users.ts
+++ server/__tests__/users.ts
@@ -1,5 +1,21 @@
 import { chargeUser } from 'lib/users/charge';
+import { getUser } from 'lib/users/get';
+import { Ptorx } from 'typings/ptorx';
 import 'lib/tests/prepare';
+
+test('get user', async () => {
+  const userId = Date.now();
+  const _user: Ptorx.User = {
+    credits: 100,
+    email: 'test@example.com',
+    emailTemplate: null,
+    userId
+  };
+  let user = await getUser({ userId, email: 'test@example.com' });
+  expect(user).toMatchObject(_user);
+  user = await getUser({ userId, email: 'test@example.com' });
+  expect(user).toMatchObject(_user);
+});
 
 test('charge user', async () => {
   expect(await chargeUser(1234, 3)).toBe(2);
