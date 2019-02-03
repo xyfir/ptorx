e0b39115cab75a0d74d61b746fe7aa8904224124
MrXyfir <mr@xyfir.com>
2019-02-03 12:58:24 -0800

    Add theme

---
+++ web/constants/theme.ts
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: { main: '#6a1b9a' },
+    secondary: { main: '#3f51b5' }
+  }
+});
