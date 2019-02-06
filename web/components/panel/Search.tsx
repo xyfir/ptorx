af9f6df96f5a47e0b5aabf313d03742fb5c318ae
MrXyfir <mr@xyfir.com>
2019-02-06 12:05:49 -0800

    Fix and improve <Search>

--- web/components/panel/Search.tsx
+++ web/components/panel/Search.tsx
@@ -1,4 +1,5 @@
 import { TextField, ListItemText, ListItem, List } from '@material-ui/core';
+import { DomainMatches } from 'components/panel/domains/Matches';
 import { PanelContext } from 'lib/PanelContext';
 import * as moment from 'moment';
 import * as React from 'react';
@@ -10,27 +11,38 @@ export class Search extends React.Component {
   static contextType = PanelContext;
   context!: React.ContextType<typeof PanelContext>;
 
-  search<T>(items: T[]): T[] {
+  search<
+    T extends {
+      fullAddress?: string;
+      subject?: string;
+      address?: string;
+      domain?: string;
+      name?: string;
+      from?: string;
+    }
+  >(items: T[]): T[] {
     const { search } = this.context;
     if (search) {
       const fuse = new Fuse(items, {
         shouldSort: true,
         threshold: 0.4,
-        // @ts-ignore
-        keys: ['name', 'domain', 'fullAddress']
+        keys: ['from', 'name', 'domain', 'address', 'subject', 'fullAddress']
       });
-      items = fuse.search(search.toLowerCase());
+      items = fuse.search(search);
     }
     return items;
   }
 
   renderPrimaryEmails(primaryEmails: Ptorx.PrimaryEmailList) {
-    if ([].indexOf('primary emails') == -1 || !primaryEmails.length)
+    if ([].indexOf('Primary Emails') == -1 || !primaryEmails.length)
       return null;
     return (
       <List>
         {this.search(primaryEmails).map(primaryEmail => (
-          <Link key={primaryEmail.id} to={`/primary-emails/${primaryEmail.id}`}>
+          <Link
+            key={primaryEmail.id}
+            to={`/app/primary-emails/${primaryEmail.id}`}
+          >
             <ListItem>
               <ListItemText
                 primary={primaryEmail.address}
@@ -46,11 +58,11 @@ export class Search extends React.Component {
   }
 
   renderProxyEmails(proxyEmails: Ptorx.ProxyEmailList) {
-    if ([].indexOf('proxy emails') == -1 || !proxyEmails.length) return null;
+    if ([].indexOf('Proxy Emails') == -1 || !proxyEmails.length) return null;
     return (
       <List>
         {this.search(proxyEmails).map(proxyEmail => (
-          <Link key={proxyEmail.id} to={`/proxy-emails/${proxyEmail.id}`}>
+          <Link key={proxyEmail.id} to={`/app/proxy-emails/${proxyEmail.id}`}>
             <ListItem>
               <ListItemText
                 primary={proxyEmail.fullAddress}
@@ -66,11 +78,11 @@ export class Search extends React.Component {
   }
 
   renderModifiers(modifiers: Ptorx.ModifierList) {
-    if ([].indexOf('modifiers') == -1 || !modifiers.length) return null;
+    if ([].indexOf('Modifiers') == -1 || !modifiers.length) return null;
     return (
       <List>
         {this.search(modifiers).map(modifier => (
-          <Link key={modifier.id} to={`/modifiers/${modifier.id}`}>
+          <Link key={modifier.id} to={`/app/modifiers/${modifier.id}`}>
             <ListItem>
               <ListItemText
                 primary={modifier.name}
@@ -84,11 +96,11 @@ export class Search extends React.Component {
   }
 
   renderMessages(messages: Ptorx.MessageList) {
-    if ([].indexOf('messages') == -1 || !messages.length) return null;
+    if ([].indexOf('Messages') == -1 || !messages.length) return null;
     return (
       <List>
         {this.search(messages).map(message => (
-          <Link key={message.id} to={`/messages/${message.id}`}>
+          <Link key={message.id} to={`/app/messages/${message.id}`}>
             <ListItem>
               <ListItemText
                 primary={message.subject}
@@ -104,11 +116,11 @@ export class Search extends React.Component {
   }
 
   renderFilters(filters: Ptorx.FilterList) {
-    if ([].indexOf('filters') == -1 || !filters.length) return null;
+    if ([].indexOf('Filters') == -1 || !filters.length) return null;
     return (
       <List>
         {this.search(filters).map(filter => (
-          <Link key={filter.id} to={`/filters/${filter.id}`}>
+          <Link key={filter.id} to={`/app/filters/${filter.id}`}>
             <ListItem>
               <ListItemText
                 primary={filter.name}
@@ -121,43 +133,37 @@ export class Search extends React.Component {
     );
   }
 
-  renderDomains(domains: Ptorx.DomainList) {
-    if ([].indexOf('domains') == -1 || !domains.length) return null;
-    return (
-      <List>
-        {this.search(domains).map(domain => (
-          <Link key={domain.id} to={`/domains/${domain.id}`}>
-            <ListItem>
-              <ListItemText
-                primary={domain.domain}
-                secondary={`Created ${moment.unix(domain.created).fromNow()}`}
-              />
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    );
-  }
-
   render() {
-    const { context } = this;
+    const {
+      primaryEmails,
+      proxyEmails,
+      categories,
+      modifiers,
+      messages,
+      dispatch,
+      domains,
+      filters,
+      search
+    } = this.context;
     return (
       <div>
         <TextField
           fullWidth
           id="search"
           type="search"
-          value={context.search}
+          value={search}
           margin="normal"
-          onChange={e => context.dispatch({ search: e.target.value })}
+          onChange={e => dispatch({ search: e.target.value.toLowerCase() })}
           placeholder="Search..."
         />
-        {this.renderPrimaryEmails(context.primaryEmails)}
-        {this.renderProxyEmails(context.proxyEmails)}
-        {this.renderModifiers(context.modifiers)}
-        {this.renderMessages(context.messages)}
-        {this.renderFilters(context.filters)}
-        {this.renderDomains(context.domains)}
+        {this.renderPrimaryEmails(primaryEmails)}
+        {this.renderProxyEmails(proxyEmails)}
+        {this.renderModifiers(modifiers)}
+        {this.renderMessages(messages)}
+        {this.renderFilters(filters)}
+        {categories.indexOf('Domains') > -1 && domains.length ? (
+          <DomainMatches domains={this.search(domains)} />
+        ) : null}
       </div>
     );
   }
