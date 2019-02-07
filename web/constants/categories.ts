de28de00ef358ba19c6cd154435046cef56dc153
MrXyfir <mr@xyfir.com>
2019-02-07 10:54:17 -0800

    Improve CATEGORIES

--- web/constants/categories.ts
+++ web/constants/categories.ts
@@ -1,8 +1,38 @@
 export const CATEGORIES = [
-  'Primary Emails',
-  'Proxy Emails',
-  'Modifiers',
-  'Messages',
-  'Filters',
-  'Domains'
+  {
+    name: 'Primary Emails',
+    route: 'primary-emails',
+    singular: 'Primary Email',
+    variable: 'primaryEmails'
+  },
+  {
+    name: 'Proxy Emails',
+    route: 'proxy-emails',
+    singular: 'Proxy Email',
+    variable: 'proxyEmails'
+  },
+  {
+    name: 'Modifiers',
+    route: 'modifiers',
+    singular: 'Modifier',
+    variable: 'modifiers'
+  },
+  {
+    name: 'Messages',
+    route: 'messages',
+    singular: 'Message',
+    variable: 'messages'
+  },
+  {
+    name: 'Filters',
+    route: 'filters',
+    singular: 'Filter',
+    variable: 'filters'
+  },
+  {
+    name: 'Domains',
+    route: 'domains',
+    singular: 'Domain',
+    variable: 'domains'
+  }
 ];
