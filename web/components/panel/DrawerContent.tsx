cb1794cbb4028a2a65d8b0e32a747a8b621829ba
MrXyfir <mr@xyfir.com>
2019-02-12 09:05:58 -0800

    Display available credits and clean up code

--- web/components/panel/DrawerContent.tsx
+++ web/components/panel/DrawerContent.tsx
@@ -1,18 +1,14 @@
-import {
-  ACCOWNT_API_URL,
-  HELP_DOCS_URL,
-  ACCOWNT_WEB_URL
-} from 'constants/config';
 import { AccountBox, ExitToApp, Help } from '@material-ui/icons';
+import { CATEGORIES, Category } from 'constants/categories';
 import { PanelContext } from 'lib/PanelContext';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import {
   ListItemSecondaryAction,
   ListSubheader,
   ListItemIcon,
   ListItemText,
   createStyles,
+  Typography,
   WithStyles,
   withStyles,
   ListItem,
@@ -20,19 +16,20 @@ import {
   Switch,
   List
 } from '@material-ui/core';
-import { PanelState } from './Panel';
+import {
+  ACCOWNT_API_URL,
+  ACCOWNT_WEB_URL,
+  HELP_DOCS_URL
+} from 'constants/config';
 
 const styles = createStyles({ link: { textDecoration: 'none' } });
 
-class CategoryToggle extends React.Component<{
-  category: PanelState['categories'][0];
-}> {
+class _DrawerContent extends React.Component<WithStyles<typeof styles>> {
   static contextType = PanelContext;
   context!: React.ContextType<typeof PanelContext>;
 
-  onToggle() {
+  onToggle(category: Category) {
     const { categories, dispatch } = this.context;
-    const { category } = this.props;
     const _categories =
       categories.indexOf(category) > -1
         ? categories.filter(c => c != category)
@@ -42,65 +39,59 @@ class CategoryToggle extends React.Component<{
   }
 
   render() {
-    const { categories } = this.context;
-    const { category } = this.props;
+    const { categories, user } = this.context;
+    const { classes } = this.props;
     return (
-      <ListItem>
-        <ListItemText primary={category} />
-        <ListItemSecondaryAction>
-          <Switch
-            onChange={() => this.onToggle()}
-            checked={categories.indexOf(category) > -1}
-          />
-        </ListItemSecondaryAction>
-      </ListItem>
+      <div>
+        <List>
+          <ListSubheader>Filters</ListSubheader>
+          {CATEGORIES.map(category => (
+            <ListItem key={category.name}>
+              <ListItemText primary={category.name} />
+              <ListItemSecondaryAction>
+                <Switch
+                  onChange={() => this.onToggle(category.name)}
+                  checked={categories.indexOf(category.name) > -1}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <ListSubheader>Credits: {user.credits}</ListSubheader>
+          <a href={ACCOWNT_WEB_URL} className={classes.link}>
+            <ListItem button>
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Account" />
+            </ListItem>
+          </a>
+          <a href={`${ACCOWNT_API_URL}/login/logout`} className={classes.link}>
+            <ListItem button>
+              <ListItemIcon>
+                <ExitToApp />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItem>
+          </a>
+          <a
+            href={`${HELP_DOCS_URL}#terminology`}
+            target="_blank"
+            className={classes.link}
+          >
+            <ListItem button>
+              <ListItemIcon>
+                <Help />
+              </ListItemIcon>
+              <ListItemText primary="Help" />
+            </ListItem>
+          </a>
+        </List>
+      </div>
     );
   }
 }
-
-const _DrawerContent = ({ classes }: WithStyles<typeof styles>) => (
-  <div>
-    <ListSubheader>Filters</ListSubheader>
-    <List>
-      <CategoryToggle category="Primary Emails" />
-      <CategoryToggle category="Proxy Emails" />
-      <CategoryToggle category="Modifiers" />
-      <CategoryToggle category="Messages" />
-      <CategoryToggle category="Filters" />
-      <CategoryToggle category="Domains" />
-    </List>
-    <Divider />
-    <List>
-      <a href={ACCOWNT_WEB_URL} className={classes.link}>
-        <ListItem button>
-          <ListItemIcon>
-            <AccountBox />
-          </ListItemIcon>
-          <ListItemText primary="Account" />
-        </ListItem>
-      </a>
-      <a href={`${ACCOWNT_API_URL}/login/logout`} className={classes.link}>
-        <ListItem button>
-          <ListItemIcon>
-            <ExitToApp />
-          </ListItemIcon>
-          <ListItemText primary="Logout" />
-        </ListItem>
-      </a>
-      <a
-        href={`${HELP_DOCS_URL}#terminology`}
-        target="_blank"
-        className={classes.link}
-      >
-        <ListItem button>
-          <ListItemIcon>
-            <Help />
-          </ListItemIcon>
-          <ListItemText primary="Help" />
-        </ListItem>
-      </a>
-    </List>
-  </div>
-);
 
 export const DrawerContent = withStyles(styles)(_DrawerContent);
