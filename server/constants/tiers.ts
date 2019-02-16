6adcbc0f849f6ed43e90ac060f8fc2f1fe93ebde
MrXyfir <mr@xyfir.com>
2019-02-16 11:40:17 -0800

    Add TIERS

---
+++ server/constants/tiers.ts
@@ -0,0 +1,32 @@
+import { Ptorx } from 'types/ptorx';
+
+export const TIERS: {
+  name: Ptorx.Tier;
+  credits: number;
+  durations: {
+    duration: Ptorx.TierDuration;
+    cost: number;
+  }[];
+}[] = [
+  {
+    name: 'basic',
+    credits: 100,
+    durations: [{ duration: 'life', cost: 0 }]
+  },
+  {
+    name: 'premium',
+    credits: 1500,
+    durations: [
+      { duration: 'month', cost: 100 },
+      { duration: 'year', cost: 1000 }
+    ]
+  },
+  {
+    name: 'ultimate',
+    credits: 10000,
+    durations: [
+      { duration: 'month', cost: 500 },
+      { duration: 'year', cost: 5000 }
+    ]
+  }
+];
