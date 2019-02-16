cf3cea29c0ba39833ef3710d0fa06801e729d461
MrXyfir <mr@xyfir.com>
2019-02-16 11:40:52 -0800

    Update and add tests

--- server/__tests__/payments.ts
+++ server/__tests__/payments.ts
@@ -1,18 +1,49 @@
+import { finishPayment } from 'lib/payments/finish';
+import { startPayment } from 'lib/payments/start';
 import { addPayment } from 'lib/payments/add';
+import { verifyJWT } from 'lib/jwt/verify';
+import { signJWT } from 'lib/jwt/sign';
 import { Ptorx } from 'types/ptorx';
 import 'lib/tests/prepare';
+import { getUser } from 'lib/users/get';
 
 test('create payment', async () => {
   const paid = Date.now();
-  const payment = await addPayment({ months: 1, tier: 'premium', paid }, 1234);
-  expect(Object.keys(payment).length).toBe(5);
+  const payment = await addPayment(
+    { duration: 'month', tier: 'premium', paid },
+    1234
+  );
+  expect(Object.keys(payment).length).toBe(6);
   expect(payment.id).toBeNumber();
   expect(payment.userId).toBe(1234);
   const _payment: Ptorx.Payment = {
     ...payment,
-    months: 1,
+    amount: 100,
+    duration: 'month',
     tier: 'premium',
     paid
   };
   expect(payment).toMatchObject(_payment);
+});
+
+test('start and finish payment', async () => {
+  const { url } = await startPayment(
+    { tier: 'premium', duration: 'month' },
+    1234
+  );
+  const { id }: { id: Ptorx.Payment['id'] } = await verifyJWT(
+    url.split('?jwt=')[1]
+  );
+  const payment: Partial<Ptorx.Payment> = { id };
+  let jwt = await signJWT(payment, '1d');
+  await expect(finishPayment(jwt, 1234)).toReject();
+  payment.paid = Date.now();
+  jwt = await signJWT(payment, '1d');
+  await expect(finishPayment(jwt, 1234)).not.toReject();
+  await expect(finishPayment(jwt, 1234)).toReject();
+
+  const user = await getUser(1234);
+  expect(user.tier).toBe('premium');
+  expect(user.tierExpiration).toBeNumber();
+  expect(user.credits).toBeGreaterThan(100);
 });
