37b4efd09ac70d712929188f178e6f1108d32d14
MrXyfir <mr@xyfir.com>
2019-02-16 18:41:41 -0800

    Remove qs

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,9 +1,7 @@
 import { API } from 'constants/config';
-import * as qs from 'qs';
 import axios from 'axios';
 
 export const api = axios.create({
   baseURL: `${API}/api/6`,
-  withCredentials: true,
-  paramsSerializer: qs.stringify
+  withCredentials: true
 });
