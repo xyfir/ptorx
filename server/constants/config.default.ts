5fb22d302f212f2cbdc0ede79dc07e78960944a5
MrXyfir <mr@xyfir.com>
2019-02-17 11:19:06 -0800

    Add annotated default config files

--- server/constants/config.default.ts
+++ server/constants/config.default.ts
@@ -1,40 +1,89 @@
 import { SMTPServerOptions } from 'smtp-server';
 import { readFileSync } from 'fs';
 
-export const URL = 'http://';
-export const NAME = '';
+/**
+ * Name of the application as displayed to the user.
+ */
+export const NAME = 'Ptorx';
+/**
+ * Is this a production environment?
+ */
 export const PROD = false;
+/**
+ * Should app run cron jobs?
+ */
 export const CRON = false;
+/**
+ * MySQL / MariaDB connection object passed to mysql.createPool()
+ * https://github.com/mysqljs/mysql#pooling-connections
+ */
 export const MYSQL = {
   host: 'localhost',
   port: 3306,
   user: 'root',
   charset: 'UTF8MB4_UNICODE_CI',
   password: '',
-  database: '',
+  database: 'ptorx',
   dateStrings: true,
   connectionLimit: 100,
   supportBigNumbers: true,
   waitForConnections: true
 };
-export const DOMAIN = '';
-export const JWT_KEY = '';
-export const API_PORT = 2070;
+/**
+ * The app's main domain name.
+ */
+export const DOMAIN = 'ptorx.com';
+/**
+ * Shared secret (also used by Accownt and Rich Cow) used for signing and
+ *  verifying JSON Web Tokens.
+ */
+export const JWT_KEY = 'ptorx-jwt';
+/**
+ * The app's root API URL.
+ */
+export const API_URL = 'https://ptorx.com/api';
+/**
+ * The app's root web client URL.
+ */
+export const WEB_URL = 'https://ptorx.com';
+/**
+ * The port to host the API server on.
+ */
+export const API_PORT = 12345;
+/**
+ * The internal database id of `DOMAIN`.
+ */
 export const DOMAIN_ID = 4;
+/**
+ * The port to host the SMTP server on.
+ */
 export const SMTP_PORT = 2071;
-export const DIRECTORIES = {
-  MAIL_CACHE: '',
-  WEB: ''
-};
-export const WEBPACK_PORT = 2073;
-export const CALLBACK_URL = 'http://';
-export const XYPAYMENTS_ID = 13;
+/**
+ * Absolute path for ptorx-web.
+ */
+export const WEB_DIRECTORY = '/path/to/ptorx/web';
+/**
+ * The port to host the test SMTP server on. Used for capturing outgoing mail
+ *  in a testing environment.
+ */
 export const TEST_SMTP_PORT = 2072;
-export const XYPAYMENTS_KEY = '';
-export const XYPAYMENTS_URL = 'http://';
-export const RICH_COW_WEB_URL = 'http://';
-export const XYPAYMENTS_PRODUCTS = { 1: 10, 2: 11, 3: 12 };
-export const PERSISTENT_PROXY_EMAIL = '';
+/**
+ * URL for the Rich Cow web client.
+ */
+export const RICH_COW_WEB_URL = 'https://ptorx.com/rich-cow';
+/**
+ * Absolute path for where to temporarily cache large outgoing mail for
+ *  before being signed.
+ */
+export const MAIL_CACHE_DIRECTORY = '';
+/**
+ * A proxy email within the database. Used for testing.
+ */
+export const PERSISTENT_PROXY_EMAIL = 'test@ptorx.com';
+/**
+ * Configuration for the SMTP server.
+ * https://nodemailer.com/extras/smtp-server/#step-3-create-smtpserver-instance
+ */
 export const SMTP_SERVER_OPTIONS: SMTPServerOptions = {
   banner: '',
   logger: true,
