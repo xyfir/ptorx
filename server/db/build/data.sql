13d0f3f855be5e87aeb4de05848e258f9a8b4d62
MrXyfir <mr@xyfir.com>
2019-02-28 13:51:23 -0800

    Add /server/db

---
+++ server/db/build/data.sql
@@ -0,0 +1,6 @@
+-----
+-- Add cron jobs
+-----
+INSERT INTO `cron_jobs` (`id`, `name`, `lastRun`, `minutesInterval`) VALUES (NULL, 'delete-expired-messages', NULL, '60');
+INSERT INTO `cron_jobs` (`id`, `name`, `lastRun`, `minutesInterval`) VALUES (NULL, 'top-up-credits', NULL, '43800');
+INSERT INTO `cron_jobs` (`id`, `name`, `lastRun`, `minutesInterval`) VALUES (NULL, 'reset-tiers', NULL, '1440');
---
+++ server/db/build/structure.sql
@@ -0,0 +1,452 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ptorx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cron_jobs`
+--
+
+CREATE TABLE `cron_jobs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `lastRun` datetime DEFAULT NULL,
+  `minutesInterval` mediumint(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deleted_proxy_emails`
+--
+
+CREATE TABLE `deleted_proxy_emails` (
+  `domainId` int(10) UNSIGNED NOT NULL,
+  `address` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `domains`
+--
+
+CREATE TABLE `domains` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `domain` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publicKey` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `privateKey` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `selector` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `verified` tinyint(1) NOT NULL,
+  `global` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `domain_users`
+--
+
+CREATE TABLE `domain_users` (
+  `domainId` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `requestKey` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `authorized` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `filters`
+--
+
+CREATE TABLE `filters` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `find` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `blacklist` tinyint(1) NOT NULL,
+  `regex` tinyint(1) NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
+CREATE TABLE `links` (
+  `proxyEmailId` int(10) UNSIGNED NOT NULL,
+  `orderIndex` tinyint(3) UNSIGNED NOT NULL,
+  `primaryEmailId` int(10) UNSIGNED DEFAULT NULL,
+  `modifierId` int(10) UNSIGNED DEFAULT NULL,
+  `filterId` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `proxyEmailId` int(10) UNSIGNED NOT NULL,
+  `key` varchar(36) CHARACTER SET utf8mb4 NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `subject` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `from` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `to` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `html` mediumtext COLLATE utf8mb4_unicode_ci,
+  `headers` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `replyTo` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_attachments`
+--
+
+CREATE TABLE `message_attachments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `messageId` bigint(20) UNSIGNED NOT NULL,
+  `filename` tinytext COLLATE utf8mb4_unicode_ci,
+  `contentType` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `size` int(10) UNSIGNED NOT NULL,
+  `content` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modifiers`
+--
+
+CREATE TABLE `modifiers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `target` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `template` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `tier` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` mediumint(8) UNSIGNED NOT NULL,
+  `paid` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `primary_emails`
+--
+
+CREATE TABLE `primary_emails` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `address` varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `key` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `verified` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proxy_emails`
+--
+
+CREATE TABLE `proxy_emails` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `domainId` int(10) UNSIGNED NOT NULL,
+  `address` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `saveMail` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `credits` int(10) UNSIGNED NOT NULL,
+  `tier` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tierExpiration` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cron_jobs`
+--
+ALTER TABLE `cron_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deleted_proxy_emails`
+--
+ALTER TABLE `deleted_proxy_emails`
+  ADD UNIQUE KEY `domainId` (`domainId`,`address`);
+
+--
+-- Indexes for table `domains`
+--
+ALTER TABLE `domains`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `domain` (`domain`),
+  ADD KEY `fk__domains__userId` (`userId`);
+
+--
+-- Indexes for table `domain_users`
+--
+ALTER TABLE `domain_users`
+  ADD UNIQUE KEY `domainId` (`domainId`,`userId`) USING BTREE,
+  ADD KEY `fk__domain_users__userId` (`userId`);
+
+--
+-- Indexes for table `filters`
+--
+ALTER TABLE `filters`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__filters__userId` (`userId`);
+
+--
+-- Indexes for table `links`
+--
+ALTER TABLE `links`
+  ADD KEY `fk__link__proxyEmailId` (`proxyEmailId`),
+  ADD KEY `fk__link__primaryEmailId` (`primaryEmailId`),
+  ADD KEY `fk__link__modifierId` (`modifierId`),
+  ADD KEY `fk__link__filterId` (`filterId`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__messages__proxyEmailId` (`proxyEmailId`) USING BTREE;
+
+--
+-- Indexes for table `message_attachments`
+--
+ALTER TABLE `message_attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__message_attachments__messageId` (`messageId`);
+
+--
+-- Indexes for table `modifiers`
+--
+ALTER TABLE `modifiers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__modifiers__userId` (`userId`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__payments__userId` (`userId`);
+
+--
+-- Indexes for table `primary_emails`
+--
+ALTER TABLE `primary_emails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__primary_emails__userId` (`userId`);
+
+--
+-- Indexes for table `proxy_emails`
+--
+ALTER TABLE `proxy_emails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__proxy_emails__domainId` (`domainId`) USING BTREE,
+  ADD KEY `fk__proxy_emails__userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cron_jobs`
+--
+ALTER TABLE `cron_jobs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `domains`
+--
+ALTER TABLE `domains`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `filters`
+--
+ALTER TABLE `filters`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=533;
+
+--
+-- AUTO_INCREMENT for table `message_attachments`
+--
+ALTER TABLE `message_attachments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=178;
+
+--
+-- AUTO_INCREMENT for table `modifiers`
+--
+ALTER TABLE `modifiers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `primary_emails`
+--
+ALTER TABLE `primary_emails`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `proxy_emails`
+--
+ALTER TABLE `proxy_emails`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483647;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `deleted_proxy_emails`
+--
+ALTER TABLE `deleted_proxy_emails`
+  ADD CONSTRAINT `fk__deleted_proxy_emails__domainId` FOREIGN KEY (`domainId`) REFERENCES `domains` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `domains`
+--
+ALTER TABLE `domains`
+  ADD CONSTRAINT `fk__domains__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `domain_users`
+--
+ALTER TABLE `domain_users`
+  ADD CONSTRAINT `fk__domain_users__domainId` FOREIGN KEY (`domainId`) REFERENCES `domains` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk__domain_users__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `filters`
+--
+ALTER TABLE `filters`
+  ADD CONSTRAINT `fk__filters__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `links`
+--
+ALTER TABLE `links`
+  ADD CONSTRAINT `fk__link__filterId` FOREIGN KEY (`filterId`) REFERENCES `filters` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk__link__modifierId` FOREIGN KEY (`modifierId`) REFERENCES `modifiers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk__link__primaryEmailId` FOREIGN KEY (`primaryEmailId`) REFERENCES `primary_emails` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk__link__proxyEmailId` FOREIGN KEY (`proxyEmailId`) REFERENCES `proxy_emails` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `fk__messages__proxyEmailId` FOREIGN KEY (`proxyEmailId`) REFERENCES `proxy_emails` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `message_attachments`
+--
+ALTER TABLE `message_attachments`
+  ADD CONSTRAINT `fk__message_attachments__messageId` FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `modifiers`
+--
+ALTER TABLE `modifiers`
+  ADD CONSTRAINT `fk__modifiers__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `fk__payments__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `primary_emails`
+--
+ALTER TABLE `primary_emails`
+  ADD CONSTRAINT `fk__primary_emails__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `proxy_emails`
+--
+ALTER TABLE `proxy_emails`
+  ADD CONSTRAINT `fk__proxy_emails__domainId` FOREIGN KEY (`domainId`) REFERENCES `domains` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk__proxy_emails__userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
