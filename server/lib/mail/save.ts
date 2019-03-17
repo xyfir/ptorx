c600d8234e9b7bc790b23fbe9ac941e2d25e4cc3
MrXyfir <mr@xyfir.com>
2019-03-16 20:09:51 -0700

    Encrypt html and text if public key is available

--- server/lib/mail/save.ts
+++ server/lib/mail/save.ts
@@ -1,11 +1,36 @@
 import { addMessage } from 'lib/messages/add';
 import { ParsedMail } from 'mailparser';
+import * as openpgp from 'openpgp';
+import { getUser } from 'lib/users/get';
 import { Ptorx } from 'types/ptorx';
 
 export async function saveMail(
   original: ParsedMail,
   alias: Ptorx.Alias
 ): Promise<Ptorx.Message> {
+  const user = await getUser(alias.userId);
+
+  let html = typeof original.html == 'string' ? original.html : undefined;
+  let text = original.text;
+
+  // Encrypt email body
+  if (user.publicKey) {
+    const { keys } = await openpgp.key.readArmored(user.publicKey);
+    let ciphertext = await openpgp.encrypt({
+      message: openpgp.message.fromText(text),
+      publicKeys: keys
+    });
+    text = ciphertext.data;
+
+    if (html !== undefined) {
+      ciphertext = await openpgp.encrypt({
+        message: openpgp.message.fromText(html),
+        publicKeys: keys
+      });
+      html = ciphertext.data;
+    }
+  }
+
   return await addMessage(
     {
       attachments: original.attachments.map(a => ({
@@ -16,10 +41,10 @@ export async function saveMail(
       })),
       from: original.from.text,
       headers: original.headerLines.map(h => h.line),
-      html: typeof original.html == 'string' ? original.html : undefined,
+      html,
       aliasId: alias.id,
       subject: original.subject,
-      text: original.text,
+      text,
       to: original.to.text
     },
     alias.userId
