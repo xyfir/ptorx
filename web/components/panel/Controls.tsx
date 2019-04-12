1e10cc718e3b90c411027315664521825d6b3553
MrXyfir <mr@xyfir.com>
2019-04-11 19:39:10 -0700

    Add SpeedDial and delete controls

--- web/components/panel/Controls.tsx
+++ web/components/panel/Controls.tsx
@@ -1,7 +1,9 @@
+import { SpeedDialAction, SpeedDialIcon, SpeedDial } from '@material-ui/lab';
+import { PanelContext } from 'lib/PanelContext';
+import { Delete, Add } from '@material-ui/icons';
 import { CATEGORIES } from 'constants/categories';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Add } from '@material-ui/icons';
 import {
   createStyles,
   ListItemText,
@@ -10,15 +12,14 @@ import {
   withStyles,
   ListItem,
   Dialog,
-  List,
-  Fab
+  List
 } from '@material-ui/core';
 
 const styles = createStyles({
   link: {
     textDecoration: 'none'
   },
-  fab: {
+  speedDial: {
     position: 'fixed',
     bottom: '1em',
     zIndex: 1
@@ -27,17 +28,25 @@ const styles = createStyles({
 
 interface PanelControlsState {
   create: boolean;
+  open: boolean;
 }
 
 class _PanelControls extends React.Component<
   WithStyles<typeof styles>,
   PanelControlsState
 > {
-  state: PanelControlsState = { create: false };
+  state: PanelControlsState = { create: false, open: false };
+  static contextType = PanelContext;
+  context!: React.ContextType<typeof PanelContext>;
+
+  onDelete() {
+    const { dispatch, manage } = this.context;
+    dispatch({ manage: manage == 'delete' ? null : 'delete' });
+  }
 
   render() {
+    const { create, open } = this.state;
     const { classes } = this.props;
-    const { create } = this.state;
     return (
       <React.Fragment>
         <Dialog
@@ -63,14 +72,31 @@ class _PanelControls extends React.Component<
             ))}
           </List>
         </Dialog>
-        <Fab
-          color="primary"
-          onClick={() => this.setState({ create: true })}
-          className={classes.fab}
-          aria-label="Create new..."
+
+        <SpeedDial
+          open={open}
+          icon={<SpeedDialIcon />}
+          onBlur={() => this.setState({ open: false })}
+          onClick={() => this.setState({ open: true })}
+          onFocus={() => this.setState({ open: true })}
+          onClose={() => this.setState({ open: false })}
+          direction="up"
+          ariaLabel="Controls"
+          className={classes.speedDial}
+          onMouseEnter={() => this.setState({ open: true })}
+          onMouseLeave={() => this.setState({ open: false })}
         >
-          <Add />
-        </Fab>
+          <SpeedDialAction
+            icon={<Delete />}
+            onClick={() => this.onDelete()}
+            tooltipTitle="Delete..."
+          />
+          <SpeedDialAction
+            icon={<Add />}
+            onClick={() => this.setState({ create: true })}
+            tooltipTitle="Create new..."
+          />
+        </SpeedDial>
       </React.Fragment>
     );
   }
