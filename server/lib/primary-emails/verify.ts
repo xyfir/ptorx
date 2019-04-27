a560a44e4f8300ec7ba88d6e0d695413cbbbc6ff
MrXyfir <mr@xyfir.com>
2019-04-27 14:05:41 -0700

    Remove editPrimaryEmail() usage

--- server/lib/primary-emails/verify.ts
+++ server/lib/primary-emails/verify.ts
@@ -1,18 +1,26 @@
-import { editPrimaryEmail } from 'lib/primary-emails/edit';
 import { getPrimaryEmail } from 'lib/primary-emails/get';
 import { Ptorx } from 'types/ptorx';
+import { MySQL } from 'lib/MySQL';
 
 export async function verifyPrimaryEmail(
   primaryEmailId: Ptorx.PrimaryEmail['id'],
   primaryEmailKey: Ptorx.PrimaryEmail['key'],
   userId: number
 ): Promise<void> {
+  const db = new MySQL();
   try {
+    // Verify id/key/user
     const primaryEmail = await getPrimaryEmail(primaryEmailId, userId);
     if (primaryEmail.key != primaryEmailKey)
       throw 'Primary email could not be verified';
-    await editPrimaryEmail({ ...primaryEmail, verified: true }, userId);
+
+    // Mark as verified
+    await db.query('UPDATE primary_emails SET verified = 1 WHERE id = ?', [
+      primaryEmail.id
+    ]);
+    db.release();
   } catch (err) {
+    db.release();
     throw err;
   }
 }
