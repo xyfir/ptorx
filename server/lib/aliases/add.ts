c02a35eae18fd4d60ef3d965fe9cd70d450d4cc4
MrXyfir <mr@xyfir.com>
2019-04-27 13:22:21 -0700

    Link new aliases to autolinking primary emails

--- server/lib/aliases/add.ts
+++ server/lib/aliases/add.ts
@@ -1,3 +1,4 @@
+import { listPrimaryEmails } from 'lib/primary-emails/list';
 import { listDomains } from 'lib/domains/list';
 import { checkAlias } from 'lib/aliases/check';
 import { editAlias } from 'lib/aliases/edit';
@@ -13,6 +14,7 @@ export async function addAlias(
 ): Promise<Ptorx.Alias> {
   const db = new MySQL();
   try {
+    // Validate that user is authorized to use domain
     const domains = await listDomains(userId);
     if (domains.findIndex(d => d.id == alias.domainId) == -1)
       throw 'You cannot use that domain';
@@ -28,29 +30,30 @@ export async function addAlias(
         if (available) break;
       }
     }
-    // Make sure address exists
+    // Validate provided address
     else {
       alias.address = alias.address.toLowerCase();
 
       if (alias.address) {
         if (!/^[\w\-]{1,64}$/.test(alias.address))
-          throw 'Bad address: must 1-64 alphanumerical characters';
+          throw 'Bad address: must be 1-64 alphanumerical characters';
         if (alias.address.startsWith('x-'))
           throw 'Bad address: cannot start with "x-"';
         if (alias.address.endsWith('-x'))
           throw 'Bad address: cannot end with "-x"';
         if (alias.address.indexOf('--') > -1)
-          throw 'Bad address: cannot contain two or more consecutive hyphens';
+          throw 'Bad address: cannot contain 2+ consecutive hyphens';
         if (alias.address.indexOf('__') > -1)
-          throw 'Bad address: cannot contain two or more consecutive underscores';
+          throw 'Bad address: cannot contain 2+ consecutive underscores';
         if (alias.address.startsWith('srs'))
-          throw 'Bad address: cannot start with srs';
+          throw 'Bad address: cannot start with "srs"';
       }
 
       const { available } = await checkAlias(alias.domainId, alias.address);
       if (!available) throw 'Email is already in use';
     }
 
+    // Create alias
     const insert: Partial<Ptorx.Alias> = {
       userId,
       created: moment().unix(),
@@ -59,8 +62,22 @@ export async function addAlias(
     };
     const result = await db.query('INSERT INTO aliases SET ?', insert);
     const _alias = await getAlias(result.insertId, userId);
-
     db.release();
+
+    // Automatically link primary emails
+    if (!alias.links || !alias.links.length) {
+      const primaryEmails = await listPrimaryEmails(userId);
+      _alias.links = primaryEmails
+        .filter(primaryEmail => primaryEmail.autolink)
+        .map(
+          (primaryEmail, i): Ptorx.AliasLink => ({
+            aliasId: _alias.id,
+            orderIndex: i,
+            primaryEmailId: primaryEmail.id
+          })
+        );
+    }
+
     return await editAlias({ ..._alias, ...alias }, userId);
   } catch (err) {
     db.release();
