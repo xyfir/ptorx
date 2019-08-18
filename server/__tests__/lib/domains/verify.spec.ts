9145445cfa206c66be9052077058bae84a177956
MrXyfir <mr@xyfir.com>
2019-08-18 12:36:06 -0700

    Fix and improve test with mock

--- server/__tests__/lib/domains/verify.spec.ts
+++ server/__tests__/lib/domains/verify.spec.ts
@@ -1,9 +1,23 @@
 import { verifyDomain } from 'lib/domains/verify';
 import { addDomain } from 'lib/domains/add';
+import * as dns from 'dns';
 
 test('verifyDomain()', async () => {
   const domain = await addDomain({ domain: 'example7.com' }, 1234);
+
+  // No key
+  (dns as any).resolveTxt = jest.fn((d, fn) => fn(null, []));
   await expect(verifyDomain(domain.id, 1234)).rejects.toBe(
-    'Domain could not be verified'
+    'Could not find domain key'
   );
+
+  // Bad key
+  (dns as any).resolveTxt = jest.fn((d, fn) => fn(null, [['bad']]));
+  await expect(verifyDomain(domain.id, 1234)).rejects.toBe(
+    'Invalid domain key'
+  );
+
+  // Good key
+  (dns as any).resolveTxt = jest.fn((d, fn) => fn(null, [[domain.publicKey]]));
+  await expect(verifyDomain(domain.id, 1234)).not.toReject();
 });
