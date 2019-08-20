820b20f3d93f1a29f4f44e49250417f6d5c450b8
MrXyfir <mr@xyfir.com>
2019-08-20 14:21:34 -0700

    Fix alias config

--- server/db/build/prepare-tests.sql
+++ server/db/build/prepare-tests.sql
@@ -10,7 +10,7 @@ INSERT INTO `users` (`email`, `credits`, `tier`, `tierExpiration`) VALUES ('test
 INSERT INTO `domains` (`userId`, `domain`, `verified`, `global`, `publicKey`, `privateKey`, `selector`, `created`) VALUES (1, 'dev.ptorx.com', 1, 1, '', '', '', 0);
 
 -- Add alias
-INSERT INTO `aliases` (`userId`, `domainId`, `address`, `name`, `created`, `smtpKey`, `saveMail`, `canReply`) VALUES (1, 1, 'alias12', '', 0, '', 0, 0);
+INSERT INTO `aliases` (`userId`, `domainId`, `address`, `name`, `created`, `smtpKey`, `saveMail`, `canReply`) VALUES (1, 1, 'alias12', '', 0, '', 1, 1);
 
 -- Add primary email and link to alias
 INSERT INTO `primary_emails` (`userId`, `address`, `verified`, `created`, `key`, `autolink`) VALUES (1, 'user@example.com', 1, 0, '', 0);
