19f3ca09de82aaac145d8e85db99ac3b6c10616b
MrXyfir <mr@xyfir.com>
2019-08-19 19:10:27 -0700

    Fix SQL file paths

--- .travis.sh
+++ .travis.sh
@@ -3,9 +3,9 @@ if [ "$PACKAGE" == "server" ]; then
   # sudo apt install mariadb-server
   # sudo systemctl enable mysql
   sudo mysql -u root -e "CREATE DATABASE ptorx"
-  sudo mysql -u root -p ptorx < server/db/build/structure.sql
-  sudo mysql -u root -p ptorx < server/db/build/data.sql
-  sudo mysql -u root -p ptorx < server/db/build/test-data.sql
+  sudo mysql -u root -p ptorx < db/build/structure.sql
+  sudo mysql -u root -p ptorx < db/build/data.sql
+  sudo mysql -u root -p ptorx < db/build/test-data.sql
 
   # misc config
   mkdir mail-cache
