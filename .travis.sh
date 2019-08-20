ca3ca468d3d8e3c408d323754d2b8db16e0c2f7d
MrXyfir <mr@xyfir.com>
2019-08-19 19:07:22 -0700

    Change MariaDB installation

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 if [ "$PACKAGE" == "server" ]; then
   # configure mysql
-  sudo apt install mariadb-server
-  sudo systemctl enable mysql
+  # sudo apt install mariadb-server
+  # sudo systemctl enable mysql
   sudo mysql -u root -e "CREATE DATABASE ptorx"
   sudo mysql -u root -p ptorx < server/db/build/structure.sql
   sudo mysql -u root -p ptorx < server/db/build/data.sql
