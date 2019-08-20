7f026082d6dc2776aa03d1892943d14d8427c752
MrXyfir <mr@xyfir.com>
2019-08-19 18:14:00 -0700

    Add Travis CI config

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+if [ "$PACKAGE" == "server" ]; then
+  # configure mysql
+  sudo apt install mariadb-server
+  sudo systemctl enable mysql
+  sudo mysql -u root -e "CREATE DATABASE ptorx"
+  sudo mysql -u root -p ptorx < server/db/build/structure.sql
+  sudo mysql -u root -p ptorx < server/db/build/data.sql
+  sudo mysql -u root -p ptorx < server/db/build/test-data.sql
+
+  # misc config
+  mkdir mail-cache
+fi
+
+if [ "$PACKAGE" != "." ]; then
+  cp example.env .env
+  npm t
+  npm run build
+fi
